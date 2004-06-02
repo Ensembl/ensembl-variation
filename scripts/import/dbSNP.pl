838cd2c2b0b5c6dd711916284647b1aad7dd91e0
Graham McVicker <mcvicker@sanger.ac.uk>
2004-06-02 16:32:36 +0000

    modified population and variation table creation, added allele_group and variation_group creation

--- scripts/import/dbSNP.pl
+++ scripts/import/dbSNP.pl
@@ -14,12 +14,16 @@ my $dbVar = DBI->connect( "DBI:mysql:host=ecs4.internal.sanger.ac.uk;dbname=mcvi
 my $dbCore = DBI->connect( "DBI:mysql:host=ecs2.internal.sanger.ac.uk;dbname=homo_sapiens_core_22_34d;port=3364","ensro" );
 
 
+my $TAX_ID = 9606; # human
+
 source_table();
 variation_table();
 population_table();
 allele_table();
 flanking_sequence_table();
 variation_feature();
+allele_group();
+variation_group();
 cleanup();
 
 
@@ -31,11 +35,6 @@ sub source_table {
 # filling of the variation table from SubSNP and SNP
 # creating of a link table variation_id --> subsnp_id
 sub variation_table {
-
-  ### TBD make sure only the correct SNPs for the species come across
-  ### by looking at the tax_id
-
-
   $dbVar->do( "ALTER TABLE variation add column snp_id int" );
   $dbVar->do( "ALTER TABLE variation add column subsnp_id int" );
 
@@ -44,8 +43,9 @@ sub variation_table {
   debug("Dumping RefSNPs");
 
   dumpSQL( qq{
-           SELECT 1,concat( "rs", snp_id), snp_id
+           SELECT 1, concat( "rs", snp_id), snp_id
            FROM SNP
+           WHERE tax_id = $TAX_ID
            LIMIT 10000
           }
       );
@@ -58,7 +58,7 @@ sub variation_table {
 
   $dbVar->do( "ALTER TABLE variation ADD INDEX snpidx( snp_id )" );
 
-  # create a temp table of subSNP info 
+  # create a temp table of subSNP info
   # containing RefSNP id, SubSNP id and validation status
 
   debug("Dumping SubSNPs");
@@ -74,7 +74,7 @@ sub variation_table {
   create_and_load( "tmp_var", "subsnp", "refsnp", "valid" );
 
   $dbVar->do( qq{
-                 ALTER TABLE tmp_var MODIFY subsnp int 
+                 ALTER TABLE tmp_var MODIFY subsnp int
                 } );
   $dbVar->do( qq{
                  ALTER TABLE tmp_var add INDEX subsnp_idx( subsnp )
@@ -103,6 +103,45 @@ sub variation_table {
   $dbVar->do('DROP table tmp_var');
   $dbVar->do('DROP table tmp_var2');
 
+  # set the validation status of the RefSNPs.  A refSNP is validated if
+  # it has a valid subsnp
+
+  debug("Reloading RefSNPs with validation status set");
+
+  my $sth = $dbVar->prepare
+    (qq{SELECT a.variation_id, a.source_id, a.name, a.snp_id,
+               b.validation_status
+        FROM   variation a, variation b
+        WHERE  b.parent_variation_id = a.variation_id
+        ORDER BY a.variation_id, b.validation_status});
+
+  $sth->execute();
+
+  my $cur_variation_id = undef;
+  my $validated = 0;
+  my $arr;
+
+  # dump RefSNPs to tmp file with validation status set
+
+  open ( FH, ">$tmp_dir/tabledump.txt" );
+
+  while($arr = $sth->fetchrow_arrayref()) {
+    if(defined($cur_variation_id) && $arr->[0] != $cur_variation_id) {
+      my @arr = map {(defined($_)) ? $_ : '\N' } @$arr;
+      print FH join("\t", @arr), "\n";
+    }
+    $cur_variation_id = $arr->[0];
+  }
+
+  close(FH);
+
+  # remove RefSNPs from db and reload them (faster than individual updates)
+
+  $dbVar->do("DELETE FROM variation WHERE parent_variation_id is NULL");
+
+  load("variation", "variation_id", "source_id",
+       "name", "snp_id", "validation_status");
+
   return;
 }
 
@@ -112,12 +151,6 @@ sub variation_table {
 #
 sub population_table {
 
-  ### TBD create new parent pops for the few populations that have multiple
-  ### PopClassCodes in dbSNP, either that or just use the MULTINATIONAL
-  ### class
-
-  ### TBD add description
-
   $dbVar->do("ALTER TABLE population ADD column pop_id int");
   $dbVar->do("ALTER TABLE population ADD column pop_class_id int");
 
@@ -125,11 +158,11 @@ sub population_table {
 
   debug("Dumping population class data");
 
-  dumpSQL("SELECT pop_class, pop_class_id FROM PopClassCode");
+  dumpSQL("SELECT pop_class, pop_class_id, pop_class_text FROM PopClassCode");
 
   debug("Loading population class data");
 
-  create_and_load( "tmp_pop_class", "name", "pop_class_id");
+  create_and_load( "tmp_pop_class", "name", "pop_class_id", "description");
 
   $dbVar->do("ALTER TABLE tmp_pop_class MODIFY pop_class_id int");
   $dbVar->do(qq{ALTER TABLE tmp_pop_class 
@@ -140,37 +173,63 @@ sub population_table {
   # load Population data into tmp table
 
   dumpSQL(qq{SELECT concat(p.handle, ':', p.loc_pop_id),
-                    p.pop_id, pc.pop_class_id
+                    p.pop_id, pc.pop_class_id, count(*) as count
              FROM   Population p, PopClass pc
-             WHERE  p.pop_id = pc.pop_id });
+             WHERE  p.pop_id = pc.pop_id
+             GROUP  BY pc.pop_id});
 
   debug("Loading population data");
 
-  create_and_load( "tmp_pop", "name", "pop_id", "pop_class_id");
+  create_and_load( "tmp_pop", "name", "pop_id", "pop_class_id", "count" );
 
   $dbVar->do("ALTER TABLE tmp_pop MODIFY pop_class_id int");
   $dbVar->do("ALTER TABLE tmp_pop ADD INDEX pop_class_id(pop_class_id)");
   $dbVar->do("ALTER TABLE tmp_pop MODIFY pop_id int");
   $dbVar->do("ALTER TABLE tmp_pop ADD INDEX pop_id(pop_id)");
-
+  $dbVar->do("ALTER TABLE tmp_pop MODIFY count int");
+  $dbVar->do("ALTER TABLE tmp_pop ADD INDEX count_idx(count)");
 
   debug("Creating population table");
 
   # load PopClasses as parent populations
-  $dbVar->do(qq{INSERT INTO population (name, pop_class_id)
-                SELECT name, pop_class_id from tmp_pop_class});
+  $dbVar->do(qq{INSERT INTO population (name, pop_class_id, description,
+                                        population_type)
+                SELECT name, pop_class_id, description, 'general'
+                FROM   tmp_pop_class});
 
-  # set the parent pop_id of the other populations
+  # set the parent pop_id of the other populations,
+  # take only populations with one parent
 
   $dbVar->do(qq(CREATE table tmp_pop2
                 SELECT tp.name as name, tp.pop_id as pop_id,
                        p.population_id as parent_population_id
                 FROM   tmp_pop tp, population p
-                WHERE  tp.pop_class_id = p.pop_class_id));
+                WHERE  tp.pop_class_id = p.pop_class_id
+                AND    tp.count = 1));
 
-  $dbVar->do(qq{INSERT INTO population (name, parent_population_id, pop_id)
-                SELECT name, parent_population_id, pop_id
+  $dbVar->do(qq{INSERT INTO population (name, parent_population_id, pop_id,
+                                        population_type)
+                SELECT name, parent_population_id, pop_id, 'general'
                 FROM   tmp_pop2});
+
+  # take all populations with multiple parent classes
+  # and name them MULTI-NATIONAL
+
+  $dbVar->do("DROP TABLE tmp_pop2");
+
+  $dbVar->do(qq(CREATE table tmp_pop2
+                SELECT tp.name as name, tp.pop_id as pop_id,
+                       p.population_id as parent_population_id
+                FROM   tmp_pop tp, population p
+                WHERE  p.name = 'MULTI-NATIONAL'
+                AND    tp.count > 1));
+
+  $dbVar->do(qq{INSERT INTO population (name, parent_population_id, pop_id,
+                                       population_type)
+                SELECT name, parent_population_id, pop_id, 'general'
+                FROM   tmp_pop2});
+
+
 
   $dbVar->do("DROP TABLE tmp_pop_class");
   $dbVar->do("DROP TABLE tmp_pop");
@@ -313,7 +372,7 @@ sub flanking_sequence_table {
 sub variation_feature {
 
   ### TBD not sure if variations with map_weight > 1 or 2 should be
-  ### imported
+  ### imported. If they are then the map_weight needs to be set.
 
   debug("Dumping seq_region data");
 
@@ -355,9 +414,66 @@ sub variation_feature {
 
   $dbVar->do("DROP TABLE tmp_contig_loc");
 
-  # need to fix strands of zero, push snps to toplevel and check
+  # TBD need to fix strands of zero, push snps to toplevel and check
   # that flanking sequence seems right
 
+}
+
+#
+# loads variation_group and variation_group_variation tables from the
+# contents of the HapSet and HapSetSnpList tables
+#
+sub variation_group {
+  debug("Dumping HapSet data");
+
+  dumpSQL(qq{SELECT CONCAT(handle, ':', hapset_name), 1, hapset_id
+             FROM HapSet});
+
+  $dbVar->do("ALTER TABLE variation_group add column hapset_id int");
+
+  debug("Loading variation_group");
+
+  load('variation_group', 'name', 'source_id', 'hapset_id');
+
+  dumpSQL(qq{SELECT hapset_id, subsnp_id
+             FROM   HapSetSnpList});
+
+  create_and_load('tmp_variation_group', 'hapset_id', 'subsnp_id');
+
+  $dbVar->do(qq{INSERT INTO variation_group_variation
+                     (variation_group_id, variation_id)
+                SELECT vg.variation_group_id, v.variation_id
+                FROM   variation_group vg, variation v, tmp_variation_group tvg
+                WHERE  tvg.hapset_id = vg.hapset_id
+                AND    tvg.subsnp_id = v.subsnp_id});
+
+  $dbVar->do("DROP TABLE tmp_variation_group");
+}
+
+#
+# loads allele_group table
+#
+sub allele_group {
+  debug("Dumping Hap data");
+
+  dumpSQL(qq{SELECT hap_id, hapset_id, loc_hap_id
+             FROM   Hap});
+
+  $dbVar->do(qq{ALTER TABLE allele_group ADD COLUMN hap_id int});
+
+  create_and_load('tmp_allele_group', 'hap_id', 'hapset_id', 'name');
+
+  $dbVar->do(qq{INSERT INTO allele_group (variation_group_id, name, source_id,
+                                          hap_id)
+                SELECT vg.variation_group_id, tag.name, 1, tag.hap_id
+                FROM   variation_group vg, tmp_allele_group tag
+                WHERE  vg.hapset_id = tag.hapset_id});
+
+#  dumpSQL(qq{SELECT hap_id, subsnp_id, snp_allele
+#             FROM   Hap});
+            
+
+  $dbVar->do("DROP TABLE tmp_allele_group");
 }
 
 
@@ -371,6 +487,8 @@ sub cleanup {
   $dbVar->do('ALTER TABLE variation  DROP COLUMN subsnp_id');
   $dbVar->do('ALTER TABLE population DROP COLUMN pop_class_id');
   $dbVar->do('ALTER TABLE population DROP COLUMN pop_id');
+  $dbVar->do('ALTER TABLE variation_group DROP COLUMN hapset_id');
+  $dbVar->do('ALTER TABLE allele_group DROP COLUMN hap_id');
 }
 
 
