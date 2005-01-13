486638c37ebbf19add328d3909c62ece4a609961
Yuan Chen <yuan@sanger.ac.uk>
2005-01-13 16:36:07 +0000

    excluding snps with more than 2 mappings

--- scripts/import/load_mapping2vf.pl
+++ scripts/import/load_mapping2vf.pl
@@ -7,7 +7,7 @@ use DBI;
 use DBH;
 use Getopt::Long;
 use Benchmark;
-use Bio::EnsEMBL::DBSQL::DBAdaptor;
+use Bio::EnsEMBL::DBSQL::DBConnection;
 use ImportUtils qw(dumpSQL debug create_and_load load );
 
 my ($TAX_ID, $LIMIT_SQL, $CONTIG_SQL, $TMP_DIR, $TMP_FILE);
@@ -15,7 +15,6 @@ my ($TAX_ID, $LIMIT_SQL, $CONTIG_SQL, $TMP_DIR, $TMP_FILE);
 my $dbSNP;
 my $dbVar;
 my $dbCore;
-my %rec;
 
 {
   my($dshost, $dsuser, $dspass, $dsport, $dsdbname, # dbSNP db
@@ -39,7 +38,7 @@ my %rec;
              'limit=i'   => \$limit);
   
   $vhost    ||= 'ecs2';
-  $vport    ||= 3361;
+  $vport    ||= 3366;
   $vuser    ||= 'ensadmin';
   
   $chost    ||= 'ecs2';
@@ -61,7 +60,7 @@ my %rec;
     {'RaiseError' => 1});
   die("Could not connect to variation database: $!") if(!$dbVar);
 
-  $dbCore = Bio::EnsEMBL::DBSQL::DBAdaptor->new
+  $dbCore = Bio::EnsEMBL::DBSQL::DBConnection->new
     (-host   => $chost,
      -user   => $cuser,
      -pass   => $cpass,
@@ -76,14 +75,18 @@ sub variation_feature {
   
   my $alldiff_file = shift;
 
+  my (%rec, %source, %status, %rec_hits, %rec_line);
+
   debug("Dumping Variation");
   
-  my $sth = $dbVar->prepare (qq{SELECT variation_id, name
-                                FROM variation});
+  my $sth = $dbVar->prepare (qq{SELECT variation_id, name, source_id, validation_status
+                                  FROM variation});
   $sth->execute();
 
-  while(my ($variation_id, $name) = $sth->fetchrow_array()) {
+  while(my ($variation_id, $name, $source_id, $validation_status) = $sth->fetchrow_array()) {
     $rec{$name} = $variation_id;
+    $source{$name} = $source_id;
+    $status{$name} = $validation_status;
   }
 
   $sth->finish();
@@ -92,15 +95,24 @@ sub variation_feature {
   open ( FH, ">$TMP_DIR/$TMP_FILE" );
 
   while (<IN>) {
-    s/^MORE_HITS//;
+    chomp;
+    s/^MORE_HITS\s+//;
     my ($ref_id, $slice_name, $ver, $start, $end, $exact, $snp_type, $strand, $score, $ratio) =split;
-    next if $ratio <0.5;
-
-    ##make start < end if it is a between type
+    $rec_hits{$ref_id}++ if $ref_id;
+    $rec_line{$ref_id} = $_ if $ref_id;
+  }
+  
+  foreach my $key (keys %rec_line) {
+    next if $rec_hits{$key} >2;
+    
+    my ($ref_id, $slice_name, $ver, $start, $end, $exact, $snp_type, $strand, $score, $ratio) =split /\s+/,$rec_line{$key};
+    next if $ratio <0.7;
+    
+    ##make start > end if it is a between type
     if ($exact =~ /between/i) {
       $end = $start-1;
     }
-
+    
     my ($coord_sys,$assembly,$seq_region_name,$seq_region_start,$seq_region_end,$version) = split /\:/, $slice_name
       if $slice_name =~ /\:/;
     
@@ -108,12 +120,17 @@ sub variation_feature {
     $sth->execute("$seq_region_name");
     
     my $seq_region_id = $sth->fetchrow_array();
+    
+    if (!$seq_region_id) {
+      warn "There is no seq_region_id for $ref_id\n";
+      next;
+    }
 
     my $new_seq_region_start = $seq_region_start + $start -1 if ($seq_region_start);
     my $new_seq_region_end = $seq_region_start + $end -1 if ($seq_region_start);
     
     $ref_id = "rs$ref_id";
-    print FH "$seq_region_id\t$new_seq_region_start\t$new_seq_region_end\t$strand\t$rec{$ref_id}\t$ref_id\n";
+    print FH "$seq_region_id\t$new_seq_region_start\t$new_seq_region_end\t$strand\t$rec{$ref_id}\t$ref_id\t$source{$ref_id}\t$status{$ref_id}\n";
   }
   
   $sth->finish();
@@ -122,7 +139,7 @@ sub variation_feature {
   close FH;
   
   load($dbVar, "variation_feature","seq_region_id","seq_region_start","seq_region_end",
-       "seq_region_strand","variation_id","variation_name");
+       "seq_region_strand","variation_id","variation_name","source_id", "validation_status");
 }
 
 ###
