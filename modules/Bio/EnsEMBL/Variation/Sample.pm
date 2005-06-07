2f80ba102af3abfaf73091551168fb177e1a2cde
Daniel Rios <dr2@sanger.ac.uk>
2005-06-07 13:52:25 +0000

    new base class Sample to merge Individual and Population

---
+++ modules/Bio/EnsEMBL/Variation/Sample.pm
@@ -0,0 +1,103 @@
+# Ensembl module for Bio::EnsEMBL::Variation::Sample
+#
+# Copyright (c) 2005 Ensembl
+#
+
+
+=head1 NAME
+
+Bio::EnsEMBL::Variation::Sample - An abstract base class to represent
+Population, Individual or Strain
+
+
+=head1 SYNOPSIS
+
+Abstract class - should not be instantiated.  Implementation of
+abstract methods must be performed by subclasses.
+
+=head1 DESCRIPTION
+
+This is a base class representing population, individual and strain. This base
+class is simply a way of merging similar concepts that should have the same ID
+
+
+=head1 CONTACT
+
+Post questions to the Ensembl development list: ensembl-dev@ebi.ac.uk
+
+=head1 METHODS
+
+=cut
+
+use strict;
+use warnings;
+
+package Bio::EnsEMBL::Variation::Sample;
+
+use Bio::EnsEMBL::Storable;
+use Bio::EnsEMBL::Utils::Argument qw(rearrange);
+use Bio::EnsEMBL::Utils::Exception qw(throw);
+
+our @ISA = ('Bio::EnsEMBL::Storable');
+
+
+=head2 name
+
+  Arg [1]    : string $newval (optional)
+               The new value to set the name attribute to
+  Example    : $name = $obj->name()
+  Description: Getter/Setter for the name attribute
+  Returntype : string
+  Exceptions : none
+  Caller     : general
+
+=cut
+
+sub name{
+  my $self = shift;
+  return $self->{'name'} = shift if(@_);
+  return $self->{'name'};
+}
+
+
+
+=head2 description
+
+  Arg [1]    : string $newval (optional) 
+               The new value to set the description attribute to
+  Example    : $description = $obj->description()
+  Description: Getter/Setter for the description attribute
+  Returntype : string
+  Exceptions : none
+  Caller     : general
+
+=cut
+
+sub description{
+  my $self = shift;
+  return $self->{'description'} = shift if(@_);
+  return $self->{'description'};
+}
+
+
+
+=head2 size
+
+  Arg [1]    : int $newval (optional) 
+               The new value to set the size attribute to
+  Example    : $size = $obj->size()
+  Description: Getter/Setter for the size attribute
+  Returntype : int
+  Exceptions : none
+  Caller     : general
+
+=cut
+
+sub size{
+  my $self = shift;
+  return $self->{'size'} = shift if(@_);
+  return $self->{'size'};
+}
+
+
+1;
