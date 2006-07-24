08431e941e3ffc631daa29a19c728e660acc67bf
Yuan Chen <yuan@sanger.ac.uk>
2006-07-24 13:53:32 +0000

    when fetch seq_region_id,name, only fetch them from current assembly, also for human, araprt from fetch reference snps, also fetch snps from haplotype chromosomes

--- scripts/import/dbSNP/GenericChromosome.pm
+++ scripts/import/dbSNP/GenericChromosome.pm
@@ -23,6 +23,7 @@ sub variation_feature{
 				      if (sr.name like "E%", CONCAT("LG",sr.name),sr.name) ##add LG for chicken
 				      FROM   seq_region sr, coord_system cs
 				      WHERE cs.name = 'chromosome'
+                                      AND cs.attrib like "default%"
 				      AND cs.name not like "%random"
 				      AND cs.coord_system_id = sr.coord_system_id});
 
@@ -32,6 +33,7 @@ sub variation_feature{
     debug("Dumping SNPLoc data");
     
     my ($tablename1,$tablename2,$row);
+
 
     my ($assembly_version) =  $self->{'assembly_version'} =~ /^[a-zA-Z]+(\d+)\.*.*$/;
     my $sth = $self->{'dbSNP'}->prepare(qq{SHOW TABLES LIKE 
@@ -51,48 +53,49 @@ sub variation_feature{
     }
     print "table_name1 is $tablename1 table_name2 is $tablename2\n";
     #my $tablename = $self->{'species_prefix'} . 'SNPContigLoc';
-    dumpSQL($self->{'dbSNP'}, qq{SELECT t1.snp_id, t2.contig_chr, 
+    dumpSQL($self->{'dbSNP'}, qq{SELECT t1.snp_id, 
+				 IF(t2.contig_label = 'reference',t2.contig_chr,t2.contig_label), 
 				 IF(t1.loc_type = 3, t1.phys_pos_from+2, t1.phys_pos_from+1),
 				 IF(t1.loc_type = 3,  t1.phys_pos_from+1, t1.phys_pos_from+length(t1.allele)),
 				 IF(t1.orientation, -1, 1)
 				 FROM $tablename1 t1, $tablename2 t2 
 				 WHERE t1.ctg_id = t2.ctg_id
-				 AND t2.group_term like "ref_%"
+				 #AND t2.group_term like "ref_%"
 				 $self->{'limit'}});
     
     
-    debug("Loading SNPLoc data");
+   debug("Loading SNPLoc data");
     
-    create_and_load($self->{'dbVariation'}, "tmp_contig_loc", "snp_id i*", "chr *", "start i", 
+    create_and_load($self->{'dbVariation'}, "tmp_contig_loc_chrom", "snp_id i*", "chr *", "start i", 
 		    "end i", "strand i");
+#
+#     debug("Creating genotyped variations");
+#     #creating the temporary table with the genotyped variations
 
-    debug("Creating genotyped variations");
-    #creating the temporary table with the genotyped variations
-
-    $self->{'dbVariation'}->do(qq{CREATE TABLE tmp_genotyped_var SELECT DISTINCT variation_id FROM tmp_individual_genotype_single_bp});
-    $self->{'dbVariation'}->do(qq{CREATE UNIQUE INDEX variation_idx ON tmp_genotyped_var (variation_id)});
-    $self->{'dbVariation'}->do(qq{INSERT IGNORE INTO tmp_genotyped_var SELECT DISTINCT variation_id FROM individual_genotype_multiple_bp});
+     $self->{'dbVariation'}->do(qq{CREATE TABLE tmp_genotyped_var SELECT DISTINCT variation_id FROM tmp_individual_genotype_single_bp});
+     $self->{'dbVariation'}->do(qq{CREATE UNIQUE INDEX variation_idx ON tmp_genotyped_var (variation_id)});
+     $self->{'dbVariation'}->do(qq{INSERT IGNORE INTO tmp_genotyped_var SELECT DISTINCT variation_id FROM individual_genotype_multiple_bp});
 
     debug("Creating tmp_variation_feature data");
     
     dumpSQL($self->{'dbVariation'},qq{SELECT v.variation_id, ts.seq_region_id, tcl.start, tcl.end, tcl.strand, v.name, v.source_id, v.validation_status
-					  FROM variation v, tmp_contig_loc tcl, tmp_seq_region ts
+					  FROM variation v, tmp_contig_loc_chrom tcl, tmp_seq_region ts
 					  WHERE v.snp_id = tcl.snp_id
 					  AND tcl.chr = ts.name });
 
-    create_and_load($self->{'dbVariation'},'tmp_variation_feature',"variation_id *","seq_region_id", "seq_region_start", "seq_region_end", "seq_region_strand", "variation_name", "source_id", "validation_status");
+    create_and_load($self->{'dbVariation'},'tmp_variation_feature_chrom',"variation_id *","seq_region_id", "seq_region_start", "seq_region_end", "seq_region_strand", "variation_name", "source_id", "validation_status");
     
     debug("Dumping data into variation_feature table");
-    $self->{'dbVariation'}->do(qq{INSERT INTO variation_feature (variation_id, seq_region_id,seq_region_start, seq_region_end, seq_region_strand,
+    $self->{'dbVariation'}->do(qq{INSERT INTO variation_feature_chrom (variation_id, seq_region_id,seq_region_start, seq_region_end, seq_region_strand,
 								 variation_name, flags, source_id, validation_status)
 				      SELECT tvf.variation_id, tvf.seq_region_id, tvf.seq_region_start, tvf.seq_region_end, tvf.seq_region_strand,
 				      tvf.variation_name,IF(tgv.variation_id,'genotyped',NULL), tvf.source_id, tvf.validation_status
-				      FROM tmp_variation_feature tvf LEFT JOIN tmp_genotyped_var tgv ON tvf.variation_id = tgv.variation_id
+				      FROM tmp_variation_feature_chrom tvf LEFT JOIN tmp_genotyped_var tgv ON tvf.variation_id = tgv.variation_id
 				  });
-    #$self->{'dbVariation'}->do("DROP TABLE tmp_contig_loc");
-    #$self->{'dbVariation'}->do("DROP TABLE tmp_seq_region");
-    #$self->{'dbVariation'}->do("DROP TABLE tmp_genotyped_var");
-    #$self->{'dbVariation'}->do("DROP TABLE tmp_variation_feature");
+    $self->{'dbVariation'}->do("DROP TABLE tmp_contig_loc");
+    $self->{'dbVariation'}->do("DROP TABLE tmp_seq_region");
+    $self->{'dbVariation'}->do("DROP TABLE tmp_genotyped_var");
+    $self->{'dbVariation'}->do("DROP TABLE tmp_variation_feature");
     #for the chicken, delete 13,000 SNPs that cannot be mapped to EnsEMBL coordinate
     if ($self->{'dbCore'}->species =~ /gga/i){
 	$self->{'dbVariation'}->do("DELETE FROM variation_feature WHERE seq_region_end = -1");
