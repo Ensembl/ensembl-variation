78a7510fc0f73d50b271d760b1ae5982741b756a
Daniel Rios <dr2@sanger.ac.uk>
2006-09-12 14:48:39 +0000

    patch to upgrade the schema to support new design of sample,individual, population and strain

---
+++ sql/patch_40_41_a.sql
@@ -0,0 +1,36 @@
+#this patch is suposed to change the sample, individual and population schema
+
+#delete from the Allele table the alleles that belong to strains
+DELETE a FROM allele a, population p where p.sample_id = a.sample_id and p.is_strain = 1;
+#delete from Individual_population table the relation between population->individual
+DELETE ip FROM individual_population ip, population p where p.sample_id = ip.population_sample_id and p.is_strain = 1;
+#and delete from the Sample table all strains
+DELETE s FROM sample s, population p where s.sample_id = p.sample_id and p.is_strain = 1;
+#and the strains from the population table
+DELETE p FROM population p where p.is_strain = 1;
+#remove the is_strain column from the population table
+ALTER TABLE population drop column is_strain;
+#create new individual_type table
+create table individual_type(
+  individual_type_id int(0) unsigned not null auto_increment,
+  name varchar(255) not null,
+  description text,
+  
+  primary key (individual_type_id)
+);
+
+#this table will always contain the same values
+
+INSERT INTO individual_type (name,description) VALUES ('fully_inbred','multiple organisms have the same genome sequence');
+INSERT INTO individual_type (name,description) VALUES ('partly_inbred','single organisms have reduced genome variability due to human intervention');
+INSERT INTO individual_type (name,description) VALUES ('outbred','a single organism which breeds freely');
+INSERT INTO individual_type (name,description) VALUES ('mutant','a single or multiple organisms with the same genome sequence that have a natural or experimentally induced mutation');
+
+#modify table Individual with a new column
+ALTER TABLE individual add column individual_type_id int(10) unsigned not null;
+#we have to set the value of individual_type_id in the individual table depending on the specie
+UPDATE individual set individual_type_id = 1 where (database() like 'mus%');
+UPDATE individual set individual_type_id = 2 where (database() like 'canis%' or database() like 'danio%' or database() like 'gallus%' or database() like 'rattus%');
+UPDATE individual set individual_type_id = 3 where (database() like 'homo%' or database() like 'anopheles%');
+
+INSERT INTO meta (meta_key,meta_value) VALUES ('patch','patch_40_41_a.sql|change population/individual schema');
