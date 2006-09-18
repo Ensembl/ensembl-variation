8fed36bc30274e7f73355c2df0306309de7be00e
Daniel Rios <dr2@sanger.ac.uk>
2006-09-18 15:56:18 +0000

    modifed to use the on the fly calculation of the LD values

--- scripts/import/select_tag_snps.pl
+++ scripts/import/select_tag_snps.pl
@@ -4,60 +4,58 @@ use warnings;
 
 use Getopt::Long;
 use Bio::EnsEMBL::Variation::DBSQL::DBAdaptor;
+use Bio::EnsEMBL::Variation::DBSQL::LDFeatureContainerAdaptor;
+use FindBin qw( $Bin );
+use Data::Dumper;
 
-my ($TMP_DIR);
+my ($TMP_DIR, $population_id, $species);
 
-my ($vhost, $vport, $vdbname, $vuser, $vpass,
-    $population_id);
 
-GetOptions('vhost=s'   => \$vhost,
-	   'vuser=s'   => \$vuser,
-	   'vpass=s'   => \$vpass,
-	   'vport=i'   => \$vport,
-	   'vdbname=s' => \$vdbname,
+GetOptions('species=s' => \$species,
 	   'tmpdir=s'  => \$TMP_DIR,
 	   'population_id=i' => \$population_id);
 
+warn("Make sure you have a updated ensembl.registry file!\n");
 
-my $dbVariation = Bio::EnsEMBL::Variation::DBSQL::DBAdaptor->new
-    (-host => $vhost,
-     -user => $vuser,
-     -pass => $vpass,
-     -port => $vport,
-     -dbname => $vdbname
-     );
+my $registry_file ||= $Bin . "/ensembl.registry";
 
+#added default options
+$species ||= 'human';
+Bio::EnsEMBL::Registry->load_all( $registry_file );
+
+my $dbVariation = Bio::EnsEMBL::Registry->get_DBAdaptor($species,'variation');
+my $dbCore = Bio::EnsEMBL::Registry->get_DBAdaptor($species,'core');
+
+#first, copy the file locally
+system("lsrcp ecs4a:$TMP_DIR/$ARGV[0] /tmp/$ARGV[0]");
 #first, calculate the MAF for the SNPs in the chromosome
-my $file = $ARGV[0] if (defined @ARGV);
+my $file = "/tmp/$ARGV[0]" if (defined @ARGV);
 die "Not possible to calculate SNP tagging without file with SNPs" if (!defined @ARGV);
 
-`sort -k 1 -o $file $file`; #order snps by id
+`sort -k 3 -o $file $file`; #order snps by position
 
-open IN, "$file" or die("Could not open input file: $ARGV[0]\n");
+open IN, "<$file" or die("Could not open input file: $file\n");
 
 my $r2 = 0.99; #default value for r2
 
-my $previous_variation_feature = 0;
-my $previous_seq_region_start;
-my ($variation_feature_id, $allele_1, $allele_2, $seq_region_start);
+my $previous_seq_region_start = 0;
+my ($allele_1, $allele_2, $seq_region_start);
 my $genotypes_snp = {};
 my $last_snp = 0; #to indicate if there is a last snp that needs the MAF calculation
 my $MAF_snps = {};
 while (<IN>){
     chomp;
     /^\#/ && next;
-    ($variation_feature_id, $allele_1, $allele_2, $seq_region_start) = split;
+    ($allele_1, $allele_2, $seq_region_start) = split;
     #when we change variation_feature, calculate the MAF
-    if (($previous_variation_feature != $variation_feature_id) && $previous_variation_feature != 0){
-	$MAF_snps->{$previous_variation_feature} = &calculate_MAF($genotypes_snp) . '-' . $previous_seq_region_start;
+    if (($previous_seq_region_start != $seq_region_start) && $previous_seq_region_start != 0){
+	$MAF_snps->{$previous_seq_region_start} = &calculate_MAF($genotypes_snp) . '-' . $previous_seq_region_start;
 	$genotypes_snp = {};
-	$previous_variation_feature = $variation_feature_id;
 	$previous_seq_region_start = $seq_region_start;
 	$last_snp = 1;
 
     }
-    if ($previous_variation_feature == 0){ #initialize the variables
-	$previous_variation_feature = $variation_feature_id;
+    if ($previous_seq_region_start == 0){ #initialize the variables
 	$previous_seq_region_start = $seq_region_start;
     }
     $genotypes_snp->{$allele_1}++ if ($allele_1 ne 'N');
@@ -65,30 +63,62 @@ while (<IN>){
     $last_snp = 0;
 }
 close IN;
+
 #calculate the MAF for the last SNP, if necessary
-$MAF_snps->{$previous_variation_feature} = &calculate_MAF($genotypes_snp) . '-' . $previous_seq_region_start if ($last_snp == 0);
-
-
+$MAF_snps->{$previous_seq_region_start} = &calculate_MAF($genotypes_snp) . '-' . $previous_seq_region_start if ($last_snp == 0);
 
 #get LD values for the chromosome
-$file =~/.*:(\d+)\.txt/; #extract the seq_region_id from the name of the file
-my $LD_values = &get_LD_chromosome($dbVariation,$1,$r2,$population_id);
-#do the algorithm
-my $remove_snps = {}; #hash containing the snps that must be removed from the entry, they have been ruled out
-foreach $variation_feature_id (sort {$MAF_snps->{$b} cmp $MAF_snps->{$a}} keys %{$MAF_snps}){
-    if (!defined $remove_snps->{$variation_feature_id}){
-	#add the SNPs that should be removed in future iterations
-	#and delete from the hash with the MAF_snps, the ones that have a r2 greater than r2 with $variation_feature_id
-	map {$remove_snps->{$_}++;delete $MAF_snps->{$_}} @{$LD_values->{$variation_feature_id}};
-    }
-}
+$file =~/.*-(\d+)\.txt/; #extract the seq_region_id from the name of the file
+my $seq_region_id = $1;
+my $pos_to_vf = {};
 my $host = `hostname`;
 chop $host;
-open OUT, ">$TMP_DIR/snps_tagged_$population_id\_$host\:$$\.txt" or die ("Could not open output file");
-map {print OUT join("\t",$_,$population_id),"\n"} keys %{$MAF_snps};
+my $LD_values = &get_LD_chromosome($dbCore,$dbVariation,$seq_region_id,$r2,$population_id, $pos_to_vf);
+#do the algorithm
+my $remove_snps = {}; #hash containing the snps that must be removed from the entry, they have been ruled out
+foreach $seq_region_start (sort {$MAF_snps->{$b} cmp $MAF_snps->{$a}} keys %{$MAF_snps}){
+    if (!defined $remove_snps->{$seq_region_start}){
+	#add the SNPs that should be removed in future iterations
+	#and delete from the hash with the MAF_snps, the ones that have a r2 greater than r2 with $variation_feature_id
+	map {$remove_snps->{$_}++;delete $MAF_snps->{$_}} @{$LD_values->{$seq_region_start}};
+    }
+}
+my $genotype_without_vf = 0;
+open OUT, ">/tmp/snps_tagged_$population_id\_$host\-$$\.txt" or die ("Could not open output file");
+foreach my $position_vf (keys %{$MAF_snps}){
+    if (! defined $pos_to_vf->{$position_vf}){ #some variations might not have LD, get dbID from database
+	#get it from the database
+	$pos_to_vf->{$position_vf} = &get_vf_id_from_position($dbCore,$dbVariation,$seq_region_id,$position_vf);
+    }
+    if ($pos_to_vf->{$position_vf} ne ''){
+	print OUT join("\t",$pos_to_vf->{$position_vf},$population_id),"\n";
+    }
+    else{
+	$genotype_without_vf++;
+    }
+}
+print "Genotypes without vf $genotype_without_vf\n";
 close OUT or die ("Could not close output file with tagged SNPs");
+system("lsrcp /tmp/snps_tagged_$population_id\_$host\-$$\.txt ecs4a:$TMP_DIR/snps_tagged_$population_id\_$host\-$$\.txt");
+system("rm -r /tmp/snps_tagged_$population_id\_$host\-$$\.txt");
+system("rm -r $file");
 
+#for a given position retrieve the vf_id from the database
+sub get_vf_id_from_position{
+    my $dbCore = shift;
+    my $dbVariation = shift;
+    my $seq_region_id = shift;
+    my $seq_region_start = shift;
 
+    my $slice_adaptor = $dbCore->get_SliceAdaptor;
+    my $vf_adaptor = $dbVariation->get_VariationFeatureAdaptor;
+    my $slice = $slice_adaptor->fetch_by_seq_region_id($seq_region_id); #get the Slice
+    my $sub_Slice = $slice->sub_Slice($seq_region_start,$seq_region_start,1); 
+    my $vfs = $vf_adaptor->fetch_all_by_Slice($sub_Slice) if (defined $sub_Slice);
+    #there should be just one
+    return $vfs->[0]->dbID if defined $vfs->[0];
+    return ''; #no vf in the position ??
+}
 #for a list of genotypes, get the MAF
 sub calculate_MAF{
     my $genotypes_snp = shift;
@@ -110,25 +140,33 @@ sub calculate_MAF{
 
 #creates a hash with all the variation_features in the chromosome with a r2 greater than r2
 sub get_LD_chromosome{
+    my $dbCore = shift;
     my $dbVariation = shift;
     my $seq_region_id = shift;
     my $r2 = shift;
     my $population_id = shift;
+    my $pos_to_vf = shift;
     
     my $variation_features = {};
-    my $sth = $dbVariation->dbc->prepare(qq{SELECT variation_feature_id_1, variation_feature_id_2
-						FROM pairwise_ld
-						WHERE seq_region_id = ?
-						AND r2 > ?
-						AND sample_id = ?
-					    },{mysql_use_result =>1});
-    $sth->execute($seq_region_id,$r2,$population_id);
-    my ($variation_feature_id_1, $variation_feature_id_2);
-    $sth->bind_columns(\$variation_feature_id_1, \$variation_feature_id_2);
-    while ($sth->fetch()){
-	push @{$variation_features->{$variation_feature_id_1}}, $variation_feature_id_2;
-	push @{$variation_features->{$variation_feature_id_2}}, $variation_feature_id_1;
+    my $ld_adaptor = $dbVariation->get_LDFeatureContainerAdaptor();
+    my $slice_adaptor = $dbCore->get_SliceAdaptor;
+    my $population_adaptor = $dbVariation->get_PopulationAdaptor;
+    my $population = $population_adaptor->fetch_by_dbID($population_id); 
+    my $slice = $slice_adaptor->fetch_by_seq_region_id($seq_region_id);
+    my $ld_container = $ld_adaptor->fetch_by_Slice($slice, $population);
+ #returns a list with vf1 vf2 r2 sample_id
+    my ($vf1,$vf2);
+    foreach my $ld_key (keys %{$ld_container->{'ldContainer'}}){
+	foreach my $sample_id (keys %{$ld_container->{'ldContainer'}->{$ld_key}}){		    
+	    ($vf1,$vf2) = split /-/, $ld_key;
+	    $pos_to_vf->{$ld_container->{'variationFeatures'}->{$vf1}->start} = $vf1;
+	    $pos_to_vf->{$ld_container->{'variationFeatures'}->{$vf2}->start} = $vf2;
+	    if (($ld_container->{'ldContainer'}->{$ld_key}->{$sample_id}->{'r2'} > $r2) and ($sample_id == $population_id)){
+		#there is high LD between these 2 variations in this population
+		push @{$variation_features->{$ld_container->{'variationFeatures'}->{$vf1}->start}}, $ld_container->{'variationFeatures'}->{$vf2}->start;
+		push @{$variation_features->{$ld_container->{'variationFeatures'}->{$vf2}->start}}, $ld_container->{'variationFeatures'}->{$vf1}->start;
+	    }
+	}
     }
-    $sth->finish();
     return $variation_features;
 }
--- scripts/import/tag_snps.pl
+++ scripts/import/tag_snps.pl
@@ -8,39 +8,28 @@ use Bio::EnsEMBL::Variation::DBSQL::DBAdaptor;
 use ImportUtils qw(load);
 use FindBin qw( $Bin );
 
-my ($TMP_DIR, $TMP_FILE);
-
-my ($species, $vhost, $vport, $vdbname, $vuser, $vpass);
+my ($TMP_DIR, $TMP_FILE, $species);
 
 
-
-GetOptions(#'vhost=s'   => \$vhost,
-	   #'vuser=s'   => \$vuser,
-	   #'vpass=s'   => \$vpass,
-	   #'vport=i'   => \$vport,
-	   #'vdbname=s' => \$vdbname,
-	   'species=s' => \$species,
+GetOptions('species=s' => \$species,
 	   'tmpdir=s'  => \$ImportUtils::TMP_DIR,
 	   'tmpfile=s' => \$ImportUtils::TMP_FILE);
+
 warn("Make sure you have a updated ensembl.registry file!\n");
 
 my $registry_file ||= $Bin . "/ensembl.registry";
 
 Bio::EnsEMBL::Registry->load_all( $registry_file );
 
-my $dbVariation = Bio::EnsEMBL::Registry->get_DBAdaptor($species,'variation');
-
-
 #added default options
-
-$vhost = $dbVariation->dbc->host;
-$vport = $dbVariation->dbc->port;
-$vuser    ||= 'ensadmin';
-$vdbname = $dbVariation->dbc->dbname;
-$vpass = $dbVariation->dbc->password;
+$species ||= 'human';
 
 $TMP_DIR  = $ImportUtils::TMP_DIR;
 $TMP_FILE = $ImportUtils::TMP_FILE;
+
+
+my $dbVariation = Bio::EnsEMBL::Registry->get_DBAdaptor($species,'variation');
+my $dbCore = Bio::EnsEMBL::Registry->get_DBAdaptor($species,'core');
 
 my $population_id;
 my $population_name;
@@ -67,38 +56,76 @@ while($sth->fetch){
 my $in_str = " IN (" . join(',', @pops). ")";
 
 #select all SNPs in the population given with genotypes, and order them by variation
-$sth = $dbVariation->dbc->prepare(qq{SELECT vf.variation_feature_id, ig.allele_1, ig.allele_2, vf.seq_region_id, 
-				            vf.seq_region_start, ip.individual_sample_id, ip.population_sample_id
-				     FROM individual_genotype_single_bp ig, variation_feature vf, individual_population ip
-				     WHERE ig.variation_id = vf.variation_id
-				     AND ig.sample_id = ip.individual_sample_id
-				     AND vf.map_weight = 1
-				     AND ip.population_sample_id $in_str
-				 },{mysql_use_result =>1});
-my ($variation_feature_id, $allele_1, $allele_2, $seq_region_id, $seq_region_start,$individual_id);
+$sth = $dbVariation->dbc->prepare(qq{SELECT STRAIGHT_JOIN c.genotypes, c.seq_region_id, 
+				            c.seq_region_start, ip.individual_sample_id, ip.population_sample_id
+						FROM compressed_genotype_single_bp c FORCE INDEX(pos_idx), individual_population ip
+						WHERE c.sample_id = ip.individual_sample_id
+						AND   ip.population_sample_id $in_str
+						ORDER BY c.seq_region_id,c.seq_region_start
+					    },{mysql_use_result=>1});
+
+my ($genotypes, $seq_region_id, $seq_region_start,$individual_id);
 my $buffer = {}; #buffer with data to write to file
+my $blob; # contains the genotype information compressed
+my @genotypes = ();
+my $seq_region = {};
 $sth->execute();
-$sth->bind_columns(\$variation_feature_id, \$allele_1, \$allele_2, \$seq_region_id, \$seq_region_start,\$individual_id,\$population_id);
+$sth->bind_columns(\$genotypes, \$seq_region_id, \$seq_region_start,\$individual_id,\$population_id);
 while ($sth->fetch()){
+    $seq_region->{$seq_region_id}++; #store the seq_region_id
     #only print genotypes without parents genotyped
     if (!exists $siblings->{$population_id.'-'.$individual_id}){
-	#print the data to the file with the information in the chromosome
-	print_buffered($buffer,"$TMP_DIR/tag_snps_$population_id\:$seq_region_id\.txt",
-		       join("\t", $variation_feature_id, $allele_1, $allele_2, $seq_region_start) . "\n");
+	#uncompress the genotypes
+	$blob = substr($genotypes,2);
+	@genotypes = unpack("naa" x (length($blob)/4),$blob);
+
+	unshift @genotypes, substr($genotypes,1,1); #add the second allele of the first genotype
+	unshift @genotypes, substr($genotypes,0,1); #add the first allele of the first genotype
+	unshift @genotypes, 0; #the first SNP is in the position indicated by the seq_region1
+	my ($snp_start, $allele_1, $allele_2);
+	#print the information to the file
+	for (my $i=0; $i < @genotypes -1;$i+=3){
+	    if ($genotypes[$i] == 0){
+		$snp_start = $seq_region_start; #first SNP is in the beginning of the region
+	    }
+	    else{
+		$snp_start += $genotypes[$i] +1;
+	    }
+	    #genotype
+	    $allele_1 = $genotypes[$i+1];
+	    $allele_2 = $genotypes[$i+2];
+	    
+	    #print the data to the file with the information in the chromosome
+	    print_buffered($buffer,"$TMP_DIR/tag_snps_$population_id\-$seq_region_id\.txt",
+			   join("\t", $allele_1, $allele_2, $snp_start) . "\n");
+	}
     }
 }
 $sth->finish();
 print_buffered($buffer);
 my $call;
+my $file_name;
+my $chromosomes = &get_hugemem_chromosomes($dbCore,$seq_region);
 #when the data is selected in the files (one per chromosome), send the jobs to the farm
 foreach my $file (keys %{$buffer}){
-    $file =~ /tag_snps_(\d+)\:.*/; #extract the population_id from the name of the file
-    $call = "bsub -q normal -m 'bc_hosts ecs4_hosts' -J tag_snps_$1 ";
-    $call .= "-o $TMP_DIR/output_tag.txt /usr/local/ensembl/bin/perl select_tag_snps.pl $file ";
-    $call .= " -vhost $vhost -vuser $vuser -vport $vport -vdbname $vdbname -tmpdir $TMP_DIR ";
-    $call .= " -population_id $1 ";
-    
-    $call .= "-vpass $vpass " if ($vpass);
+    ($population_id) = $file =~ /tag_snps_(\d+)\-.*/; #extract the population_id from the name of the file
+    ($file_name) = $file =~ /\/(tag_snps_\d+.*)/;
+    ($seq_region_id) = $file =~ /tag_snps_\d+\-(\d+)\.txt/;
+    if (defined $chromosomes->{$seq_region_id} and $chromosomes->{$seq_region_id} eq 'hugemem'){
+	$call = "bsub -q hugemem -R'select[mem>10000] rusage[mem=10000]' -J tag_snps_$population_id ";
+	$call .= "/usr/local/bin/perl ";
+    }
+    elsif (defined $chromosomes->{$seq_region_id} and $chromosomes->{$seq_region_id} eq 'bigmem'){
+	$call = "bsub -q bigmem -R'select[mem>3500] rusage[mem=3500]' -J tag_snps_$population_id ";
+	$call .= "/usr/local/ensembl/bin/perl ";
+    }
+    else{
+	$call = "bsub -J tag_snps_$population_id ";
+	$call .= "/usr/local/ensembl/bin/perl ";
+    }
+    $call .= "/nfs/acari/dr2/projects/src/ensembl/ensembl-variation/scripts/import/select_tag_snps.pl $file_name ";
+    $call .= " -tmpdir $TMP_DIR -population_id $population_id -species $species";
+#    print $call,"\n";
     system($call); #send the job to one queue
     
 }
@@ -107,7 +134,26 @@ system($call);
 
 &import_tagg_snps($dbVariation);    
 
-
+#method that will return a hash with seq_region that need to be run in hugemem queue
+sub get_hugemem_chromosomes{
+    my $dbCore = shift;
+    my $seq_region = shift;
+    my $chromosomes = {};
+    my $slice_adaptor = $dbCore->get_SliceAdaptor;
+    my $slice;
+    my %huge_chromosomes = (10=>1,11=>1,7=>1,13=>1,12=>1,2=>1,3=>1,9=>1,8=>1,1=>1,4=>1);
+    my %big_chromosomes = (19=>1,5=>1,'c5_H2'=>1,'c6_COX'=>1,15=>1,21=>1,17=>1,16=>1,20=>1,18=>1,14=>1,6=>1,'X'=>1,'c6_QBL'=>1);
+    foreach my $seq_region_id (keys %{$seq_region}){
+	$slice = $slice_adaptor->fetch_by_seq_region_id($seq_region_id);
+	if (defined $huge_chromosomes{$slice->seq_region_name}){
+	    $chromosomes->{$seq_region_id} = 'hugemem';
+	}
+	elsif (defined $big_chromosomes{$slice->seq_region_name}){
+	    $chromosomes->{$seq_region_id} = 'bigmem';
+	}
+    }
+    return $chromosomes;
+}
 
 #for a given population, gets all individuals that are children (have father or mother)
 sub get_siblings{
@@ -179,11 +225,7 @@ sub usage {
 usage: perl tag_snps.pl <options>
 
 options:
-    -vhost <hostname>    hostname of variation MySQL database to write to
-    -vuser <user>        username of variation MySQL database to write to (default = ensadmin)
-    -vpass <pass>        password of variation MySQL database to write to
-    -vport <port>        TCP port of variation MySQL database to write to (default = 3306)
-    -vdbname <dbname>    dbname of variation MySQL database to write to
+    -species <name>      species name (default:human)
     -tmpdir <dir>        temp directory to use (with lots of space!)
     -tmpfile <filename>  name of temp file to use
 
