06adc508bd2a3b1e560bde21c9ed6600dad4e646
Daniel Rios <dr2@sanger.ac.uk>
2007-01-04 16:18:04 +0000

    removed data from tmp_individual_genotype_single_bp and a reminder to run compressed_genotype script

--- sql/remove_wrong_variations.sql
+++ sql/remove_wrong_variations.sql
@@ -5,6 +5,8 @@ DELETE f FROM flanking_sequence f, failed_variation v WHERE f.variation_id = v.v
 DELETE i FROM individual_genotype_multiple_bp i, failed_variation v WHERE i.variation_id = v.variation_id;
 DELETE vf FROM variation_feature vf, failed_variation v WHERE vf.variation_id = v.variation_id;
 DELETE vs FROM variation_synonym vs, failed_variation v WHERE vs.variation_id = v.variation_id;
+DELETE i FROM tmp_individual_genotype_single_bp i, failed_variation v WHERE i.variation_id = v.variation_id;
+SELECT 'Remember to run again compressed_genotype script to remove variations from the compressed table' as '';
 #after removing suspicious variations, update the Variation table to reflect changes
 UPDATE variation v, wrong_variation w set v.validation_status = 'failed', v.ancestral_allele = '', v.failed_description_id = w.failed_description_id WHERE v.variation_id = w.variation_id;
 #and finally, remove the table with the variations
