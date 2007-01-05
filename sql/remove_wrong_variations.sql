3dd08a49a410bfb0773851fd1192151b5991e31e
Daniel Rios <dr2@sanger.ac.uk>
2007-01-05 10:19:13 +0000

    changed table name from wrong_variation -> failed_variation

--- sql/remove_wrong_variations.sql
+++ sql/remove_wrong_variations.sql
@@ -8,6 +8,6 @@ DELETE vs FROM variation_synonym vs, failed_variation v WHERE vs.variation_id = 
 DELETE i FROM tmp_individual_genotype_single_bp i, failed_variation v WHERE i.variation_id = v.variation_id;
 SELECT 'Remember to run again compressed_genotype script to remove variations from the compressed table' as '';
 #after removing suspicious variations, update the Variation table to reflect changes
-UPDATE variation v, wrong_variation w set v.validation_status = 'failed', v.ancestral_allele = '', v.failed_description_id = w.failed_description_id WHERE v.variation_id = w.variation_id;
+UPDATE variation v, failed_variation w set v.validation_status = 'failed', v.ancestral_allele = '', v.failed_description_id = w.failed_description_id WHERE v.variation_id = w.variation_id;
 #and finally, remove the table with the variations
 DROP TABLE failed_variation;
