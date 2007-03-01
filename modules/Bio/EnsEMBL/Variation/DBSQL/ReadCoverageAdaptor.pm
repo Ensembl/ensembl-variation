80f2524a61e7ff3b8f080c2c0705b93c4fd0edb3
Daniel Rios <dr2@sanger.ac.uk>
2007-03-01 11:33:43 +0000

    modified strand information

--- modules/Bio/EnsEMBL/Variation/DBSQL/ReadCoverageAdaptor.pm
+++ modules/Bio/EnsEMBL/Variation/DBSQL/ReadCoverageAdaptor.pm
@@ -127,15 +127,10 @@ sub fetch_all_by_Slice_Sample_depth{
 		$constraint = "rc.sample_id = " . $args[0]->dbID;
 	    }
 	}
-	$strand = $slice->strand;
-	$slice->{'strand'} = 1;
 	$rcs = $self->fetch_all_by_Slice_constraint($slice,$constraint);    
-	$slice->{'strand'} = $strand;
 	return $rcs;
     }
     #call the method fetch_all_by_Slice
-    $strand = $slice->strand;
-    $slice->{'strand'} = 1;
     $rcs = $self->fetch_all_by_Slice($slice);    
     return $rcs;
 }
