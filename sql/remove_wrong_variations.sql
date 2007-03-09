50cfc8cae27bb51c617fbaf06872162ad1559dda
Daniel Rios <dr2@sanger.ac.uk>
2007-03-09 13:54:48 +0000

    update to deal with new schema

--- sql/remove_wrong_variations.sql
+++ sql/remove_wrong_variations.sql
@@ -7,7 +7,5 @@ DELETE vf FROM variation_feature vf, failed_variation v WHERE vf.variation_id = 
 DELETE vs FROM variation_synonym vs, failed_variation v WHERE vs.variation_id = v.variation_id;
 DELETE i FROM tmp_individual_genotype_single_bp i, failed_variation v WHERE i.variation_id = v.variation_id;
 SELECT 'Remember to run again compressed_genotype script to remove variations from the compressed table' as '';
-#after removing suspicious variations, update the Variation table to reflect changes
-UPDATE variation v, failed_variation w set v.validation_status = 'failed', v.ancestral_allele = NULL, v.failed_description_id = w.failed_description_id WHERE v.variation_id = w.variation_id;
-#and finally, remove table with the variations
-DROP TABLE failed_variation;
+TRUNCATE TABLE compressed_genotype_single_bp; #emtpy the compressed table for future recreation
+DELETE FROM meta_coord where table_name = 'compressed_genotype_single_bp';
