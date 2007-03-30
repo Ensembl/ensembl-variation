7d99a95ceee9cf498e74fa0ebb5013693c2160d1
Daniel Rios <dr2@sanger.ac.uk>
2007-03-30 10:16:28 +0000

    script to upgrade the version of the resequencing files when there is no change in the data

---
+++ scripts/import/upgrade_resequencing_files.pl
@@ -0,0 +1,63 @@
+#!/usr/local/ensembl/bin/perl -w
+
+use strict;
+use warnings;
+
+use Tie::File;
+use Getopt::Long;
+
+my $new_version;
+my $path;
+
+#
+# This is how I run it for version 44
+# perl upgrade_resequencing_files.pl -path /lustre/scratch1/ensembl/dr2/release-44/homo_sapiens_variation_44_36f/ -version 44
+#
+
+GetOptions('path=s'     => \$path,
+	   'version=i'  => \$new_version
+	   );
+
+usage('-version must contain a valid number') if (!$new_version);
+usage('-path must contain path to directory with resequencing files files') if (!$path);
+
+
+my @files = glob("$path*");
+my @line;
+my $call;
+my $new_file;
+my $previous_version = $new_version - 1;
+
+foreach my $file (@files){
+    print "Processing.... ",$file,"\n";
+    #first, unzip the file
+    $call = 'gunzip '. $file;
+    system($call);
+    $file =~ s/\.gz//;
+    #then, replace the line
+    tie @line,"Tie::File",$file || die "Could not tie the $file with File::Tie: $!\n";
+    $line[2] = $line[2] . " $new_version";
+    #change the file name
+    $new_file = $file;
+    $new_file =~ s/$previous_version/$new_version/;    
+    $call = 'mv ' . $file . " $new_file";
+    system($call);
+    #finally, compress the file again
+    $call = 'gzip ' . $new_file;
+    system($call);
+}
+
+sub usage {
+  my $msg = shift;
+
+  print STDERR <<EOF;
+
+usage: perl upgrade_resequencing_files <options>
+
+options:
+    -path    <pathname>       path to the directory containing the resequencing files
+    -version <new_version>    version to upgrade the files to
+EOF
+
+  die("\n$msg\n\n");
+}
