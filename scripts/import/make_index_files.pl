f1469f354e326168ff910291915a21122ff0116a
Yuan Chen <yuan@sanger.ac.uk>
2007-07-31 09:57:04 +0000

    initial adding

---
+++ scripts/import/make_index_files.pl
@@ -0,0 +1,15 @@
+#!/usr/local/ensembl/bin/perl
+
+use strict;
+use warnings;
+
+use Bio::Index::Fastq;
+
+my $index_filename = shift(@ARGV);
+
+my $inx = Bio::Index::Fastq->new(-filename => $index_filename,
+				 -write_flag => 1);
+
+$inx->make_index(@ARGV);
+
+exit 0;
