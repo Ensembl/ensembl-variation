e94684812425d6e895e640c48a5cdf38d5d0c92d
Steve Trevanion <st3@sanger.ac.uk>
2008-08-01 15:05:49 +0000

    meta schema in sync with other dbs

---
+++ sql/patch_50_51_b.sql
@@ -0,0 +1,25 @@
+# patch_50_51_d.sql
+#
+# Title: make database multi-species capable
+#
+# Description:
+#   Add a species_id column to the meta and coord_system table and make
+#   new indexes on these tables.
+
+-- Add the new species_id column after meta_id
+ALTER TABLE meta ADD COLUMN
+ species_id INT UNSIGNED DEFAULT 1 -- Default species_id is 1
+                                   -- NULL means "not species specific"
+ AFTER meta_id;
+
+-- Redo the indexes on the meta table
+ALTER TABLE meta DROP INDEX meta_key_index;
+ALTER TABLE meta DROP INDEX meta_value_index;
+
+ALTER TABLE meta
+ ADD UNIQUE INDEX species_key_value_idx (species_id, meta_key, meta_value);
+ALTER TABLE meta
+ ADD INDEX species_value_idx (species_id, meta_value);
+
+-- Optimize the modified tables
+OPTIMIZE TABLE meta;
