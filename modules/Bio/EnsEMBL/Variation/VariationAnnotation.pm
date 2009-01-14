3ae0aae534517b4cedc235bc2471b0b7faadaaf4
Yuan Chen <yuan@sanger.ac.uk>
2009-01-14 11:09:48 +0000

    add more attributes for genome catalogue data

--- modules/Bio/EnsEMBL/Variation/VariationAnnotation.pm
+++ modules/Bio/EnsEMBL/Variation/VariationAnnotation.pm
@@ -1,4 +1,4 @@
-# Ensembl module for Bio::EnsEMBL::Variation::VariationFeature
+# Ensembl module for Bio::EnsEMBL::Variation::VariationAnnotation
 #
 # Copyright (c) 2004 Ensembl
 #
@@ -18,6 +18,12 @@ Bio::EnsEMBL::Variation::VariationAnnotation - A genotype phenotype annotation f
         -source_name  => 'EGA',
         -study_type   => 'GWAS',
         -local_stable_id => 'EGAS00000000001',
+        -study        => 'pubmed/17293876',
+        -associated_gene => 'HHEX',
+        -associated_variant_risk_allele => 'rs13266634-C',
+        -variation_names => 'rs13266634',
+        -risk_allele_freq_in_controls => '0.3',
+        -p_value  => '6.00E-08',
         -variation => $v);
 
     ...
@@ -26,6 +32,7 @@ Bio::EnsEMBL::Variation::VariationAnnotation - A genotype phenotype annotation f
     print "From source ",$va->source_name,'-',$va->local_stable_id,"\n";
     print " With study_type ", $va->study_type(),"\n";
 
+    ...
     # Get the Variation object which this annotation represents
     # If not already retrieved from the DB, this will be
     # transparently lazy-loaded
@@ -71,24 +78,38 @@ use vars qw(@ISA);
     string - description of the phenotype
   Arg [-SOURCE_NAME] :
     string - name of the source
-  Arg [-VARIATION_NAME] :
-    string - name of the variation
+  Arg [-VARIATION_NAMES] :
+    string - name of the associated variations
   Arg [-VARIATION] :
     int - the variation object associated with this annotation.
-
   Arg [_VARIATION_ID] :
     int _ the internal id of the variation object associated with this
     identifier. This may be provided instead of a variation object so that
     the variation may be lazy-loaded from the database on demand.
+  Arg [-STUDY] :
+    string - the pubmed/ids or project/study names
+  Arg [-ASSOCIATED_GENE] :
+    string - the gene names associated with this annotation/variant.
+  Arg [-ASSOCIATED_VARIANT_RISK_ALLELE] :
+    string - the variants-risk alleles associated with this annotation.
+  Arg [-RISK_ALLELE_FREQ_IN_CONTROLS] :
+    string - the risk allele frequency in controls associated with this annotation.
+  Arg [-VARIATION] :
+    string - the p_value associated with this annotation.
 
   Example    :
     $va = Bio::EnsEMBL::Variation::VariationAnnotation->new
        (-phenotype_name => 'BD',
         -phenotype_description => 'Bipolar Disorder', 
         -souce_name  => 'EGA',
-        -variation_name => 'rs123',
+        -variation_names => 'rs123',
         -local_stable_id => 'EGAS00000000001',
         _variation_id => 10,
+        -study        => 'pubmed/17293876',
+        -associated_gene => 'HHEX',
+        -associated_variant_risk_allele => 'rs13266634-C',
+        -risk_allele_freq_in_controls => '0.3',
+        -p_value  => '6.00E-08',
         -variation => $v);
 
   Description: Constructor. Instantiates a new VariationAnnotation object.
@@ -104,9 +125,11 @@ sub new {
   my $class = ref($caller) || $caller;
   my $self = $class->SUPER::new(@_);
 
-  my ($dbID,$adaptor,$phenotype_name,$phenotype_description,$source_name,$study_type,$local_stable_id,$variation_id,$variation_name,$variation) =
+  my ($dbID,$adaptor,$phenotype_name,$phenotype_description,$source_name,$study_type,$local_stable_id,$variation_id,$variation_names,$variation,$study,$associated_gene,$associated_variant_risk_allele,$risk_allele_freq_in_controls,$p_value) =
     rearrange([qw(dbID ADAPTOR PHENOTYPE_NAME PHENOTYPE_DESCRIPTION SOURCE_NAME
-                  STUDY_TYPE LOCAL_STABLE_ID _VARIATION_ID VARIATION_NAME VARIATION)],@_); 
+                  STUDY_TYPE LOCAL_STABLE_ID _VARIATION_ID VARIATION_NAMES VARIATION
+		  STUDY ASSOCIATED_GENE ASSOCIATED_VARIANT_RISK_ALLELE
+		 RISK_ALLELE_FREQ_IN_CONTROLS P_VALUE)],@_); 
 
   $self->{'dbID'} = $dbID;
   $self->{'adaptor'}    = $adaptor;
@@ -116,8 +139,13 @@ sub new {
   $self->{'variation'}        = $variation;
   $self->{'_variation_id'}    = $variation_id;
   $self->{'source_name'}      = $source_name;
-  $self->{'variation_name'}   = $variation_name;
+  $self->{'variation_names'}   = $variation_names;
   $self->{'study_type'}  = $study_type;
+  $self->{'study'} = $study;
+  $self->{'associated_gene'} = $associated_gene;
+  $self->{'associated_variant_risk_allele'} = $associated_variant_risk_allele;
+  $self->{'risk_allele_freq_in_controls'} = $risk_allele_freq_in_controls;
+  $self->{'p_value'} = $p_value;
  
   return $self;
 }
@@ -262,13 +290,13 @@ sub variation {
   return $self->{'variation'};
 }
 
-=head2 variation_name
+=head2 variation_names
 
   Arg [1]    : string $newval (optional)
-               The new value to set the variation_name attribute to
-  Example    : $variation_name = $obj->variation_name()
-  Description: Getter/Setter for the variation_name attribute.  This is the
-               name of the variation associated with this feature.
+               The new value to set the variation_names attribute to
+  Example    : $variation_names = $obj->variation_names()
+  Description: Getter/Setter for the variation_names attribute.  This is the
+               names of the variation associated with this study.
   Returntype : string
   Exceptions : none
   Caller     : general
@@ -276,10 +304,110 @@ sub variation {
 
 =cut
 
-sub variation_name{
+sub variation_names{
   my $self = shift;
-  return $self->{'variation_name'} = shift if(@_);
-  return $self->{'variation_name'};
+  return $self->{'variation_names'} = shift if(@_);
+  return $self->{'variation_names'};
+}
+
+=head2 study
+
+  Arg [1]    : string $newval (optional)
+               The new value to set the study attribute to
+  Example    : $variation_names = $obj->study()
+  Description: Getter/Setter for the study attribute.  This is the
+               pubmed/id or project name associated with this study.
+  Returntype : string
+  Exceptions : none
+  Caller     : general
+  Status     : At Risk
+
+=cut
+
+sub study{
+  my $self = shift;
+  return $self->{'study'} = shift if(@_);
+  return $self->{'study'};
+}
+
+=head2 associated_gene
+
+  Arg [1]    : string $newval (optional)
+               The new value to set the associated_gene attribute to
+  Example    : $associated_gene = $obj->associated_gene()
+  Description: Getter/Setter for the associated_gene attribute.  This is the
+               gene names associated with this study.
+  Returntype : string
+  Exceptions : none
+  Caller     : general
+  Status     : At Risk
+
+=cut
+
+sub associated_gene{
+  my $self = shift;
+  return $self->{'associated_gene'} = shift if(@_);
+  return $self->{'associated_gene'};
+}
+
+=head2 associated_variant_risk_allele
+
+  Arg [1]    : string $newval (optional)
+               The new value to set the associated_variant_risk_allele attribute to
+  Example    : $associated_variant_risk_allele = $obj->associated_variant_risk_allele()
+  Description: Getter/Setter for the associated_variant_risk_allele attribute.  This is the
+               associated_variant_risk_allele associated with this study.
+  Returntype : string
+  Exceptions : none
+  Caller     : general
+  Status     : At Risk
+
+=cut
+
+sub associated_variant_risk_allele{
+  my $self = shift;
+  return $self->{'associated_variant_risk_allele'} = shift if(@_);
+  return $self->{'associated_variant_risk_allele'};
+}
+
+=head2 risk_allele_freq_in_controls
+
+  Arg [1]    : string $newval (optional)
+               The new value to set the risk_allele_freq_in_controls attribute to
+  Example    : $risk_allele_freq_in_controls = $obj->risk_allele_freq_in_controls()
+  Description: Getter/Setter for the risk_allele_freq_in_controls attribute.  This is the
+               risk_allele_freq_in_controls associated with this study.
+  Returntype : string
+  Exceptions : none
+  Caller     : general
+  Status     : At Risk
+
+=cut
+
+sub risk_allele_freq_in_controls{
+  my $self = shift;
+  return $self->{'risk_allele_freq_in_controls'} = shift if(@_);
+  return $self->{'risk_allele_freq_in_controls'};
+}
+
+=head2 p_value
+
+  Arg [1]    : string $newval (optional)
+               The new value to set the p_value attribute to
+  Example    : $p_value = $obj->p_value()
+  Description: Getter/Setter for the p_value attribute.  This is the
+               p_value associated with this study.
+  Returntype : string
+  Exceptions : none
+  Caller     : general
+  Status     : At Risk
+
+=cut
+
+sub p_value{
+  my $self = shift;
+  return $self->{'p_value'} = shift if(@_);
+  return $self->{'p_value'};
 }
 
 
