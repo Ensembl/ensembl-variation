fc25fb9018e1fa24b5e1cc7fea27b2e5803724f0
William McLaren <wm2@ebi.ac.uk>
2009-03-02 11:29:26 +0000

    *** empty log message ***

--- sql/patch_53_54_b.sql
+++ sql/patch_53_54_b.sql
@@ -3,5 +3,7 @@
 alter table source
 add column description varchar(255) default NULL;
 
+update source set description = 'Variation features from dbSNP' where name = 'dbSNP';
+
 # patch identifier
-INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL,'patch', 'patch_53_54_b.sql|schema version');
+INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL,'patch', 'patch_53_54_b.sql|add description column to source table');
