716db5aa5a0e756a783f43b8c51c20d54214b3a4
William McLaren <wm2@ebi.ac.uk>
2009-03-02 11:29:26 +0000

    *** empty log message ***

--- sql/patch_53_54_c.sql
+++ sql/patch_53_54_c.sql
@@ -1,7 +1,7 @@
-# ADD DESCRIPTION COLUMN TO SOURCE TABLE
+# ADD DISPLAY COLUMN TO SAMPLE
 
 alter table sample
-add column display enum('REFERENCE','DEFAULT','DISPLAYABLE','UNDISPLAYABLE') default 'DISPLAYABLE';
+add column display enum('REFERENCE','DEFAULT','DISPLAYABLE','UNDISPLAYABLE') default 'UNDISPLAYABLE';
 
 # patch identifier
-INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL,'patch', 'patch_53_54_c.sql|schema version');
+INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL,'patch', 'patch_53_54_c.sql|add display column to sample table');
