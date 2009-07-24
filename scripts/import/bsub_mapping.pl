65cc0647bfeb4b4d618b65d998d1313b4f3ae169
Yuan Chen <yuan@sanger.ac.uk>
2009-07-24 11:11:21 +0000

    update

--- scripts/import/bsub_mapping.pl
+++ scripts/import/bsub_mapping.pl
@@ -1,6 +1,6 @@
 #! /usr/local/ensembl/bin/perl
 #
-#./bsub_ssaha2.pl -species human -input_dir /ecs4/scratch2/yuan/hum/mapping_36 -output_dir /ecs4/scratch2/yuan/hum/mapping_36 -target_dir /ecs4/scratch2/yuan/hum/mapping_36/target_dna -start 1 -end 9
+#./bsub_mapping.pl -species mouse -job flank -tmpdir /lustre/scratch1/ensembl/yuan/tmp/mouse -tmpfile patric_mapping
 
 use strict;
 #use DBH;
@@ -22,43 +22,54 @@ use ImportUtils qw(dumpSQL debug create_and_load load);
 # optional a directory or sequence files (for unknown placing)
 
 
-our ($species, $output_dir, $input_dir, $target_dir, $TMP_DIR, $TMP_FILE, $start, $end);
+our ($species, $job, $source_name, $output_dir, $input_dir, $target_dir, $TMP_DIR, $TMP_FILE, $start, $end);
 
 GetOptions('species=s'    => \$species,
-	   'tmpdir=s'     => \$ImportUtils::TMP_DIR,
+	   'job=s'        => \$job,
+	   'source_name=s'=> \$source_name,
+           'tmpdir=s'     => \$ImportUtils::TMP_DIR,
 	   'tmpfile=s'    => \$ImportUtils::TMP_FILE,
 	  );
 my $registry_file ||= $Bin . "/ensembl.registry";
 
 $TMP_DIR  = $ImportUtils::TMP_DIR;
 $TMP_FILE = $ImportUtils::TMP_FILE;
-$species ||= 'mouse';
+
+my $script_name;
+if ($job =~/flank/) {
+  $script_name = "map_flanking_sequence.pl";
+}
+elsif($job =~ /var/) {
+  $script_name = "map_variation_feature.pl";
+}
+
 
 Bio::EnsEMBL::Registry->load_all( $registry_file );
 
-my $cdb = Bio::EnsEMBL::Registry->get_DBAdaptor($species,'core');
-my $vdb = Bio::EnsEMBL::Registry->get_DBAdaptor($species,'variation');
+my $cdb = Bio::EnsEMBL::Registry->get_DBAdaptor($species,'core1');
+#my $vdb = Bio::EnsEMBL::Registry->get_DBAdaptor($species,'variation');
 
 my $dbCore = $cdb->dbc->db_handle;
-my $dbVar = $vdb->dbc->db_handle;
+#my $dbVar = $vdb->dbc->db_handle;
 
 $start ||=1;
 
-&run();
+run();
 
 sub run {
 
   my $slice_adaptor = $cdb->get_SliceAdaptor();
 
-  my (%seq_region_names);
+  my ($count,%seq_region_names);
 
   #my $sthc = $dbCore->prepare(qq{select sr.seq_region_id from seq_region_attrib sra, attrib_type at, seq_region sr where sra.attrib_type_id=at.attrib_type_id and at.code="toplevel" and sr.seq_region_id = sra.seq_region_id });
-  my $sthc = $dbCore->prepare(qq{select sr.seq_region_id from seq_region sr, coord_system cs where cs.coord_system_id=sr.coord_system_id and cs.name="chromosome"});
+  my $sthc = $dbCore->prepare(qq{select sr.seq_region_id from seq_region_attrib sra, attrib_type at, seq_region sr where sra.attrib_type_id=at.attrib_type_id and at.code="toplevel" and sr.seq_region_id = sra.seq_region_id});
   $sthc->execute();
   while (my ($seq_region_id) = $sthc->fetchrow_array()) {
-    my $call = "bsub -q normal -o mapping_out /usr/local/bin/perl map_variation_feature.pl -species $species -seq_region_id $seq_region_id -tmpdir $TMP_DIR -tmpfile $TMP_FILE";
+    my $call = "bsub -q normal -o $TMP_DIR/mapping_out\_$seq_region_id\_$job  /usr/local/bin/perl $script_name -species $species -seq_region_id $seq_region_id -source_name $source_name -tmpdir $TMP_DIR -tmpfile $TMP_FILE";
     system($call);
-    print "submitting job for $seq_region_id\n";
+    $count++;
+    print "submitting job for $seq_region_id count $count\n";
   }
 }
 
