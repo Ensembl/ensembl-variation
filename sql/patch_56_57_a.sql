ec1bf2031868ea73e10df3cf28b2843fc26bc956
William McLaren <wm2@ebi.ac.uk>
2009-10-28 10:42:59 +0000

    *** empty log message ***

--- sql/patch_56_57_a.sql
+++ sql/patch_56_57_a.sql
@@ -1,5 +1,6 @@
-# update the sample table to include an 'LD' option for populations
-ALTER TABLE sample MODIFY `display` ENUM('REFERENCE','DEFAULT','DISPLAYABLE','UNDISPLAYABLE','LD') DEFAULT 'UNDISPLAYABLE';
+# update the schema_version entry in the meta table
+##################
+update meta set meta_value = 57 where meta_key = 'schema_version';
 
 # patch identifier
 INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL,'patch', 'patch_56_57_a.sql|schema version');
