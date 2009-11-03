c63d4823d454584af2fb90c3f21673e6a2d428c7
William McLaren <wm2@ebi.ac.uk>
2009-11-03 14:01:45 +0000

    *** empty log message ***

--- sql/patch_56_57_b.sql
+++ sql/patch_56_57_b.sql
@@ -2,4 +2,4 @@
 ALTER TABLE sample MODIFY `display` ENUM('REFERENCE','DEFAULT','DISPLAYABLE','UNDISPLAYABLE','LD') DEFAULT 'UNDISPLAYABLE';
 
 # patch identifier
-INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL,'patch', 'patch_56_57_b.sql|schema version');
+INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL,'patch', 'patch_56_57_b.sql|add LD option to display column in sample');
