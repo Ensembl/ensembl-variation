1b4fba11662e84bff0a1eb4e11306a0af80c2b85
William McLaren <wm2@ebi.ac.uk>
2010-06-08 10:24:01 +0000

    *** empty log message ***

---
+++ sql/patch_58_59_c.sql
@@ -0,0 +1,5 @@
+# Add schema_type entry to meta table
+INSERT INTO meta (meta_key, meta_value) VALUES ('schema_type', 'variation');
+
+# patch identifier
+INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL,'patch', 'patch_58_59_c.sql|schema_type entry in meta table');
