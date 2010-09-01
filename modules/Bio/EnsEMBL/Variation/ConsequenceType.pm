b8ae121fc011580332d9fbeecbfd96cae82ca326
Graham Ritchie <gr5@sanger.ac.uk>
2010-09-01 13:48:14 +0000

    undo erroneous commit (back to 1.22)

--- modules/Bio/EnsEMBL/Variation/ConsequenceType.pm
+++ modules/Bio/EnsEMBL/Variation/ConsequenceType.pm
@@ -357,26 +357,6 @@ sub type {
   return $self->{'type'}
 }
 
-sub allele_type {
-    my $self = shift;
-    
-    if (@_ == 2) {
-        my ($allele, $type) = @_;
-        
-        if (defined $CONSEQUENCE_TYPES{$type} || $type eq 'SARA') {
-            $self->{allele_type}->{$allele} = $type;
-        }
-        else {
-            warning("Invalid consequence type: $type\n");
-        }
-    }
-    else {
-        warning("Need to specify both allele and type to allele_type method");
-    }
-    
-    return $self->{allele_type};
-}
-
 
 =head2 aa_alleles
 
