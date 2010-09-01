966bde7bf1b33bd0dd3d2f5053e81980eb7384d6
Graham Ritchie <gr5@sanger.ac.uk>
2010-09-01 10:34:47 +0000

    added more fields to the OverlapConsequence object

--- modules/Bio/EnsEMBL/Variation/OverlapConsequence.pm
+++ modules/Bio/EnsEMBL/Variation/OverlapConsequence.pm
@@ -25,4 +25,34 @@ sub predicate {
     return $self->{predicate};
 }
 
+sub rank {
+    my ($self, $rank) = @_;
+    $self->{rank} = $rank if $rank;
+    return $self->{rank};
+}
+
+sub ensembl_term {
+    my ($self, $ensembl_term) = @_;
+    $self->{ensembl_term} = $ensembl_term if $ensembl_term;
+    return $self->{ensembl_term};
+}
+
+sub SO_id {
+    my ($self, $SO_id) = @_;
+    $self->{SO_id} = $SO_id if $SO_id;
+    return $self->{SO_id};
+}
+
+sub NCBI_term {
+    my ($self, $NCBI_term) = @_;
+    $self->{NCBI_term} = $NCBI_term if $NCBI_term;
+    return $self->{NCBI_term};
+}
+
+sub is_definitive {
+    my ($self, $is_definitive) = @_;
+    $self->{is_definitive} = $is_definitive if defined $is_definitive;
+    return $self->{is_definitive};
+}
+
 1;
