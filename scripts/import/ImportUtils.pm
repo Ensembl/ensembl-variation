2dc7a2605764e467050e507df9cd5301bb3cecdb
Pontus Larsson <pl4@sanger.ac.uk>
2010-11-03 12:30:02 +0000

    Added subroutine for making a file XML compliant and one for dumping SQL from a prepared statement

--- scripts/import/ImportUtils.pm
+++ scripts/import/ImportUtils.pm
@@ -7,22 +7,60 @@ use Exporter;
 
 our @ISA = ('Exporter');
 
-our @EXPORT_OK = qw(dumpSQL debug create_and_load load get_create_statement);
+our @EXPORT_OK = qw(dumpSQL debug create_and_load load get_create_statement make_xml_compliant);
 
 our $TMP_DIR = "/ecs4/scratch4/yuan/tmp";
 our $TMP_FILE = 'tabledump.txt';
 
+#ÊThis will strip non-xml-compliant characters from an infile, saving a backup in {infile name}.bak
+# If no infile was specified, will use the tempfile but no backup will be kept. If no xml version was specified, will default to 1.1
+# If the xml version was not recognized, will do nothing.
+sub make_xml_compliant {
+  my $infile = shift;
+  my $version = shift;
+  
+  my $keep_backup = defined($infile);
+  
+  $infile ||= $TMP_DIR . "/" . $TMP_FILE;
+  $version ||= "1.1";
+  
+  my @ARGV_bak = @ARGV;
+  @ARGV = ($infile);
+  $^I = '.bak';
+  while (<>) {
+    if ($version =~ m/1\.1/) {
+      s/[^\x01-\x{D7FF}\x{E000}-\x{FFFD}\x{10000}-\x{10FFFF}]//go;
+      s/[\x01-\x08\x0B-\x0C\x0E-\x1F\x7F-\x84\x86-\x9F]//go;
+    }
+    elsif ($version =~ m/1\.0/) {
+      s/[^\x09\x0A\x0D\x20-\x{D7FF}\x{E000}-\x{FFFD}\x{10000}-\x{10FFFF}]//go;
+    }
+    print;
+  }
+  # Remove the backup if an input file wasn't supplied
+  unlink($infile . ".bak") if (!$keep_backup);
+  # Restore the @ARGV variable
+  @ARGV = @ARGV_bak;
+}
 
 # successive dumping and loading of tables is typical for this process
 # dump does effectively a select into outfile without server file system access
 sub dumpSQL {
   my $db  = shift;
   my $sql = shift;
+  my $sth = $db->prepare( $sql );
+  dumpPreparedSQL($sth);
+  $sth->finish();
+}
+
+sub dumpPreparedSQL {
+  my $sth = shift;
+  
   local *FH;
   my $counter = 0;
   open( FH, ">$TMP_DIR/$TMP_FILE" )
       or die( "Cannot open $TMP_DIR/$TMP_FILE: $!" );
-  my $sth = $db->prepare( $sql);
+      
   $sth->{mysql_use_result} = 1;
   $sth->execute();
   my $first;
@@ -32,11 +70,7 @@ sub dumpSQL {
   }
 
   close FH;
-
-  $sth->finish();
 }
-
-
 
 # load imports a table, optionally not all columns
 # if table doesnt exist, create a varchar(255) for each column
