98a4ca18a0601d6337dfbabc8c61596aef2936a1
William McLaren <wm2@ebi.ac.uk>
2010-12-07 15:31:22 +0000

    Added ignore

--- sql/patch_60_61_e.sql
+++ sql/patch_60_61_e.sql
@@ -1,5 +1,5 @@
 # add new failed description entry
-INSERT INTO failed_description (failed_description_id,description) VALUES (6,'Variation has no genotypes');
+INSERT IGNORE INTO failed_description (failed_description_id,description) VALUES (6,'Variation has no genotypes');
 
 # patch identifier
 INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL,'patch', 'patch_60_61_e.sql|add new failed_description entry');
