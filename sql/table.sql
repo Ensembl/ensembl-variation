bc845782cece5e87f0565a3f5c99d3bf72836a0b
Pontus Larsson <pl4@sanger.ac.uk>
2010-12-07 11:21:47 +0000

    Fix

--- sql/table.sql
+++ sql/table.sql
@@ -317,8 +317,8 @@ create table variation_feature(
             '33','34','35','36','37','38','39','40',
             '41','42','43','44','45','46','47','48',
             '49','50','51','52','53','54','55','56',
-            '57','58','59','60','61','62','63','64',
-        ) DEFAULT 0,
+            '57','58','59','60','61','62','63','64'
+        ) DEFAULT '',
 	primary key( variation_feature_id ),
 	key pos_idx( seq_region_id, seq_region_start, seq_region_end ),
 	key variation_idx( variation_id ),
