005ea072f740ab734a7baae2a9ea433177ed5840
William McLaren <wm2@ebi.ac.uk>
2010-12-07 17:30:56 +0000

    Fixes

--- sql/patch_60_61_e.sql
+++ sql/patch_60_61_e.sql
@@ -1,5 +1,8 @@
+# change id to auto_increment
+ALTER TABLE `failed_description` CHANGE `failed_description_id` `failed_description_id` int(10) UNSIGNED NOT NULL  auto_increment;
+
 # add new failed description entry
-INSERT IGNORE INTO failed_description (failed_description_id,description) VALUES (6,'Variation has no genotypes');
+INSERT IGNORE INTO failed_description (description) VALUES ('Variation has no genotypes');
 
 # patch identifier
 INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL,'patch', 'patch_60_61_e.sql|add new failed_description entry');
