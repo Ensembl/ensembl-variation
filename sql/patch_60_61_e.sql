b51ffb737ad0da0b6436cf394f4173d127e3261f
William McLaren <wm2@ebi.ac.uk>
2010-12-08 11:36:15 +0000

    *** empty log message ***

--- sql/patch_60_61_e.sql
+++ sql/patch_60_61_e.sql
@@ -3,6 +3,7 @@ ALTER TABLE `failed_description` CHANGE `failed_description_id` `failed_descript
 
 # add new failed description entry
 INSERT IGNORE INTO failed_description (description) VALUES ('Variation has no genotypes');
+INSERT IGNORE INTO failed_description (description) VALUES ('Genotype frequencies do not add up to 1');
 
 # patch identifier
 INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL,'patch', 'patch_60_61_e.sql|add new failed_description entry');
