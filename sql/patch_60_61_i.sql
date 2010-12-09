d45a4684b6e10046591c0f012ea6c4ebd8d1a87e
Graham Ritchie <gr5@sanger.ac.uk>
2010-12-09 17:09:48 +0000

    use auto_increment key on failed_description rather than hard coding

--- sql/patch_60_61_i.sql
+++ sql/patch_60_61_i.sql
@@ -1,6 +1,6 @@
 # add a failed description for variations that have no associated sequence
 
-INSERT INTO failed_description (failed_description_id,description) VALUES (8,'Variation has no associated sequence');
+INSERT INTO failed_description (failed_description_id,description) VALUES ('Variation has no associated sequence');
 
 # patch identifier
 INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL,'patch', 'patch_60_61_i.sql|add a failed description for variations with no associated sequence');
