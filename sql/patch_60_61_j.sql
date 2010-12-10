f2989832e6dc995f0a9e0d3b865c48672ae5dbd5
Graham Ritchie <gr5@sanger.ac.uk>
2010-12-10 13:16:35 +0000

    fixed typo in meta table entry

--- sql/patch_60_61_j.sql
+++ sql/patch_60_61_j.sql
@@ -4,5 +4,5 @@ UPDATE variation_class SET display_term = 'insertion' WHERE so_term = 'insertion
 UPDATE variation_class SET display_term = 'deletion' WHERE so_term = 'deletion';
 
 # patch identifier
-INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL,'patch', 'patch_60_61_j.sql|change the display tern for insertions and deletions');
+INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL,'patch', 'patch_60_61_j.sql|change the display term for insertions and deletions');
 
