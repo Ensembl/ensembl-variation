d227ae1bf9bba84abba75c9bb7a0c8ced07baf0b
William McLaren <wm2@ebi.ac.uk>
2011-01-04 10:15:32 +0000

    Formatting and fixes

--- scripts/import/create_ld_table.pl
+++ scripts/import/create_ld_table.pl
@@ -40,27 +40,31 @@ my $sth = $dbVariation->dbc->prepare(qq{SELECT c.sample_id,c.seq_region_id,c.seq
 				 FROM compressed_genotype_single_bp c FORCE INDEX(pos_idx), individual_population ip
 				 WHERE  ip.individual_sample_id = c.sample_id
 				 AND   ip.population_sample_id $in_str
+				 #AND c.seq_region_id in (27504,27508,27511,27522)
 #				 LIMIT 100000
 			     },{mysql_use_result => 1});
 
 $sth->execute();
 create_files($sth,$siblings);
 $sth->finish();
-my @files = glob("$TMP_DIR/*");
+my @files = glob("$TMP_DIR/dump*");
 my $call = '';
 my @stats;
 foreach my $file (@files){
+	
     $file =~ /.*_(\d+)/;
+	
     @stats = stat($file);
     if ($stats[7] > MAX_SIZE){
 #once the files are created, we have to calculate the ld
-	$call = "bsub -W2:00 -M3500000 -R'select[mem>3500] rusage[mem=3500]' -q normal -J ld_calculation_$1 ";
+	$call = "bsub -M15000000 -R'select[mem>15000] rusage[mem=15000]' -q long -J ld_calculation_$1 -o $TMP_DIR\/$1\_farm.out ";
     }
     $call .= "perl calculate_ld_table.pl -tmpdir $TMP_DIR -tmpfile $TMP_FILE -ldfile $file ";
   #  print $call,"\n";
     system($call);
     $call = '';
 }
+
 sleep(60);
 $call = "bsub -K -W2:00 -w 'done(ld_calculation*)' -J waiting_ld date";
 system($call);
@@ -137,7 +141,7 @@ sub print_buffered {
     if( ! $filename ) {
 	# flush the buffer
 	foreach my $file (keys %{$buffer}){
-	    open( FH, ">>$file" ) or die;
+	    open( FH, ">>$file" ) or die "Could not write to file $file";
 	    print FH $buffer->{ $file };
 	    close FH;
 	}
@@ -145,7 +149,7 @@ sub print_buffered {
     } else {
 	$buffer->{ $filename } .= $text;
 	if( length( $buffer->{ $filename } ) > 10_000 ) {
-	    open( FH, ">>$filename" ) or die;
+	    open( FH, ">>$filename" ) or die "Could not write to file $filename";
 	    print FH $buffer->{ $filename };
 	    close FH;
 	    $buffer->{ $filename } = '';
@@ -158,20 +162,23 @@ sub get_LD_populations{
     my $dbVariation = shift;
     my $siblings = shift;
     my ($pop_id,$population_name);
-    my $sth = $dbVariation->dbc->prepare(qq{SELECT s.sample_id, s.name
-				     FROM population p, sample s
-				     WHERE (s.name like 'PERLEGEN:AFD%'
-				     OR s.name like 'CSHL-HAPMAP%')
-				     AND s.sample_id = p.sample_id});
+    my $sth = $dbVariation->dbc->prepare(qq{
+		SELECT s.sample_id, s.name
+		FROM population p, sample s
+		WHERE (s.name like 'PERLEGEN:AFD%'
+		OR s.name like 'CSHL-HAPMAP%'
+		OR s.name like '1000GENOMES%')
+		AND s.sample_id = p.sample_id
+	});
 
     $sth->execute();
     $sth->bind_columns(\$pop_id,\$population_name);
     #get all the children that we do not want in the genotypes
     my @pops;
     while($sth->fetch){
-	if($population_name =~ /CEU|YRI|MEX/){
+	#if($population_name =~ /CEU|YRI|MEX/){
 	    get_siblings($dbVariation,$pop_id,$siblings);
-	}
+	#}
 	push @pops, $pop_id;
     }
     
@@ -188,19 +195,21 @@ sub get_siblings{
     my $population_id = shift;
     my $siblings = shift;
 
-    my $sth_individual = $dbVariation->dbc->prepare(qq{SELECT i.sample_id
-							     FROM individual i, individual_population ip
-							     WHERE ip.individual_sample_id = i.sample_id
-							     AND ip.population_sample_id = ? 
-							     AND i.father_individual_sample_id IS NOT NULL
-							     AND i.mother_individual_sample_id IS NOT NULL
-							 });
+    my $sth_individual = $dbVariation->dbc->prepare(qq{
+		SELECT i.sample_id
+		FROM individual i, individual_population ip
+		WHERE ip.individual_sample_id = i.sample_id
+		AND ip.population_sample_id = ? 
+		AND i.father_individual_sample_id IS NOT NULL
+		AND i.mother_individual_sample_id IS NOT NULL
+	});
     my ($individual_id);
     $sth_individual->execute($population_id);
     $sth_individual->bind_columns(\$individual_id);
     while ($sth_individual->fetch){
-	$siblings->{$population_id.'-'.$individual_id}++; #necessary to have in the key the population, since some individuals are shared between
-	                                                   #populations
+		# necessary to have in the key the population,
+		# since some individuals are shared between populations
+		$siblings->{$population_id.'-'.$individual_id}++;
     }
 }
 
@@ -210,15 +219,15 @@ sub create_LD_table{
     
     #need to create ld table if it has not been create yet
     $dbVariation->dbc->do(qq{
-	CREATE TABLE IF NOT EXISTS pairwise_ld(
-					       sample_id int not null,
-					       seq_region_id int not null,
-					       seq_region_start int not null,
-					       seq_region_end int not null,
-					       r2 float not null,
-					       
-					       key seq_region_idx(seq_region_id,seq_region_start)
-					       )
+		CREATE TABLE IF NOT EXISTS pairwise_ld(
+			sample_id int not null,
+			seq_region_id int not null,
+			seq_region_start int not null,
+			seq_region_end int not null,
+			r2 float not null,
+			
+			key seq_region_idx(seq_region_id,seq_region_start)
+		)
 	});
 
 }
