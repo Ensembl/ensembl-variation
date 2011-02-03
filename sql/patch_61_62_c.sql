58d52a7dc942e09b1d3dc11915a9506a7955c394
Pontus Larsson <pl4@sanger.ac.uk>
2011-02-03 16:24:38 +0000

    failed_allele table

---
+++ sql/patch_61_62_c.sql
@@ -0,0 +1,11 @@
+# add table for failed_alleles
+CREATE TABLE failed_allele (
+  failed_allele_id int(11) NOT NULL AUTO_INCREMENT,
+  allele_id int(10) unsigned NOT NULL,
+  failed_description_id int(10) unsigned NOT NULL,
+  PRIMARY KEY (failed_allele_id),
+  KEY allele_idx (allele_id)
+);
+
+# patch identifier
+INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL,'patch', 'patch_61_62_c.sql|add failed_allele table');
