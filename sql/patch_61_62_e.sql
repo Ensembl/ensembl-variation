aa988baded08f94fcf77452048e9e13a507f1285
Laurent Gil <lgil@ebi.ac.uk>
2011-02-16 15:02:01 +0000

    add the tables study and supporting_structural_variation

---
+++ sql/patch_61_62_e.sql
@@ -0,0 +1,29 @@
+# add the tables study and supporting_structural_variation
+
+create table study (
+	study_id int(10) unsigned not null auto_increment,
+	source_id int(10) unsigned not null,
+	name varchar(255),
+	description varchar(255),
+	url varchar(255),
+	study varchar(255),
+	
+	primary key( study_id ),
+	unique key name_idx(name),
+	key source_idx (source_id)
+);
+
+
+create table supporting_structural_variation (
+	supporting_structural_variation_id int(10) unsigned not null auto_increment,
+	name varchar(255),
+	structural_variation_id int(10) unsigned not null,
+	
+	primary key( supporting_structural_variation_id ),
+	unique key name_idx(name),
+	key structural_variation_idx (structural_variation_id)
+);
+
+
+# patch identifier
+INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL,'patch', 'patch_61_62_e.sql|add the tables study and supporting_structural_variation');
