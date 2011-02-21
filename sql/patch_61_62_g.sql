f6e069594a09922c93589514ce6e4d4d86b660b6
Graham Ritchie <gr5@sanger.ac.uk>
2011-02-21 14:46:22 +0000

    fixed incorrect meta table entry

--- sql/patch_61_62_g.sql
+++ sql/patch_61_62_g.sql
@@ -4,4 +4,4 @@ ALTER TABLE source DROP COLUMN somatic;
 ALTER TABLE variation ADD COLUMN somatic tinyint(1);
 
 # patch identifier
-INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL,'patch', 'patch_61_62_f.sql|move somatic flag from source to variation table');
+INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL,'patch', 'patch_61_62_g.sql|move somatic flag from source to variation table');
