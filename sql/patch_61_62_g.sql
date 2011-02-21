28645362069cf9382c0c1439f1fafbc85fbac67b
Graham Ritchie <gr5@sanger.ac.uk>
2011-02-21 15:41:27 +0000

    made somatic default to 0 and not null

--- sql/patch_61_62_g.sql
+++ sql/patch_61_62_g.sql
@@ -1,7 +1,8 @@
 # move the somatic flag from the source to variation table
 
 ALTER TABLE source DROP COLUMN somatic;
-ALTER TABLE variation ADD COLUMN somatic tinyint(1);
+ALTER TABLE variation ADD COLUMN somatic tinyint(1) DEFAULT 0 NOT NULL;
 
 # patch identifier
+
 INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL,'patch', 'patch_61_62_g.sql|move somatic flag from source to variation table');
