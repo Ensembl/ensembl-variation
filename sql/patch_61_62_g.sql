b684995d248f242a71ec8c1b0cb485a22866458c
Graham Ritchie <gr5@sanger.ac.uk>
2011-02-21 10:37:51 +0000

    move somatic flag from source to variation

---
+++ sql/patch_61_62_g.sql
@@ -0,0 +1,7 @@
+# move the somatic flag from the source to variation table
+
+ALTER TABLE source DROP COLUMN somatic;
+ALTER TABLE variation ADD COLUMN somatic tinyint(1);
+
+# patch identifier
+INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL,'patch', 'patch_61_62_f.sql|move somatic flag from source to variation table');
