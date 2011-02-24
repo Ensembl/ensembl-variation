8962237ef4cc3ea953830067b38f0bf827c9f163
Laurent Gil <lgil@ebi.ac.uk>
2011-02-24 13:26:57 +0000

    Update the study table and the relative foreign keys in the variatiation_annotation table

--- sql/patch_61_62_f.sql
+++ sql/patch_61_62_f.sql
@@ -3,9 +3,15 @@
 ALTER TABLE structural_variation ADD COLUMN study_id int(10);
 ALTER TABLE structural_variation ADD INDEX study_idx (study_id);
 
+ALTER TABLE variation_annotation ADD COLUMN study_id int(10);
+ALTER TABLE variation_annotation INDEX study_idx (study_id);
 ALTER TABLE variation_annotation ADD COLUMN local_study_id int(10);
 ALTER TABLE variation_annotation ADD INDEX local_study_idx (local_study_id);
+
 ALTER TABLE variation_annotation DROP COLUMN local_stable_id;
+ALTER TABLE variation_annotation DROP COLUMN study;
+ALTER TABLE variation_annotation DROP COLUMN study_type;
+ALTER TABLE variation_annotation DROP COLUMN source_id;
 
 # patch identifier
 INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL,'patch', 'patch_61_62_f.sql|add a study_id column in the tables structural_variation and variation_annotation');
