effcc33e995cc7a20bbbabccf079b3eecd1da698
Graham Ritchie <gr5@sanger.ac.uk>
2011-02-24 11:10:24 +0000

    add somatic column to variation_feature as well

--- sql/patch_61_62_g.sql
+++ sql/patch_61_62_g.sql
@@ -1,8 +1,9 @@
-# move the somatic flag from the source to variation table
+# move the somatic flag from the source to variation and variation_feature tables
 
 ALTER TABLE source DROP COLUMN somatic;
 ALTER TABLE variation ADD COLUMN somatic tinyint(1) DEFAULT 0 NOT NULL;
+ALTER TABLE variation_feature ADD COLUMN somatic tinyint(1) DEFAULT 0 NOT NULL;
 
 # patch identifier
 
-INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL,'patch', 'patch_61_62_g.sql|move somatic flag from source to variation table');
+INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL,'patch', 'patch_61_62_g.sql|move somatic flag from source to variation and variation_feature tables');
