0f6c63d853ddc34f77f95aa164ce97e2afd2d825
Laurent Gil <lgil@ebi.ac.uk>
2011-02-28 11:31:28 +0000

    Just "chip" type for the moment

--- sql/patch_61_62_d.sql
+++ sql/patch_61_62_d.sql
@@ -1,6 +1,6 @@
 # add a type column in the source table
 
-ALTER TABLE source ADD COLUMN type ENUM ('database','chip','archive');
+ALTER TABLE source ADD COLUMN type ENUM ('chip');
 
 # patch identifier
 INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL,'patch', 'patch_61_62_d.sql|add a type column in the source table');
