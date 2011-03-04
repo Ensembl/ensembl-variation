fe5f5957fea691aaf5deee59d67f129aff5d9a87
Pontus Larsson <pl4@sanger.ac.uk>
2011-03-04 14:34:52 +0000

    Changed a key to be a unique composite of variation_id and failed_description_id.

--- sql/patch_61_62_b.sql
+++ sql/patch_61_62_b.sql
@@ -1,5 +1,9 @@
 # drop subsnp_id column on failed_variation
 ALTER TABLE failed_variation DROP COLUMN subsnp_id;
 
+# Drop the key on variation_id column and make it a composite key with failed_description_id instead
+ALTER TABLE failed_variation DROP KEY variation_idx;
+ALTER TABLE failed_variation ADD UNIQUE KEY variation_idx (variation_id,failed_description_id);
+
 # patch identifier
 INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL,'patch', 'patch_61_62_b.sql|change structure of failed_variation table');
