730b4cf624ba317a4cec11f8f82c29aaaeb0cec9
Laurent Gil <lgil@ebi.ac.uk>
2011-03-07 09:36:00 +0000

    - New call system with options - New comment blocks: 			 - @set: tag to describe a set of tables 			 - @info: tag to put additional information for a set of tables or just for a table

--- sql/sql2html.pl
+++ sql/sql2html.pl
@@ -2,9 +2,6 @@
 # 1st Feb 2011
 # Generate an HTML documentation page from an SQL file.
 #
-# Argument 1 : the SQL file
-# Argument 2 : the HTML/output file
-#
 # It needs to have a "javascript like" documentation above each table. e.g.:
 ####################################################################################
 #/**
@@ -47,31 +44,52 @@
 ########################################################################################
 #
 # /** and */ : begin and end of the document block
+# @set       : tag to create a set of tables
 # @table     : name of the sql table
-# @desc      : description of the role/content of the table
+# @desc      : description of the role/content of the table, set or info tags
 # @column    : column_name [tab(s)] Column description. Note: 1 ligne = 1 column
 # @see       : tables names linked to the described table
 # @link      : Internal link to an other table description. The format is ... @link table_name ...
+# @info      : tag to describe additional information about a table or a set of tables
 
 
 use strict;
 use POSIX;
+use Getopt::Long;
 
-my $sql_file  = $ARGV[0];
-my $html_file = $ARGV[1];
+###############
+### Options ###
+###############
+my ($sql_file,$html_file,$set_flag,$sort_tables,$help);
+
+usage() if (!scalar(@ARGV));
+ 
+GetOptions(
+    'i=s' => \$sql_file,
+    'o=s' => \$html_file,
+    'show_set=i' => \$set_flag,
+    'sort=i' => \$sort_tables,
+		'help!' => \$help
+);
+
+usage() if ($help);
 
 if (!$sql_file) {
-	print "Error! Please give a sql file as first argument\n";
+	print "Error! Please give a sql file using the option '-i' \n";
 	exit;
 }
 if (!$html_file) {
-	print "Error! Please give an output file as second argument\n";
+	print "Error! Please give an output file using the option '-o'\n";
 	exit;
 }
 
+#$set_flag ||= 1;
+$set_flag = 1 if (!defined($set_flag));
+$sort_tables = 1 if (!defined($sort_tables));
+
 
 ##############
-### Header  ##
+### Header ###
 ##############
 my $header = qq{
 <html>
@@ -140,11 +158,13 @@ my $footer = qq{
 ################
 my %display_col = ('Show' => 'none', 'Hide' => 'inline');
 my $documentation = {};
-my @tables_names = ();
+my $tables_names = {'default' => []};
 
 my $in_doc = 0;
 my $in_table = 0;
+my $set = 'default';
 my $table = '';
+my $info = '';
 my $nb_by_col = 15;
 my $count_sql_col = 0;
 my $tag_content = '';
@@ -178,14 +198,20 @@ while (<SQLFILE>) {
 	
 	## Parsing of the documentation ##
 	if ($in_doc==1) {
+		# Set of tables name
+		if ($doc =~ /^\@set\s*(\w+)/i and $set_flag == 1) {
+			$set = $1;
+			$tables_names->{$set} = [];
+			next;
+		}		
 		# Table name
-		if ($doc =~ /^\@table\s*(\w+)/i) {
+		elsif ($doc =~ /^\@table\s*(\w+)/i) {
 			$table = $1;
-			push(@tables_names,$table);
-			$documentation->{$table} = { 'desc' => '', 'column' => [], 'see' => [] };
+			push(@{$tables_names->{$set}},$table);
+			$documentation->{$set}{'tables'}{$table} = { 'desc' => '', 'column' => [], 'see' => [], 'info' => [] };
 			$tag = $tag_content = '';		
 		}
-		# Description
+		# Description (used for both set, table and info tags)
 		elsif ($doc =~ /^\@(desc)\s*(.+)$/i) {
 			fill_documentation ($1,$2);
 		}
@@ -197,13 +223,19 @@ while (<SQLFILE>) {
 		elsif ($doc =~ /^\@(see)\s*(.+)$/i) {
 			fill_documentation ($1,$2);	
 		}
+		# Addtional information block
+		elsif ($doc =~ /^\@(info)\s*(.+)$/i) {
+			fill_documentation ();
+			$info = $2;
+			next;
+		}
 		# End of documentation
 		elsif ($doc =~ /^\*\//) { # End of the documentation block
 			fill_documentation (); # Add the last tag content to the documentation hash
 			$in_doc=0;
 			next; 
 		}
-		elsif ($doc =~ /^\s*(.+)$/) { # If a tag content is splitted in several lines
+		elsif ($doc =~ /^\s*(.+)$/) { # If a tag content is split in several lines
 			$tag_content .= " $1";
 		}
 	}
@@ -266,7 +298,7 @@ while (<SQLFILE>) {
 		}
 		
 		elsif ($doc =~ /\).*;/) { # End of the sql table definition
-			if (scalar @{$documentation->{$table}{column}} > $count_sql_col) {
+			if (scalar @{$documentation->{$set}{'tables'}{$table}{column}} > $count_sql_col) {
 				print STDERR "Description of a non existant column in the table $table!\n";
 			}
 			$in_table=0;
@@ -282,18 +314,39 @@ close(SQLFILE);
 ### Core  ##
 ############
 
-@tables_names = sort(@tables_names);
+# Sort the tables names by alphabetic order
+if ($sort_tables == 1) {
+	while ( my($set_name,$tables) = each (%{$tables_names})) {
+		@{$tables} = sort(@{$tables});
+	}
+}
 
+# List of tables by set of tables
 my $html_content = display_tables_list();
 my $table_count = 1;
 my $col_count = 1;
 
-foreach my $t_name (@tables_names) {
-	$html_content .= add_table_name($t_name);
-	$html_content .= add_description($documentation->{$t_name}{desc});
-	$html_content .= add_columns($t_name,@{$documentation->{$t_name}{column}});
-	$html_content .= add_see(@{$documentation->{$t_name}{see}});
-}
+while ( my ($set_name,$tables) = each (%{$tables_names})) {
+	# Set display	
+	if ($set_flag == 1 and $set_name ne 'default') {
+		$html_content .= qq{<hr />\n<h2>Set $set_name</h2>\n};
+		my $set_desc = $documentation->{$set_name}{'desc'};		
+		$html_content .= qq{<p>$set_desc</p>} if (defined($set_desc));
+	}
+	if ($set_name eq 'default' and defined($documentation->{$set_name}{'info'})) {
+		$html_content .= qq{<h2>Additional information about the schema</h2>\n};
+	}	
+	$html_content .= add_info($documentation->{$set_name}{'info'});
+	
+	# Tables display
+	foreach my $t_name (@{$tables}) {
+		$html_content .= add_table_name($t_name);
+		$html_content .= add_description($documentation->{$set_name}{'tables'}{$t_name}{desc});
+		$html_content .= add_info($documentation->{$set_name}{'tables'}{$t_name}{info});	
+		$html_content .= add_columns($t_name,@{$documentation->{$set_name}{'tables'}{$t_name}{column}});
+		$html_content .= add_see(@{$documentation->{$set_name}{'tables'}{$t_name}{see}});
+	}
+}	
 
 
 ## HTML/output file ##
@@ -311,32 +364,37 @@ close(HTML);
 ###############
 
 sub display_tables_list {
-	my $count = scalar @tables_names;
-	my $nb_col = ceil($count/$nb_by_col);
-	my $table_count = 0;
-	my $col_count = 1;
-	my $html = '';
+	my $html = qq{<p>List of the tables:</p>\n};
+	while ( my ($set_name,$tables) = each (%{$tables_names})) {
+		my $count = scalar @{$tables};
+		my $nb_col = ceil($count/$nb_by_col);
+		my $table_count = 0;
+		my $col_count = 1;
 	
-	if ($nb_col>3) { 
-		while ($nb_col>3) {
-			$nb_by_col += 5;
-			$nb_col = ceil($count/$nb_by_col);
+		if ($nb_col>3) { 
+			while ($nb_col>3) {
+				$nb_by_col += 5;
+				$nb_col = ceil($count/$nb_by_col);
+			}
+			$nb_col = 3;
 		}
-		$nb_col = 3;
-	}
 
-	my $html = qq{<p>List of the tables:</p>\n<table><tr><td>\n <ul>\n};
-
-	foreach my $t_name (@tables_names) {
-		if ($table_count == $nb_by_col and $col_count<$nb_col and $nb_col>1){
-			$html .= qq{	</ul></td><td><ul>\n};
-			$table_count = 0;
+		if ($set_flag == 1 and $set_name ne 'default') {
+			$html .= qq{<h2>Set $set_name</h2>\n};
 		}
-		$html .= add_table_name_to_list($t_name);
-		$table_count ++;
+		$html .= qq{<table><tr><td>\n <ul>\n};
+
+		foreach my $t_name (@{$tables}) {
+			if ($table_count == $nb_by_col and $col_count<$nb_col and $nb_col>1){
+				$html .= qq{	</ul></td><td><ul>\n};
+				$table_count = 0;
+			}
+			$html .= add_table_name_to_list($t_name);
+			$table_count ++;
+		}
+		$html .= qq{		</ul>\n</td></tr></table>\n};
 	}
-	$html .= qq{		</ul>\n</td></tr></table>\n<hr />\n};
-	
+	$html .= qq{<hr />\n};
 	return $html;
 }
 
@@ -348,28 +406,55 @@ sub fill_documentation {
 	my $t2 = shift;
 	
 	if ($tag ne '') {
+		# Description tag (info, table or set)
 		if ($tag eq 'desc') {
-			$documentation->{$table}{$tag} = $tag_content;
+			# Additional description
+			if ($info ne '') {
+				$tag_content = $info.'@info@'.$tag_content;
+				# Table: additional information				
+				if ($table ne '') {
+					push(@{$documentation->{$set}{'tables'}{$table}{'info'}},$tag_content);
+				}
+				# Set of tables: additional information
+				else {
+					if (!$documentation->{$set}{'info'}) {
+						$documentation->{$set}{'info'} = [];
+					}
+					push(@{$documentation->{$set}{'info'}},$tag_content);
+				}
+				$info = '';
+			}
+			# Table description
+			elsif ($table ne '') {
+				$documentation->{$set}{'tables'}{$table}{$tag} = $tag_content;
+			}
+			# Set description
+			else {
+				$documentation->{$set}{'desc'} = $tag_content;
+			}
 		}
 		elsif ($tag eq 'column') {
 			$tag_content =~ /(\w+)[\s\t]+(.+)/;
 			
-			my $column = { 'name' => $1,
-										 'type' => '',
+			my $column = { 'name'    => $1,
+								'type'    => '',
 			               'default' => '',
-										 'index' =>'',
-										 'desc' => $2
-									 };
-			push(@{$documentation->{$table}{$tag}},$column);
+								'index'   => '',
+						      'desc'    => $2
+							 };
+			push(@{$documentation->{$set}{'tables'}{$table}{$tag}},$column);
 		}
 		else{
-			push(@{$documentation->{$table}{$tag}},$tag_content);
+			push(@{$documentation->{$set}{'tables'}{$table}{$tag}},$tag_content);
 		}
 	}
 	# New tag initialized
 	if ($t1) {
 		$tag = $t1;
 		$tag_content = $t2;
+	}
+	else {
+		$tag = $tag_content = '';	
 	}
 }
  
@@ -395,9 +480,21 @@ sub add_table_name {
 
 sub add_description {
 	my $desc = shift;
-	return qq{<p>$desc<\p>};
+	return qq{<p>$desc<\p>\n};
 }
 
+sub add_info {
+	my $infos = shift;
+	my $html = '';
+	
+	foreach my $inf (@{$infos}) {
+		my ($title,$content) = split('@info@', $inf);
+		$html .= qq{	<table>
+		<tr><td class="bg3">$title</td><td class="bg1">$content</td></tr>
+	</table>\n};
+	}
+	return $html;
+}
 
 sub add_columns {
 	my @cols = @_;
@@ -462,7 +559,7 @@ sub add_column_index {
 		$i_col =~ s/\s+$//;
 		
 		$is_found{$i_col} = 0;
-		foreach my $col (@{$documentation->{$table}{column}}) {
+		foreach my $col (@{$documentation->{$set}{tables}{$table}{column}}) {
 			if ($col->{name} eq $i_col) {
 				if ($col->{index} ne '') {
 					$col->{index} .= '<br />';
@@ -489,7 +586,7 @@ sub add_column_type_and_default_value {
 	$count_sql_col ++;
 	
 	my $is_found = 0;
-	foreach my $col (@{$documentation->{$table}{column}}) {
+	foreach my $col (@{$documentation->{$set}{'tables'}{$table}{column}}) {
 		if ($col->{name} eq $c_name) {
 			#$col .= "\t$c_type\t";
 			#$col .= "$c_default" if ($c_default ne ''); # Add the default value
@@ -505,3 +602,28 @@ sub add_column_type_and_default_value {
 	}
 }
 
+
+sub usage {
+	
+  print qq{
+  Usage: perl sql2html.pl [OPTION]
+  
+  Convert the SQL documentation into an HTML document.
+	
+  Options:
+
+    -help		Print this message
+      
+    An input file must be specified. This file must be a SQL file, with the "Java-doc like documentation". 
+    For more information, please visit the following page: 
+    http://www.ebi.ac.uk/seqdb/confluence/display/EV/SQL+documentation
+
+    -i          A SQL file name (Required)
+    -o          An HTML output file name (Required) 
+    -show_set   A flag to take into account the set of tables (1) or not (0). 
+                By default, the value is set to 1.
+    -sort       A flag to sort (1) or not (0) the tables by alphabetic order.
+                By default, the value is set to 1.
+  } . "\n";
+  exit(0);
+}
