ab23ed7332bf48b603d1171b5d28f92fc0d8f9e6
Pontus Larsson <pl4@sanger.ac.uk>
2011-03-10 10:26:30 +0000

    Added failed descriptions

--- sql/table.sql
+++ sql/table.sql
@@ -1434,4 +1434,10 @@ INSERT INTO failed_description (failed_description_id,description) VALUES (5,'Va
 INSERT INTO failed_description (failed_description_id,description) VALUES (6,'Variation has no genotypes');
 INSERT INTO failed_description (failed_description_id,description) VALUES (7,'Genotype frequencies do not add up to 1');
 INSERT INTO failed_description (failed_description_id,description) VALUES (8,'Variation has no associated sequence');
+INSERT INTO failed_description (failed_description_id,description) VALUES (9,'Variation submission has been withdrawn by the 1000 genomes project due to high false positive rate');
+INSERT INTO failed_description (failed_description_id,description) VALUES (11,'Allele string obtained from dbSNP does not agree with the submitted alleles'); 
+INSERT INTO failed_description (failed_description_id,description) VALUES (12,'Variation has more than 3 different submitted alleles');         
+INSERT INTO failed_description (failed_description_id,description) VALUES (13,'Alleles contain non-nucleotide characters');  
+INSERT INTO failed_description (failed_description_id,description) VALUES (14,'Alleles contain ambiguity codes');  
+INSERT INTO failed_description (failed_description_id,description) VALUES (15,'Mapped position is not compatible with reported alleles');
 
