e4a5aa066bf97efdd0e2417ef1e2f5bc818fb072
Graham Ritchie <gr5@sanger.ac.uk>
2011-03-23 15:19:19 +0000

    add is_default method and write proper constructor

--- modules/Bio/EnsEMBL/Variation/OverlapConsequence.pm
+++ modules/Bio/EnsEMBL/Variation/OverlapConsequence.pm
@@ -18,6 +18,37 @@
 
 =cut
 
+=head1 NAME
+
+Bio::EnsEMBL::Variation::Overlap
+
+=head1 SYNOPSIS
+
+    use Bio::EnsEMBL::Variation::VariationFeatureOverlapAllele;
+    
+    my $vfoa = Bio::EnsEMBL::Variation::VariationFeatureOverlapAllele->new(
+        -variation_feature_overlap  => $vfo,
+        -variation_feature_seq      => 'A',
+        -is_reference               => 0,
+    );
+
+    print "sequence with respect to the feature: ", $vfoa->feature_seq, "\n";
+    print "sequence with respect to the variation feature: ", $vfoa->variation_feature_seq, "\n";
+    print "consequence SO terms: ", (join ",", map { $_->SO_term } @{ $vfoa->get_all_OverlapConsequences }), "\n";
+
+=head1 DESCRIPTION
+
+A VariationFeatureOverlapAllele object represents a single allele of a 
+VariationFeatureOverlap. It is the super-class of various feature-specific allele
+classes such as TranscriptVariationAllele and RegulatoryFeatureVariationAllele and 
+contains methods not specific to any particular feature type. Ordinarily you will 
+not create these objects yourself, but instead you would create e.g. a 
+TranscriptVariation object which will then VariationFeatureOverlapAlleles based on the 
+allele string of the associated VariationFeature. 
+
+=cut
+
+
 package Bio::EnsEMBL::Variation::OverlapConsequence;
 
 use strict;
@@ -27,7 +58,47 @@ use Bio::EnsEMBL::Variation::Utils::VariationEffect;
 
 sub new {
     my $class = shift;
-    return bless {}, $class;
+    
+    my (
+        $SO_accession,
+        $SO_term,
+        $feature_SO_term,
+        $feature_class,
+        $predicate,
+        $rank,
+        $display_term,
+        $NCBI_term,
+        $description,
+        $label,
+        $is_default,
+    ) = rearrange([qw(
+            SO_ACCESSION
+            SO_TERM
+            FEATURE_SO_TERM
+            FEATURE_CLASS
+            PREDICATE
+            RANK
+            NCBI_TERM
+            DESCRIPTION
+            LABEL
+            IS_DEFAULT
+        )], @_);
+
+    my $self = bless {
+        SO_accession        => $SO_accession,
+        SO_term             => $SO_term,
+        feature_SO_term     => $feature_SO_term,
+        feature_class       => $feature_class,
+        predicate           => $predicate,
+        rank                => $rank,
+        display_term        => $display_term,
+        NCBI_term           => $NCBI_term,
+        description         => $description,
+        label               => $label,
+        is_default          => $is_default,
+    }, $class;
+
+    return $self;
 }
 
 sub new_fast {
@@ -106,6 +177,12 @@ sub label {
     my ($self, $label) = @_;
     $self->{label} = $label if $label;
     return $self->{label};
+}
+
+sub is_default {
+    my ($self, $is_default) = @_;
+    $self->{is_default} = $is_default if defined $is_default;
+    return $self->{is_default};
 }
 
 sub is_definitive {
