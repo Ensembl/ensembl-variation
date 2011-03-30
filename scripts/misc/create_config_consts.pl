69e3175be10ba2801314c7e7d9d5893f8cddea4c
Graham Ritchie <gr5@sanger.ac.uk>
2011-03-30 13:09:04 +0000

    use a hash to avoid duplicate constants

--- scripts/misc/create_config_consts.pl
+++ scripts/misc/create_config_consts.pl
@@ -29,7 +29,7 @@ use base qw(Exporter);
 
 my $code;
 
-my %to_export;
+my $to_export;
 
 for my $attrib_type (@ATTRIB_TYPES) {
    
@@ -37,7 +37,9 @@ for my $attrib_type (@ATTRIB_TYPES) {
     
     my $const = "ATTRIB_TYPE_".uc($type_code);
 
-    push @{ $to_export{attrib_types} ||= [] }, $const;
+    next if $to_export->{attrib_types}->{$const};
+    
+    $to_export->{attrib_types}->{$const} = 1;
    
     $code .= "use constant $const => '$type_code';\n";
 }
@@ -50,7 +52,9 @@ for my $class_set (@VARIATION_CLASSES) {
 
     my $const = "SO_TERM_".uc($term);
 
-    push @{ $to_export{SO_class_terms} ||= [] }, $const;
+    next if $to_export->{SO_class_terms}->{$const};
+
+    $to_export->{SO_class_terms}->{$const} = 1;
 
     $code .= "use constant $const => '$term';\n";
 }
@@ -59,29 +63,38 @@ $code .= "\n";
 
 my $cons_code = "our \@OVERLAP_CONSEQUENCES = (\n";
 
+my $default_consequence_code;
+
 for my $cons_set (@OVERLAP_CONSEQUENCES) {
 
     my $term = $cons_set->{SO_term};
     
     my $const = "SO_TERM_".uc($term);
 
-    push @{ $to_export{SO_consequence_terms} ||= [] }, $const;
+    $code .= "use constant $const => '$term';\n" unless $to_export->{SO_consequence_terms}->{$const};
 
-    $code .= "use constant SO_TERM_".uc($term)." => '$term';\n";
-
-    $cons_code .= "$OVERLAP_CONSEQUENCE_CLASS->new_fast(".Dumper($cons_set)."),\n";
+    $to_export->{SO_consequence_terms}->{$const} = 1;
+    
+    if ($cons_set->{is_default}) {
+        die "Cannot have more than one default OverlapConsequence object!" if $default_consequence_code;
+        $default_consequence_code = "our \$DEFAULT_OVERLAP_CONSEQUENCE = $OVERLAP_CONSEQUENCE_CLASS->new_fast(".Dumper($cons_set).");\n";
+        $cons_code .= "\$DEFAULT_OVERLAP_CONSEQUENCE,\n";
+    }
+    else {
+        $cons_code .= "$OVERLAP_CONSEQUENCE_CLASS->new_fast(".Dumper($cons_set)."),\n";
+    }
 }
 
-$cons_code .= ");\n";
+$cons_code = $default_consequence_code. "\n". $cons_code .");\n";
 
-$hdr .= 'our @EXPORT_OK = qw(@OVERLAP_CONSEQUENCES '.(join ' ', map { @{$_} } values %to_export).');';
+$hdr .= 'our @EXPORT_OK = qw(@OVERLAP_CONSEQUENCES $DEFAULT_OVERLAP_CONSEQUENCE '.(join ' ', map { keys %{ $to_export->{$_} } } keys %$to_export).');';
 
 $hdr .= "\n\n";
 
 $hdr .= 'our %EXPORT_TAGS = ( ';
 
-for my $key (keys %to_export) {
-    $hdr .= "$key => [qw(".(join ' ', @{ $to_export{$key} }).")], "
+for my $key (keys %$to_export) {
+    $hdr .= "$key => [qw(".(join ' ', keys %{ $to_export->{$key} }).")], "
 }
 
 $hdr .= " );\n\n";
