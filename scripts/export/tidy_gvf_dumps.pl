08617f1e01308deffadebe4577f4d3046158e443
Graham Ritchie <gr5@sanger.ac.uk>
2011-04-04 19:36:17 +0000

    add new dump types, updated README

--- scripts/export/tidy_gvf_dumps.pl
+++ scripts/export/tidy_gvf_dumps.pl
@@ -38,32 +38,39 @@ for my $species (@species_dirs) {
     `rm -f $dir/*.out`;
     `rm -f $dir/*.err`;
     open README, ">$dir/README" or die "Failed to create README file\n";
-    print README $readme, "\n";
+    $species =~ s/(.)/uc($1)/e;
+    print README sprintf($readme,$species,$species,$species,$species), "\n";
 }
 
 __DATA__
 This directory contains a gzip compressed GVF (Genome Variation Format) file
 containing all germline variations from the current Ensembl release for this 
-species, named <species>.gvf.gz. If this species has any variations failed 
-by the Ensembl QC checks there will also be a file called 
-<species>_failed.gvf.gz identifying these. The consequences of the variants, 
-as called by the Ensembl variation consequence pipeline, will be included in 
-the GVF files as of release 62, scheduled for April 2011.
+species, named %s.gvf.gz. If this species has any structural 
+variation data this is provided in a file named 
+%s_structural_variations.gvf.gz
+
+Any variations that have been failed by the Ensembl QC checks will be included
+in a file called %s_failed.gvf.gz.
+
+A file including the consequences of the variations on the Ensembl transcriptome, 
+as called by the variation consequence pipeline, can be found in a file called 
+%s_incl_consequences.gvf.gz.
 
 For human we also provide a file containing all somatic mutations in the
 database, files with germline variations observed in the Watson and Venter 
 genomes along with their genotypes, and files containing allele frequencies
 from several of the HapMap and 1000 genomes pilot study populations.
 
-Please note that depending on the amount of variation data available for 
-this species the uncompressed file may be very large (e.g. the entire 
-germline file for human is ~3GB).
+Please note that depending on the amount of variation data available for this 
+species the uncompressed file may be very large (e.g. the entire germline file 
+for human is ~3GB and the file including consequences is ~9GB).
 
 The data contained in these files is presented in GVF format, this is a
-simple tab-delimited format which shows the location of each variant along
-with the reference and variant sequences, an identifier for the source
-of the data (typically a dbSNP rsID), and other relevant information (e.g. 
-genotypes and allele freqeuncies), a short example is presented below. For 
+simple tab-delimited format derived from GFF3 which shows the location of 
+each variant along with the reference and variant sequences, an identifier 
+for the source of the data (typically a dbSNP rsID), and other relevant 
+information (e.g. genotypes, allele frequencies, the predicted effect of 
+this variant on a transcript), a short example is presented below. For 
 more details about GVF please refer to:
 
 Reese, M.G. et al. A standard variation file format for human genome sequences.
@@ -88,13 +95,13 @@ Example content from the human germline GVF dump is shown below:
 ##data-source Source=ensembl;version=61;url=http://e61.ensembl.org/Homo_sapiens
 ##file-version 61
 ##sequence-region 11 1 135006516
-11	dbSNP	SNV	61554	61554	.	+	.	ID=1;Variant_seq=C;Dbxref=dbSNP:rs77355429;Reference_seq=A
-11	dbSNP	SNV	61645	61645	.	+	.	ID=2;Variant_seq=C;Dbxref=dbSNP:rs61869610;Reference_seq=G
-11	dbSNP	SNV	61868	61868	.	+	.	ID=3;Variant_seq=A;Dbxref=dbSNP:rs365553;Reference_seq=G
-11	dbSNP	SNV	67521	67521	.	+	.	ID=4;Variant_seq=G;Dbxref=dbSNP:rs76077193;Reference_seq=A
-11	dbSNP	SNV	70073	70073	.	+	.	ID=5;Variant_seq=C;Dbxref=dbSNP:rs1099703;Reference_seq=T
-11	dbSNP	SNV	70113	70113	.	+	.	ID=6;Variant_seq=T;Dbxref=dbSNP:rs76956518;Reference_seq=C
-11	dbSNP	SNV	70133	70133	.	+	.	ID=7;Variant_seq=T;Dbxref=dbSNP:rs112541484;Reference_seq=C
-11	dbSNP	SNV	70135	70135	.	-	.	ID=8;Variant_seq=A;Dbxref=dbSNP:rs4120101;Reference_seq=C
-11	dbSNP	SNV	70146	70146	.	-	.	ID=9;Variant_seq=C;Dbxref=dbSNP:rs4120100;Reference_seq=A
+11	dbSNP	SNV	61554	61554	.	+	.	ID=1;Variant_seq=C;Dbxref=dbSNP_132:rs77355429;Reference_seq=A
+11	dbSNP	SNV	61645	61645	.	+	.	ID=2;Variant_seq=C;Dbxref=dbSNP_132:rs61869610;Reference_seq=G
+11	dbSNP	SNV	61868	61868	.	+	.	ID=3;Variant_seq=A;Dbxref=dbSNP_132:rs365553;Reference_seq=G
+11	dbSNP	SNV	67521	67521	.	+	.	ID=4;Variant_seq=G;Dbxref=dbSNP_132:rs76077193;Reference_seq=A
+11	dbSNP	SNV	70073	70073	.	+	.	ID=5;Variant_seq=C;Dbxref=dbSNP_132:rs1099703;Reference_seq=T
+11	dbSNP	SNV	70113	70113	.	+	.	ID=6;Variant_seq=T;Dbxref=dbSNP_132:rs76956518;Reference_seq=C
+11	dbSNP	SNV	70133	70133	.	+	.	ID=7;Variant_seq=T;Dbxref=dbSNP_132:rs112541484;Reference_seq=C
+11	dbSNP	SNV	70135	70135	.	-	.	ID=8;Variant_seq=A;Dbxref=dbSNP_132:rs4120101;Reference_seq=C
+11	dbSNP	SNV	70146	70146	.	-	.	ID=9;Variant_seq=C;Dbxref=dbSNP_132:rs4120100;Reference_seq=A
 
