cb662471fe3f78d010c2bf923861703760f7febe
Graham Ritchie <gr5@sanger.ac.uk>
2011-04-08 12:24:44 +0000

    added import for base variation adaptor

--- modules/Bio/EnsEMBL/Variation/DBSQL/AlleleFeatureAdaptor.pm
+++ modules/Bio/EnsEMBL/Variation/DBSQL/AlleleFeatureAdaptor.pm
@@ -66,6 +66,7 @@ package Bio::EnsEMBL::Variation::DBSQL::AlleleFeatureAdaptor;
 
 use Bio::EnsEMBL::Variation::AlleleFeature;
 use Bio::EnsEMBL::DBSQL::BaseFeatureAdaptor;
+use Bio::EnsEMBL::Variation::DBSQL::BaseAdaptor;
 use Bio::EnsEMBL::Utils::Exception qw(throw);
 use Bio::EnsEMBL::Utils::Sequence qw(expand);
 use Bio::EnsEMBL::Variation::Utils::Sequence qw(strain_ambiguity_code);
