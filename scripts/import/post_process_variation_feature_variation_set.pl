d2290df30e28c59ddf8f4167cd15c06f6f470b4f
Pontus Larsson <pl4@sanger.ac.uk>
2011-04-20 10:08:08 +0000

    Rewrote most of the script

--- scripts/import/post_process_variation_feature_variation_set.pl
+++ scripts/import/post_process_variation_feature_variation_set.pl
@@ -4,11 +4,6 @@
 
     This script will post-process the variation_feature table to update the variation_set_id column to contain the
     primary keys of the variation_sets which the corresponding variation is part of.
-    It does so by fetching all variations for either a specified variation set (useful if only updating) or for
-    all available variation sets. For now, we'll assume that the script and server can handle ALL variations
-    belonging to a set simultaneously. If not, it may be necessary to split the query by e.g. seq_region or
-    variation_id range. Note also that the script disables the keys. This may not be entirely necessary,
-    it will probably suffice to just disable the index on the variation_set_id column...
     
 =cut
 
@@ -19,22 +14,28 @@ use Getopt::Long;
 
 use Bio::EnsEMBL::Registry;
 
-my $MAX_VARIATION_SETS = 64;
+our $MAX_VARIATION_SETS = 64;
 # This is just for convenience in case we have a custom name for the variation_feature table
-my $VARIATION_FEATURE_TABLE = 'variation_feature';
+our $VARIATION_FEATURE_TABLE = 'variation_feature';
 
 my @option_defs = (
   'species=s',
+  'group=s',
   'registry_file=s',
-  'chromosome=s@',
-  'verbose!'
+  'quiet!',
+  'help!'
 );
 
 my %options;
 GetOptions(\%options,@option_defs);
 
 my $species = $options{'species'};
+my $group = $options{'group'} || q{variation};
 my $registry_file = $options{'registry_file'};
+my $quiet = $options{'quiet'};
+my $help = $options{'help'};
+
+usage() if ($help);
 
 #ÊCheck that required parameters were passed
 die ("Required argument '-species' was not specified") unless (defined($species));
@@ -45,184 +46,177 @@ my $registry = 'Bio::EnsEMBL::Registry';
 $registry->load_all($registry_file);
 
 #ÊGet a dbadaptor to the variation database
-my $dbVar = $registry->get_DBAdaptor($species,"variation") or die ("Could not get variation DBAdaptor for $species");
+my $dbVar = $registry->get_DBAdaptor($species,$group) or die ("Could not get variation DBAdaptor for $species and $group");
 
-#ÊIf no chromosomes were specified, get all available in the database
-my @chromosomes;
-if (defined($options{'chromosome'})) {
-    @chromosomes = @{$options{'chromosome'}};
-}
-else {
-    my $stmt = qq{
-        SELECT DISTINCT
-            name
+#ÊCall the post-processing subroutine
+post_process($dbVar,$quiet);
+
+sub post_process {
+    my $dbVar = shift;
+    my $quiet = shift;
+    
+    my $stmt; 
+    
+    #ÊFirst of all, make sure that no primary key for variation_sets is too large to fit inside the set
+    $stmt = qq{
+        SELECT
+            MAX(variation_set_id)
         FROM
-            seq_region;
+            variation_set
     };
-    @chromosomes = @{$dbVar->dbc->db_handle->selectcol_arrayref($stmt)};
-}
+    my $max_id = $dbVar->dbc->db_handle->selectall_arrayref($stmt)->[0][0];
+    die ("There are variation_sets with primary keys greater than $MAX_VARIATION_SETS. This will not be possible to represent with the MySQL SET data type in the $VARIATION_FEATURE_TABLE!") if ($max_id > $MAX_VARIATION_SETS);
 
-# This statement will set the variation_set_id column for the sets that are explicitly set in the variation_set_variation table
-my $stmt = qq{
-    UPDATE
-        variation_feature vf USE INDEX (pos_idx) JOIN
-        seq_region sr ON (
-            sr.seq_region_id = vf.seq_region_id
-        )
-    SET
-        vf.variation_set_id = (
-            SELECT
-                GROUP_CONCAT(vsv.variation_set_id)
-            FROM
-                variation_set_variation vsv USE INDEX (PRIMARY)
-            WHERE
-                vsv.variation_id = vf.variation_id
-            GROUP BY
-                vsv.variation_id
-        )
-    WHERE
-        sr.name = ?
-};
-my $set_explicit_sets = $dbVar->dbc->prepare($stmt);
-
-#ÊThis statment will update the variation_set_id column to also contain the parent set in case there is a subset structure
-$stmt = qq{
-    UPDATE
-        variation_feature vf USE INDEX (pos_idx) JOIN
-        seq_region sr ON (
-            sr.seq_region_id = vf.seq_region_id
-        ),
-        variation_set_structure vss
-    SET
-        vf.variation_set_id = (vf.variation_set_id | POW(2,(vss.variation_set_super - 1)))
-    WHERE
-        sr.name = ? AND
-        FIND_IN_SET(vss.variation_set_sub,vf.variation_set_id)
-};
-my $set_inherited_sets = $dbVar->dbc->prepare($stmt);
-
-# Loop over the chromosomes
-foreach my $chromosome (@chromosomes) {
+    ###Ê
+    ## Define the statements we will be using
+    my $tmp_table = q{tmp_vs_vf_upd};    
     
-    #ÊFirst, get the explicit sets
-    my $updated = $set_explicit_sets->execute($chromosome);
+    # Create a temporary table to hold the variation_set_id string until we're ready to update variation_feature
+    $stmt = qq{
+        CREATE TABLE
+            $tmp_table (
+                variation_id INT NOT NULL, 
+                variation_set_id SET(
+                    '1','2','3','4','5','6','7','8',
+                    '9','10','11','12','13','14','15','16',
+                    '17','18','19','20','21','22','23','24',
+                    '25','26','27','28','29','30','31','32',
+                    '33','34','35','36','37','38','39','40',
+                    '41','42','43','44','45','46','47','48',
+                    '49','50','51','52','53','54','55','56',
+                    '57','58','59','60','61','62','63','64'
+                ) NOT NULL DEFAULT '', 
+                PRIMARY KEY (variation_id) 
+            );
+    };
+    my $tmp_tbl_sth = $dbVar->dbc->prepare($stmt);
     
-    #ÊThen set the implicit sets by looping until no more rows are updated
-    while ($updated) {
-        
-        $updated = $set_inherited_sets->execute($chromosome);
-        
-    }
-}
-
-#ÊGet a VariationSetAdaptor
-my $vs_adaptor = $dbVar->get_VariationSetAdaptor() or die ("Could not get a VariationSetAdaptor from variation db");
-
-#ÊGet a VariationFeatureAdaptor
-my $vf_adaptor = $dbVar->get_VariationFeatureAdaptor() or die ("Could not get VariationFeatureAdaptor from variation db");
-
-# If a variation_set was specified, get an object for it
-my @variation_sets;
-if (defined($options{'variation_set_id'})) {
-    foreach my $variation_set_id (@{$options{'variation_set_id'}}) {
-        my $variation_set = $vs_adaptor->fetch_by_dbID($variation_set_id) or warn ("Could not get a VariationSet for variation_set_id '$variation_set_id'");
-        push(@variation_sets,$variation_set);
-    }
-}
-elsif (defined($options{'variation_set_name'})) {
-    foreach my $variation_set_name (@{$options{'variation_set_name'}}) {
-        my $variation_set = $vs_adaptor->fetch_by_name($variation_set_name) or warn ("Could not get a VariationSet for variation_set_name '$variation_set_name'");
-        push(@variation_sets,$variation_set);
-    }
-}
-# Else, get all VariationSets
-else {
-    push(@variation_sets,@{$vs_adaptor->fetch_all()});
-}
-
-#ÊSince we just need the variation_feature_ids for variations belonging to a variation set, do a direct SQL query instead of going through the API
-my $stmt = qq{
-    SELECT
-        vf.variation_feature_id
-    FROM
-        variation_set_variation vsv JOIN
-        `$VARIATION_FEATURE_TABLE` vf ON (
-            vf.variation_id = vsv.variation_id
-        )
-    WHERE
-        vsv.variation_set_id = ?
-};
-my $sel_sth = $dbVar->dbc->prepare($stmt);
-
-# Prepare a statement for updating the variation_feature table
-$stmt = qq{
-    UPDATE
-        `$VARIATION_FEATURE_TABLE`
-    SET 
-        variation_set_id = (variation_set_id | ?)
-    WHERE
-        variation_feature_id = ?
-    LIMIT 1
-};
-my $up_sth = $dbVar->dbc->prepare($stmt);
-
-#print STDOUT localtime() . qq{\tDisabling keys on the variation_feature table...} if (defined($options{'verbose'}));
-
-#ÊTemporarily disable the keys on the variation_feature to speed up the updates
-$stmt = qq{
-    ALTER TABLE
-        `$VARIATION_FEATURE_TABLE`
-    DISABLE KEYS
-};
-#$dbVar->dbc->do($stmt);
-
-#print STDOUT qq{ Done!\n} if (defined($options{'verbose'}));
-
-# Wrap the actual processing in an eval statement since if we fall over, we need to enable the keys on the table again
-eval {
+    # Insert the variation_id and a comma-separated list of sets it explicitly belongs to into the temp_table
+    $stmt = qq{
+        INSERT INTO
+            $tmp_table (
+                variation_id,
+                variation_set_id
+            )
+        SELECT
+            vsv.variation_id,
+            GROUP_CONCAT(vsv.variation_set_id)
+        FROM
+            variation_set_variation vsv
+    };
+    my $ins_expl_sth = $dbVar->dbc->prepare($stmt);
     
-    #ÊLoop over the variation sets
-    while (my $variation_set = shift(@variation_sets)) {
+    #ÊAdd the implicit parent sets to the list of variation_sets
+    $stmt = qq{
+        UPDATE
+            $tmp_table t,
+            variation_set_structure vss
+        SET
+            t.variation_set_id = (t.variation_set_id | POW(vss.variation_set_super-1,2))
+        WHERE
+            t.variation_set_id & vss.variation_set_sub
+    };
+    my $upd_impl_sth = $dbVar->dbc->prepare($stmt);
+    
+    #ÊUpdate the variation_feature table
+    $stmt = qq{
+        UPDATE
+            $VARIATION_FEATURE_TABLE vf,
+            $tmp_table t
+        SET
+            vf.variation_set_id = t.variation_set_id
+        WHERE
+            vf.variation_id = t.variation_id
+    };
+    my $upd_vf_sth = $dbVar->dbc->prepare($stmt);
+    
+    
+    ####
+    ## Post-process
+    
+    #ÊFirst, create the temporary table (drop it if it already exists)
+    $stmt = qq{
+        DROP TABLE IF EXISTS
+            $tmp_table
+    };
+    print STDOUT "Dropping temp table $tmp_table if it exists..." unless ($quiet);
+    $dbVar->dbc->do($stmt);
+    print STDOUT "done!\n" unless ($quiet);
+    
+    print STDOUT "Creating temp table $tmp_table..." unless ($quiet);
+    $tmp_tbl_sth->execute();
+    print STDOUT "done!\n" unless ($quiet);
+    
+    # Next, insert the explicitly assigned variation_set_ids
+    print STDOUT "Adding explicitly assigned variation_set primary keys to $tmp_table..." unless ($quiet);
+    $ins_expl_sth->execute();
+    print STDOUT "done!\n" unless ($quiet);
+    
+    # Now, insert the implicit variation_set relationships. Because the sets can be nested to any level, we need to iterate until no more rows are updated
+    my $update_count = 1;
+    while ($update_count) {
         
-        # Skip any undefs
-        next unless (defined($variation_set));
+        print STDOUT "Adding implicitly assigned variation_set primary keys to $tmp_table..." unless ($quiet);
+        $upd_impl_sth->execute();
+        print STDOUT "done!\n" unless ($quiet);
+        $update_count = $upd_impl_sth->rows();
+        print STDOUT "$update_count rows were updated, will " . ($update_count ? "" : "NOT ") . "repeat the statement\n" unless ($quiet);
         
-        # If the variation_set_id is above 64, we cannot store it in the set. Either we have hit the limit for what we can actually store or the variation_set primary key column is sparsely populated. Anyway, can't add this set to the VariationFeature so warn and skip
-        if ($variation_set->dbID() > $MAX_VARIATION_SETS) {
-            warn ("The VariationSet '" . $variation_set->name() . "' has primary key " . $variation_set->dbID() . "! This number is too large to store in the VariationFeature table." .
-                  "If possible, review the primary keys in the variation_set table (and related tables) and try to assign a lower key value." .
-                  "If not possible, we cannot store any more VariationSets with this construct!");
-            next;
-        }
-        
-        # Calculate the bit value for this VariationFeature's set membership
-        my $bitvalue = (2 ** ($variation_set->dbID() - 1));
-        
-        print STDOUT localtime() . "\tProcessing variation features belonging to VariationSet '" . $variation_set->name() . "', bitvalue for this set (dbID = " . $variation_set->dbID() . ") is $bitvalue...\n" if (defined($options{'verbose'}));
-        
-        # Execute the SQL to get the variation features
-        $sel_sth->execute($variation_set->dbID());
-        
-        # Loop over the results
-        while (my $vf = $sel_sth->fetchrow_arrayref()) {
-        
-            # Update the variation_set_id set
-            $up_sth->execute($bitvalue,$vf->[0]);
-                
-        }
-        
-        print STDOUT qq{Done!\n} if (defined($options{'verbose'}));
     }
-}; warn ($@) if ($@);
+    
+    # Finally, update the variation_set_id column in variation_feature
+    print STDOUT "Updating variation_feature with the variation_set_id column from $tmp_table..." unless ($quiet);
+    $upd_vf_sth->execute();
+    print STDOUT "done!\n" unless ($quiet);
+    
+    # ...and lastly, drop the temporary table
+    print STDOUT "Dropping the temp table $tmp_table..." unless ($quiet);
+    $stmt = qq{
+        DROP TABLE
+            $tmp_table
+    };
+    $dbVar->dbc->do($stmt);
+    print STDOUT "done!\n" unless ($quiet);
+    
+    # And that's it
+    print STDOUT "Post-processing complete!\n" unless ($quiet);
+}
 
-#ÊEnable the keys again
-#print STDOUT localtime() . qq{\tEnabling keys on the variation_feature table...} if (defined($options{'verbose'}));
-$stmt = qq{
-    ALTER TABLE
-        `$VARIATION_FEATURE_TABLE`
-    ENABLE KEYS
+sub usage {
+    
+    print STDOUT qq{
+Usage:
+
+  $0 
+    -registry_file f 
+    -species s 
+    -group g 
+    -quiet 
+    
+Description:
+
+  Post-process the variation_feature table to update the variation_set_id column to be a
+  list of the primary keys of all variation_sets which the corresponding variation belongs to.
+
+Command line switches:
+
+  -registry_file f  (Required)
+                    An Ensembl registry configuration file with connection details to the
+                    relevant database.
+
+  -species s        (Required)
+                    The species of the database for which to do the post-processing.
+
+  -group g          (Optional)
+                    The group identifier for the database to post-process as it is specified
+                    in the registry configuration file. Defaults to 'variation'.
+                  
+  -quiet            (Optional)
+                    Suppresses output.
+                    
+  -help             Displays this message.
+        
 };
-#$dbVar->dbc->do($stmt);
-
-#print STDOUT qq{ Done!\n} if (defined($options{'verbose'}));
-
+    
+    exit(0);
+}
