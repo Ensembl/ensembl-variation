ad0c81775aeca899562b0ac142c163470606977a
William McLaren <wm2@ebi.ac.uk>
2011-04-27 15:44:47 +0000

    tag_snps.pl now does the job of create_ld_table.pl and tag_snps.pl, all in one nice package

--- scripts/import/tag_snps.pl
+++ scripts/import/tag_snps.pl
@@ -1,163 +1,177 @@
-#!/usr/local/ensembl/bin/perl
-
+#!/software/bin/perl
 use strict;
 use warnings;
 
 use Getopt::Long;
-use Bio::EnsEMBL::Variation::DBSQL::DBAdaptor;
 use ImportUtils qw(load);
+use Bio::EnsEMBL::Registry;
 use FindBin qw( $Bin );
 
-use constant MAX_SIZE => 30_000_000;
 
+##
+##  run with bsub -q long -o [tmpdir]/output_tag.txt perl tag_snps.pl -tmpdir [tmpdir] -tmpfile [tmpfile]
+use constant MAX_SIZE => 500_000_000;
 my ($TMP_DIR, $TMP_FILE, $species, $registry_file);
 
 
 GetOptions('species=s' => \$species,
 	   'tmpdir=s'  => \$ImportUtils::TMP_DIR,
-	   'tmpfile=s' => \$ImportUtils::TMP_FILE
-	   'registry_file=s' => $registry_file);
+	   'tmpfile=s' => \$ImportUtils::TMP_FILE,
+	   'registry_file=s' => \$registry_file);
 
 warn("Make sure you have a updated ensembl.registry file!\n");
 
 $registry_file ||= $Bin . "/ensembl.registry";
 
-##
-## bsub -q long -W12:00 -o [tmpdir]/output_tag.txt perl tag_snps.pl -tmpdir [tmpdir]/tag_snps -tmpfile tag_snps.txt
-##
-Bio::EnsEMBL::Registry->load_all( $registry_file );
+$TMP_DIR = $ImportUtils::TMP_DIR;
+$TMP_FILE = $ImportUtils::TMP_FILE;
 
 #added default options
 $species ||= 'human';
-
-$TMP_DIR  = $ImportUtils::TMP_DIR;
-$TMP_FILE = $ImportUtils::TMP_FILE;
-
+Bio::EnsEMBL::Registry->load_all( $registry_file );
 my $dbVariation = Bio::EnsEMBL::Registry->get_DBAdaptor($species,'variation');
 
-my $population_id;
-my $population_name;
-#get all populations to be tagged (HapMap and PerlEgen)
-my $sth = $dbVariation->dbc->prepare(qq{SELECT s.sample_id, s.name
-					    FROM population p, sample s
-					    WHERE (s.name like 'PERLEGEN:AFD%'
-					    OR s.name like 'CSHL-HAPMAP%'
-						OR s.name like '1000GENOMES:low_coverage%')
-					    AND s.sample_id = p.sample_id
-					});
+my $siblings;
+create_LD_table($dbVariation); #create the LD table if it is not there yet
+my $in_str = get_LD_populations($dbVariation,$siblings);
+
+my $sth = $dbVariation->dbc->prepare(qq{SELECT c.sample_id,c.seq_region_id,c.seq_region_start,c.seq_region_end,c.genotypes,ip.population_sample_id
+				 FROM compressed_genotype_single_bp c FORCE INDEX(pos_idx), individual_population ip
+				 WHERE  ip.individual_sample_id = c.sample_id
+				 AND   ip.population_sample_id $in_str
+			     },{mysql_use_result => 1});
 
 $sth->execute();
-$sth->bind_columns(\$population_id,\$population_name);
-#get all the children that we do not want in the genotypes
-my $siblings = {}; # hash {$individual_id} ,where the individual is sibling of another one
-my @pops;
-while($sth->fetch){
-    #if($population_name =~ /CEU|YRI|MEX/){
-	&get_siblings($dbVariation,$population_id,$siblings);
-    #}
-    push @pops, $population_id;
-}
-
-my $in_str = " IN (" . join(',', @pops). ")";
-
-#select all SNPs in the population given with genotypes, and order them by variation
-$sth = $dbVariation->dbc->prepare(qq{SELECT STRAIGHT_JOIN c.genotypes, c.seq_region_id, 
-				            c.seq_region_start, ip.individual_sample_id, ip.population_sample_id
-						FROM compressed_genotype_single_bp c FORCE INDEX(pos_idx), individual_population ip
-						WHERE c.sample_id = ip.individual_sample_id
-						AND   ip.population_sample_id $in_str
-						ORDER BY c.seq_region_id,c.seq_region_start
-#						LIMIT 10000
-					    },{mysql_use_result=>1});
-
-my ($genotypes, $seq_region_id, $seq_region_start,$individual_id);
-my $buffer = {}; #buffer with data to write to file
-my $blob; # contains the genotype information compressed
-my @genotypes = ();
-my $seq_region = {};
-$sth->execute();
-$sth->bind_columns(\$genotypes, \$seq_region_id, \$seq_region_start,\$individual_id,\$population_id);
-while ($sth->fetch()){
-    $seq_region->{$seq_region_id}++; #store the seq_region_id
-    #only print genotypes without parents genotyped
-    if (!exists $siblings->{$population_id.'-'.$individual_id}){
-	#uncompress the genotypes
-	$blob = substr($genotypes,2);
-	@genotypes = unpack("naa" x (length($blob)/4),$blob);
-
-	unshift @genotypes, substr($genotypes,1,1); #add the second allele of the first genotype
-	unshift @genotypes, substr($genotypes,0,1); #add the first allele of the first genotype
-	unshift @genotypes, 0; #the first SNP is in the position indicated by the seq_region1
-	my ($snp_start, $allele_1, $allele_2);
-	#print the information to the file
-	for (my $i=0; $i < @genotypes -1;$i+=3){
-	    if ($genotypes[$i] == 0){
-		$snp_start = $seq_region_start; #first SNP is in the beginning of the region
-	    }
-	    else{
-		$snp_start += $genotypes[$i] +1;
-	    }
-	    #genotype
-	    $allele_1 = $genotypes[$i+1];
-	    $allele_2 = $genotypes[$i+2];
-	    
-	    #print the data to the file with the information in the chromosome
-	    print_buffered($buffer,"$TMP_DIR/tag_snps_$population_id\-$seq_region_id\.txt",
-			   join("\t", $allele_1, $allele_2, $snp_start) . "\n");
-	}
-    }
-}
+debug("Dumping genotype data");
+create_files($sth,$siblings);
 $sth->finish();
-print_buffered($buffer);
-my $call;
+
+my @files = glob("$TMP_DIR/dump*");
+my $call = '';
 my @stats;
-#when the data is selected in the files (one per chromosome), send the jobs to the farm
+my $bsub_flag = 0;
+debug("Submitting calculate LD jobs");
+
+
 my $files_size = {};
-foreach my $file (keys %{$buffer}){
+foreach my $file (@files){
     @stats = stat($file);
     $files_size->{$file} = $stats[7];
 }
 foreach my $file (sort {$files_size->{$b} <=> $files_size->{$a}} keys %{$files_size}){
-    $file =~ /tag_snps_(\d+)\-.*/; #extract the population_id from the name of the file
+	
+    $file =~ /.*_(\d+)_(\d+)/;
+	
+    @stats = stat($file);
+    if ($stats[7] > MAX_SIZE){
+#once the files are created, we have to calculate the ld
+	$call = "bsub -M15000000 -R'select[mem>15000] rusage[mem=15000]' -q long -J ld_calculation_$1\_$2 -o $TMP_DIR\/$1\_$2\_farm.out ";
+	$bsub_flag = 1;
+    }
+    $call .= "perl calculate_ld_table.pl -tmpdir $TMP_DIR -tmpfile $TMP_FILE -ldfile $file ";
+    print $call,"\n";
+    system($call);
+    $call = '';
+}
+
+if($bsub_flag) {
+	sleep(60);
+	$call = "bsub -K -W2:00 -w 'done(ld_calculation*)' -J waiting_ld date";
+	system($call);
+}
+
+# import the data in the LD table
+debug("Importing LD values to DB");
+$call = "cat $TMP_DIR/$TMP_FILE*.out > $TMP_DIR/$TMP_FILE";
+system($call);
+load($dbVariation->dbc,qw(pairwise_ld sample_id seq_region_id seq_region_start seq_region_end r2));
+
+$bsub_flag = 0;
+debug("Submitting find tag SNPs jobs");
+$call = '';
+
+foreach my $file (sort {$files_size->{$b} <=> $files_size->{$a}} keys %{$files_size}){
+    $file =~ /.*_(\d+)_(\d+)/;
+	
     if ($files_size->{$file} > MAX_SIZE){
-	$call = "bsub -q long -J tag_snps_$1 -R'select[mem>15000] rusage[mem=15000]' -M15000000 ";
+	$call = "bsub -q long -J tag_snps_$1\_$2 -o $TMP_DIR\/$1\_$2\_farm.out -R'select[mem>15000] rusage[mem=15000]' -M15000000 ";
     }
     $call .= "/software/bin/perl select_tag_snps.pl $file ";
-    $call .= " -tmpdir $TMP_DIR -population_id $1 -species $species";
+    $call .= " -tmpdir $TMP_DIR -species $species";
 	$call .= " -registry_file $registry_file" if defined($registry_file);
     
-#   print $call,"\n";
+    print $call,"\n";
     system($call); #send the job to one queue
     $call = '';
     
 }
 
- $call = "bsub -K -w 'done(tag_snps*)' -J waiting_process sleep 1"; #waits until all snp_tagging have finished to continue
- system($call);
+if($bsub_flag) {
+	$call = "bsub -K -w 'done(tag_snps*)' -J waiting_process sleep 1"; #waits until all snp_tagging have finished to continue
+	system($call);
+}
 
-&import_tagg_snps($dbVariation);    
+debug("Importing tag SNPs");
+&import_tagg_snps($dbVariation);
 
-#for a given population, gets all individuals that are children (have father or mother)
-sub get_siblings{
-    my $dbVariation = shift;
+
+unlink(<$TMP_DIR/tag_snps*.out>);
+
+sub store_file{
+    my $buffer = shift;
+    my $individual_id = shift;
+    my $seq_region_start = shift;
+    my $genotype = shift;
     my $population_id = shift;
-    my $siblings = shift;
+    my $seq_region_id = shift;
 
-    my $sth_individual = $dbVariation->dbc()->prepare(qq{SELECT i.sample_id
-							     FROM individual i, individual_population ip
-							     WHERE ip.individual_sample_id = i.sample_id
-							     AND ip.population_sample_id = ? 
-							     AND i.father_individual_sample_id IS NOT NULL
-							     AND i.mother_individual_sample_id IS NOT NULL
-							 });
-    my ($individual_id);
-    $sth_individual->execute($population_id);
-    $sth_individual->bind_columns(\$individual_id);
-    while ($sth_individual->fetch){
-	$siblings->{$population_id.'-'.$individual_id}++; #necessary to have in the key the population, since some individuals are shared between
-	                                                   #populations
+    #get the first byte of the string, and unpack it (the genotype, without the gaps)
+    my $blob = substr($genotype,2);
+    #the array contains the uncompressed value of genotype, always in the format number_gaps . genotype		  
+    my @genotypes = unpack("naa" x (length($blob)/4),$blob);
+    unshift @genotypes, substr($genotype,1,1); #add the second allele of the first genotype
+    unshift @genotypes, substr($genotype,0,1); #add the first allele of the first genotype
+    unshift @genotypes, 0; #the first SNP is in the position indicated by the seq_region1
+    my $snp_start;
+    my $allele_1;
+    my $allele_2;
+    for (my $i=0; $i < @genotypes -1;$i+=3){
+	#number of gaps
+	if ($i == 0){
+	    $snp_start = $seq_region_start; #first SNP is in the beginning of the region
+	}
+	else{
+            #ignore when there is more than 1 genotype in the same position
+	    if ($genotypes[$i] == 0){
+		$snp_start += 1;
+		next; 
+	    }
+	    $snp_start += $genotypes[$i] +1;
+	}
+	#genotype
+	$allele_1 = $genotypes[$i+1];
+	$allele_2 = $genotypes[$i+2];
+	print_buffered($buffer,"$TMP_DIR/dump_data_$population_id\_$seq_region_id.txt",join("\t",$snp_start,$individual_id,$allele_1,$allele_2)."\n");
     }
-    return $siblings;
+}
+
+
+#creates all files, one per chromosome, to run the LD calculation
+sub create_files{
+    my $sth = shift;
+    my $siblings = shift;
+    
+    my $buffer = {};
+   
+    my ($individual_id, $seq_region_id, $seq_region_start,$seq_region_end,$genotypes, $population_id);
+    $sth->bind_columns(\$individual_id, \$seq_region_id, \$seq_region_start, \$seq_region_end, \$genotypes, \$population_id);
+    while($sth->fetch()) {
+	if (!exists $siblings->{$population_id . '-' . $individual_id}){ #necessary to use the population_id
+	    store_file($buffer,$individual_id,$seq_region_start,$genotypes,$population_id,$seq_region_id);
+	}
+    }
+    print_buffered($buffer);
+    
 }
 
 sub print_buffered {
@@ -170,7 +184,7 @@ sub print_buffered {
     if( ! $filename ) {
 	# flush the buffer
 	foreach my $file (keys %{$buffer}){
-	    open( FH, ">>$file" ) or die;
+	    open( FH, ">>$file" ) or die "Could not write to file $file";
 	    print FH $buffer->{ $file };
 	    close FH;
 	}
@@ -178,13 +192,90 @@ sub print_buffered {
     } else {
 	$buffer->{ $filename } .= $text;
 	if( length( $buffer->{ $filename } ) > 10_000 ) {
-	    open( FH, ">>$filename" ) or die;
+	    open( FH, ">>$filename" ) or die "Could not write to file $filename";
 	    print FH $buffer->{ $filename };
 	    close FH;
 	    $buffer->{ $filename } = '';
 	}
     }
 }
+
+
+sub get_LD_populations{
+    my $dbVariation = shift;
+    my $siblings = shift;
+    my ($pop_id,$population_name);
+    my $sth = $dbVariation->dbc->prepare(qq{
+		SELECT s.sample_id, s.name
+		FROM population p, sample s
+		WHERE (s.name like 'PERLEGEN:AFD%'
+		OR s.name like 'CSHL-HAPMAP%'
+		OR s.name like '1000GENOMES%'
+		OR s.display = 'LD')
+		AND s.sample_id = p.sample_id
+	});
+
+    $sth->execute();
+    $sth->bind_columns(\$pop_id,\$population_name);
+    #get all the children that we do not want in the genotypes
+    my @pops;
+    while($sth->fetch){
+	#if($population_name =~ /CEU|YRI|MEX/){
+	    get_siblings($dbVariation,$pop_id,$siblings);
+	#}
+	push @pops, $pop_id;
+    }
+    
+    my $in_str = " IN (" . join(',', @pops). ")";
+    
+    return $in_str if (defined $pops[0]);
+    return '' if (!defined $pops[0]);
+
+}
+
+#for a given population, gets all individuals that are children (have father or mother)
+sub get_siblings{
+    my $dbVariation = shift;
+    my $population_id = shift;
+    my $siblings = shift;
+
+    my $sth_individual = $dbVariation->dbc->prepare(qq{
+		SELECT i.sample_id
+		FROM individual i, individual_population ip
+		WHERE ip.individual_sample_id = i.sample_id
+		AND ip.population_sample_id = ? 
+		AND i.father_individual_sample_id IS NOT NULL
+		AND i.mother_individual_sample_id IS NOT NULL
+	});
+    my ($individual_id);
+    $sth_individual->execute($population_id);
+    $sth_individual->bind_columns(\$individual_id);
+    while ($sth_individual->fetch){
+		# necessary to have in the key the population,
+		# since some individuals are shared between populations
+		$siblings->{$population_id.'-'.$individual_id}++;
+    }
+}
+
+
+sub create_LD_table{
+    my $dbVariation = shift;
+    
+    #need to create ld table if it has not been create yet
+    $dbVariation->dbc->do(qq{
+		CREATE TABLE IF NOT EXISTS pairwise_ld(
+			sample_id int not null,
+			seq_region_id int not null,
+			seq_region_start int not null,
+			seq_region_end int not null,
+			r2 float not null,
+			
+			key seq_region_idx(seq_region_id,seq_region_start)
+		)
+	});
+
+}
+
 
 sub import_tagg_snps{
     my $dbVariation = shift;
@@ -196,22 +287,40 @@ sub import_tagg_snps{
     unlink(<$TMP_DIR/snps_tagged_*>);
     #and finally, load the information
     load($dbVariation->dbc(), qw(tagged_variation_feature variation_feature_id sample_id));
-    #unlink(<$TMP_DIR/tag_snps_*>);
+    unlink(<$TMP_DIR/tag_snps_*>);
    
 }
-sub usage {
-  my $msg = shift;
 
-  print STDERR <<EOF;
+# gets time
+sub get_time() {
+	my @time = localtime(time());
 
-usage: perl tag_snps.pl <options>
+	# increment the month (Jan = 0)
+	$time[4]++;
 
-options:
-    -species <name>      species name (default:human)
-    -tmpdir <dir>        temp directory to use (with lots of space!)
-    -tmpfile <filename>  name of temp file to use
+	# add leading zeroes as required
+	for my $i(0..4) {
+		$time[$i] = "0".$time[$i] if $time[$i] < 10;
+	}
 
-EOF
+	# put the components together in a string
+	my $time =
+ 		($time[5] + 1900)."-".
+ 		$time[4]."-".
+ 		$time[3]." ".
+		$time[2].":".
+		$time[1].":".
+		$time[0];
 
-  die("\n$msg\n\n");
+	return $time;
+}
+
+
+
+# prints debug output with time
+sub debug {
+	my $text = (@_ ? (join "", @_) : "No message");
+	my $time = get_time;
+	
+	print $time." - ".$text.($text =~ /\n$/ ? "" : "\n");
 }
