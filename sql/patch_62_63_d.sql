fddcb7d7d282b56613728b81665cef66bd233e79
Laurent Gil <lgil@ebi.ac.uk>
2011-05-26 12:44:32 +0000

    Update patch

--- sql/patch_62_63_d.sql
+++ sql/patch_62_63_d.sql
@@ -1,14 +1,13 @@
-# alter some structural_variation columns
+# alter some structural_variation columns and a supporting_structural_variation index
 
 ALTER TABLE structural_variation CHANGE bound_start inner_start int(11) DEFAULT NULL;
 ALTER TABLE structural_variation CHANGE bound_end inner_end int(11) DEFAULT NULL;
 ALTER TABLE structural_variation ADD COLUMN class_attrib_id int(10) unsigned not null default 0;
 ALTER TABLE structural_variation DROP COLUMN class;
-ALTER TABLE structural_variation ADD COLUMN validation_status ENUM('validated','not validated');
-
-
+ALTER TABLE structural_variation ADD COLUMN validation_status ENUM('validated','not validated','high quality');
 ALTER TABLE structural_variation ADD INDEX attrib_idx (class_attrib_id);
 
+ALTER TABLE supporting_structural_variation DROP INDEX name_idx;
 
 # patch identifier
-INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL,'patch', 'patch_62_63_d.sql|alter some structural_variation columns');
+INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL,'patch', 'patch_62_63_d.sql|alter some structural_variation columns and a supporting_structural_variation index');
