2242944bcb06be85c447b59669a8292c1f1b597e
Graham Ritchie <gr5@sanger.ac.uk>
2011-10-10 14:10:30 +0000

    the adaptor can now create a fasta dump of all transcripts passed to it

--- modules/Bio/EnsEMBL/Variation/Pipeline/TranscriptFileAdaptor.pm
+++ modules/Bio/EnsEMBL/Variation/Pipeline/TranscriptFileAdaptor.pm
@@ -6,30 +6,50 @@ use Digest::MD5 qw(md5_hex);
 
 sub new {
     my $class = shift;
-    my $args  = {@_};
-    return bless $args,  $class;
-}
 
-sub get_protein_seq {
-    my ($self, $transcript_stable_id) = @_;
+    my %args = @_;
+
+    my $self = bless {}, $class;
+
+    if ($args{fasta_file}) {
+        $self->{fasta_file} = $args{fasta_file};
+    }
     
-    my $fasta = $self->{fasta_file};
+    if ($args{transcripts}) {
+        $self->_dump_translations($args{transcripts});
+    }
     
-    my $seq = `samtools faidx $fasta $transcript_stable_id`;
-
-    $seq =~ s/>.*\n//m;
-    $seq =~ s/\s//mg;
-
-    return $seq;
+    return $self;
 }
 
-sub get_protein_md5 {
-    my ($self, $transcript_stable_id) = @_;
+sub get_translation_seq {
+    my ($self, $translation_stable_id) = @_;
 
-    return md5_hex($self->get_protein_seq($transcript_stable_id));
+    my $fasta = $self->get_translation_fasta($translation_stable_id);
+
+    $fasta =~ s/>.*\n//m;
+    $fasta =~ s/\s//mg;
+
+    return $fasta;
 }
 
-sub get_all_transcript_stable_ids {
+sub get_translation_fasta {
+    my ($self, $translation_stable_id) = @_;
+    
+    my $file = $self->{fasta_file};
+    
+    my $fasta = `samtools faidx $file $translation_stable_id`;
+
+    return $fasta;
+}
+
+sub get_translation_md5 {
+    my ($self, $translation_stable_id) = @_;
+
+    return md5_hex($self->get_translation_seq($translation_stable_id));
+}
+
+sub get_all_translation_stable_ids {
     my $self = shift;
 
     my $fasta = $self->{fasta_file};
@@ -38,5 +58,43 @@ sub get_all_transcript_stable_ids {
 
     return \@ids;
 }
+
+sub _dump_translations {
+
+    my ($self, $transcripts) = @_;
+
+    # dump the translations out to the FASTA file
+
+    my $fasta = $self->{fasta_file};
+
+    open FASTA, ">$fasta" or die "Failed to open $fasta for writing";
+
+    # get rid of any existing index file
+
+    if (-e "$fasta.fai") {
+        unlink "$fasta.fai" or die "Failed to delete fasta index file";
+    }
+
+    for my $transcript (@$transcripts) {
+
+        my $tl = $transcript->translation;
+    
+        next unless $tl;
+
+        my $protein = $tl->seq;
+
+        $protein =~ s/(.{80})/$1\n/g;
+
+        # get rid of any trailing newline
+        chomp $protein;
+
+        my $hdr = $tl->stable_id;
+
+        print FASTA ">$hdr\n$protein\n";
+    }
+
+    close FASTA;
+}
+ 
 
 1;
