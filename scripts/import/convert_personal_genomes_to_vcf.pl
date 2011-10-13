da0e272c574f66ed2c671297873e35315e82bd10
William McLaren <wm2@ebi.ac.uk>
2011-10-13 15:14:51 +0000

    Converts personal genomes files to VCF

---
+++ scripts/import/convert_personal_genomes_to_vcf.pl
@@ -0,0 +1,100 @@
+#!/usr/bin/perl
+
+use strict;
+use Bio::EnsEMBL::Registry;
+use Getopt::Long;
+use FileHandle;
+
+# COMMAND LINE OPTIONS
+######################
+
+my $config = {};
+
+GetOptions(
+	$config,
+	'help',      # displays help message
+	'input=s',   # input file
+	'sample=s',  # sample name
+);
+
+# print usage message if requested or no args supplied
+if(defined($config->{help})) {
+	&usage;
+	exit(0);
+}
+
+# defaults
+$config->{sample} ||= 'DEFAULT_SAMPLE_NAME';
+
+my $in_file_handle = new FileHandle;
+
+if(defined($config->{input})) {
+	
+	# check defined input file exists
+	die("ERROR: Could not find input file ", $config->{input}, "\n") unless -e $config->{input};
+	
+	# open file
+	if($config->{input} =~ /\.gz$/){
+		$in_file_handle->open("zcat ". $config->{input} . " | " ) or die("ERROR: Could not read from input file ", $config->{input}, "\n");
+	}
+	else {
+		$in_file_handle->open( $config->{input} ) or die("ERROR: Could not read from input file ", $config->{input}, "\n");
+	}
+}
+else {
+	$in_file_handle = 'STDIN';
+}
+
+# header
+print join "\t", (
+	'#CHROM',
+	'POS',
+	'ID',
+	'REF',
+	'ALT',
+	'QUAL',
+	'FILTER',
+	'INFO',
+	'FORMAT',
+	$config->{sample}."\n"
+);
+
+while(<$in_file_handle>) {
+	chomp;
+	
+	my @data = split /\s+/, $_;
+		
+	# some files have an extra col at the beginning
+	shift @data if $data[0] =~ /^\d+$/ && $data[1] =~ /chr/;
+	
+	# trim off characters from chrom name
+	$data[0] =~ s/chr(om)?//ig;
+	
+	# alleles
+	my @alleles = split /\//, $data[3];
+	unshift @alleles, 'N' if scalar @alleles == 1;
+	
+	# get genotype
+	my @gt = split /\//, $data[3];
+	push @gt, $gt[0] if scalar @gt == 1;
+	
+	# make codes
+	my %codes = map {$alleles[$_] => $_} 0..$#alleles;
+	
+	# make coded genotype
+	my $code = join '/', map {$codes{$_}} @gt;
+	
+	# print data
+	print join "\t", (
+		$data[0],
+		$data[1] + 1,
+		'.',
+		$alleles[0],
+		$alleles[1],
+		'.',
+		'.',
+		'.',
+		'GT',
+		$code."\n"
+	);
+}
