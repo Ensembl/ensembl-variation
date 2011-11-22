b4ce815c1c9d65a9cfa30a5fc7535211c009498a
William McLaren <wm2@ebi.ac.uk>
2011-11-22 16:40:22 +0000

    Base class as a parent for variationfeature and structuralvariationfeature

---
+++ modules/Bio/EnsEMBL/Variation/BaseVariationFeature.pm
@@ -0,0 +1,53 @@
+=head1 LICENSE
+
+ Copyright (c) 1999-2011 The European Bioinformatics Institute and
+ Genome Research Limited.  All rights reserved.
+
+ This software is distributed under a modified Apache license.
+ For license details, please see
+
+   http://www.ensembl.org/info/about/code_licence.html
+
+=head1 CONTACT
+
+ Please email comments or questions to the public Ensembl
+ developers list at <dev@ensembl.org>.
+
+ Questions may also be sent to the Ensembl help desk at
+ <helpdesk@ensembl.org>.
+
+=cut
+
+# Ensembl module for Bio::EnsEMBL::Variation::VariationFeature
+#
+# Copyright (c) 2004 Ensembl
+#
+
+
+=head1 NAME
+
+Bio::EnsEMBL::Variation::BaseVariationFeature - Abstract base class for variation features
+
+=head1 SYNOPSIS
+
+None
+
+=head1 DESCRIPTION
+
+Abstract base class representing variation features. Should not be instantiated
+directly.
+
+=head1 METHODS
+
+=cut
+
+use strict;
+use warnings;
+
+package Bio::EnsEMBL::Variation::BaseVariationFeature;
+
+use Bio::EnsEMBL::Feature;
+
+our @ISA = ('Bio::EnsEMBL::Feature');
+
+1;
