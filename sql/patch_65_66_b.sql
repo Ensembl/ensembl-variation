27106e5db65fefda079c57f30729b09adb184ef2
Laurent Gil <lgil@ebi.ac.uk>
2011-12-13 16:01:51 +0000

    remove unused tables

---
+++ sql/patch_65_66_b.sql
@@ -0,0 +1,11 @@
+# remove unused tables
+DROP TABLE variation_group;
+DROP TABLE variation_group_variation;
+DROP TABLE variation_group_feature;
+DROP TABLE allele_group;
+DROP TABLE allele_group_allele;
+
+
+# patch identifier
+INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL,'patch', 'patch_65_66_b.sql|remove unused tables');
+
