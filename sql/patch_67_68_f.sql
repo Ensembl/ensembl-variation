d67391bdcc6e2ebdfce6c3dc42e5e0779602be9f
Sarah Hunt <seh@ebi.ac.uk>
2012-06-06 08:49:07 +0000

    Fixed patch id

--- sql/patch_67_68_f.sql
+++ sql/patch_67_68_f.sql
@@ -5,4 +5,4 @@
 ALTER TABLE allele ADD COLUMN  frequency_submitter_handle varchar(20);
 
 # patch identifier
-INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL,'patch', 'patch_67_68_c.sql|update to store frequency submitter handle in allele table');
+INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL,'patch', 'patch_67_68_f.sql|update to store frequency submitter handle in allele table');
