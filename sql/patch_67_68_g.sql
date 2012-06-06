c4e6f5c37585e02940b82eb0deed4c7d4c0e2516
Sarah Hunt <seh@ebi.ac.uk>
2012-06-06 10:28:53 +0000

    expansion of text in description

--- sql/patch_67_68_g.sql
+++ sql/patch_67_68_g.sql
@@ -2,7 +2,7 @@
 ##################
 
 
-update  failed_description  set description = 'Additional allele data from dbSNP does not match the refSNP alleles' where failed_description_id = 11 ;
+update  failed_description  set description = 'Additional submitted allele data from dbSNP does not agree with the dbSNP refSNP alleles' where failed_description_id = 11 ;
 
 # patch identifier
 INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL,'patch', 'patch_67_68_g.sql|update to description text in the failed_description table');
