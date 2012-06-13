2f1092c77bd7067df343c09f5791ee1b734564e1
Laurent Gil <lgil@ebi.ac.uk>
2012-06-13 11:01:03 +0000

    Updated the code using the method 'reverse_flanking_sequence' in the script parallel_post_process.pl

--- scripts/import/reverse_flanking.pl
+++ scripts/import/reverse_flanking.pl
@@ -1,84 +1,100 @@
-#! /usr/local/bin/perl
-
-#
-
 use strict;
-#use DBH;
-use Bio::EnsEMBL::Registry;
-use Bio::EnsEMBL::DBSQL::DBAdaptor;
-use Bio::EnsEMBL::Variation::DBSQL::DBAdaptor;
-use Bio::EnsEMBL::Utils::Exception qw(verbose throw warning);
-use Bio::EnsEMBL::Utils::Argument qw( rearrange );
-use Bio::EnsEMBL::Utils::Sequence qw(reverse_comp);
-use Data::Dumper;
-use Bio::SeqIO;
-use FindBin qw( $Bin );
+use warnings;
+
+use POSIX;
 use Getopt::Long;
-use ImportUtils qw(dumpSQL debug create_and_load load);
+use ImportUtils qw(dumpSQL load create_and_load debug);
+use Bio::EnsEMBL::Utils::Exception qw(warning throw verbose);
+use Bio::EnsEMBL::Utils::Sequence qw(reverse_comp);
+use DBI qw(:sql_types);
+use Bio::EnsEMBL::DBSQL::DBAdaptor;
+use FindBin qw( $Bin );
+use Data::Dumper;
 
-our ($species,$input_dir, $output_dir,$TMP_DIR,$TMP_FILE, $registry_file);
+my ($TMP_DIR, $TMP_FILE);
 
-GetOptions('species=s'          => \$species,
-	   'tmpdir=s'           => \$ImportUtils::TMP_DIR,
-	   'tmpfile=s'          => \$ImportUtils::TMP_FILE,
-	  );
-$registry_file ||= $Bin . "/ensembl.registry";
+my ($species,$registry_file);
 
-#usage('-species argument is required') if(!$species);
+GetOptions(
+  'tmpdir=s'  => \$ImportUtils::TMP_DIR,
+  'tmpfile=s' => \$ImportUtils::TMP_FILE,
+	'species=s' => \$species,
+  'registry_file=s' => \$registry_file,
+);
 
-$TMP_DIR  = $ImportUtils::TMP_DIR;
-$TMP_FILE = $ImportUtils::TMP_FILE;
+usage('-species argument required') if(!$species);
+
+warn("Make sure you have an updated ensembl.registry file!\n");
+
+$registry_file ||= $Bin . "./ensembl.registry";
 
 Bio::EnsEMBL::Registry->load_all( $registry_file );
 
-my $cdb = Bio::EnsEMBL::Registry->get_DBAdaptor($species,'core');
-my $vdb = Bio::EnsEMBL::Registry->get_DBAdaptor($species,'variation');
+my $cdba = Bio::EnsEMBL::Registry->get_DBAdaptor($species,'core')
+    || usage( "Cannot find core db for $species in $registry_file" );
+my $vdba = Bio::EnsEMBL::Registry->get_DBAdaptor($species,'variation')
+    || usage( "Cannot find variation db for $species in $registry_file" );
 
-my $dbCore = $cdb->dbc;
-my $dbVar = $vdb->dbc;
 
-my $sth=$dbVar->prepare(qq{select * from flanking_sequence where seq_region_strand=-1});
-$sth->execute();
+my $dbVar = $vdba->dbc->db_handle;
+my $dbCore = $cdba;
+
+
+my $table = "flanking_sequence";
+
+debug("Processing flanking_sequence reverse strand");
+
+
+$dbVar->do(qq{CREATE TABLE flanking_sequence_before_re LIKE $table});
+$dbVar->do(qq{INSERT INTO flanking_sequence_before_re SELECT * FROM $table});
+
 my ($variation_id,$up_seq,$down_seq,$up_seq_start,$up_seq_end,$down_seq_start,$down_seq_end,$seq_region_id,$seq_region_strand);
 
-open OUT, ">$TMP_DIR/$TMP_FILE" or die "can't open reversed_flank:$!";
+my $sth=$dbVar->prepare(qq{
+	 SELECT fl.* FROM $table fl, variation_to_reverse vtr
+	 WHERE fl.variation_id = vtr.variation_id
+});
+$sth->execute();
 $sth->bind_columns(\$variation_id,\$up_seq,\$down_seq,\$up_seq_start,\$up_seq_end,\$down_seq_start,\$down_seq_end,\$seq_region_id,\$seq_region_strand);
+
 while($sth->fetch()) {
-  #print "$variation_id,$up_seq,$down_seq,$up_seq_start,$up_seq_end,$down_seq_start,$down_seq_end,$seq_region_id,$seq_region_strand\n";
-  if ($up_seq and $down_seq) {
-    ($up_seq, $down_seq) = ($down_seq, $up_seq);
-    reverse_comp(\$up_seq);
-    reverse_comp(\$down_seq);
-    $up_seq_start = $up_seq_end = $down_seq_start = $down_seq_end = '\N';
-  }
-  elsif (! $up_seq and ! $down_seq) {
-    my $tmp_seq_start = $up_seq_start;
-    my $tmp_seq_end = $up_seq_end;
-    ($up_seq_start, $up_seq_end) = ($down_seq_start, $down_seq_end);
-    ($down_seq_start, $down_seq_end) = ($tmp_seq_start, $tmp_seq_end);
-    $up_seq = $down_seq = '\N';
-  }
-  elsif ($up_seq and ! $down_seq) {
-    $down_seq = $up_seq;
-    reverse_comp(\$down_seq);
-    $up_seq = '\N';
-    ($up_seq_start, $up_seq_end) = ($down_seq_start, $down_seq_end);
-    $down_seq_start = '\N';
-    $down_seq_end = '\N';
-  }
-  elsif (! $up_seq and $down_seq) {
-    $up_seq = $down_seq;
-    reverse_comp(\$up_seq);
-    $down_seq = '\N';
-    ($down_seq_start, $down_seq_end) = ($up_seq_start, $up_seq_end);
-    $up_seq_start = '\N';
-    $up_seq_end = '\N';
-  }
-  $seq_region_strand = 1 if ($seq_region_strand = -1);
-  print OUT "$variation_id\t$up_seq\t$down_seq\t$up_seq_start\t$up_seq_end\t$down_seq_start\t$down_seq_end\t$seq_region_id\t$seq_region_strand\n";
+	#print "$variation_id,$up_seq,$down_seq,$up_seq_start,$up_seq_end,$down_seq_start,$down_seq_end,$seq_region_id,$seq_region_strand\n";
+	
+	if ($up_seq and $down_seq) {
+		($up_seq, $down_seq) = ($down_seq, $up_seq);
+		reverse_comp(\$up_seq);
+		reverse_comp(\$down_seq);
+		$up_seq_start = $up_seq_end = $down_seq_start = $down_seq_end = '\N';
+	}
+	elsif (! $up_seq and ! $down_seq) {
+		my $tmp_seq_start = $up_seq_start;
+		my $tmp_seq_end = $up_seq_end;
+		($up_seq_start, $up_seq_end) = ($down_seq_start, $down_seq_end);
+		($down_seq_start, $down_seq_end) = ($tmp_seq_start, $tmp_seq_end);
+		$up_seq = $down_seq = '\N';
+	}
+	elsif ($up_seq and ! $down_seq) {
+		$down_seq = $up_seq;
+		reverse_comp(\$down_seq);
+		$up_seq = '\N';
+		($up_seq_start, $up_seq_end) = ($down_seq_start, $down_seq_end);
+		$down_seq_start = '\N';
+		$down_seq_end = '\N';
+	}
+	elsif (! $up_seq and $down_seq) {
+		$up_seq = $down_seq;
+		reverse_comp(\$up_seq);
+		$down_seq = '\N';
+		($down_seq_start, $down_seq_end) = ($up_seq_start, $up_seq_end);
+		$up_seq_start = '\N';
+		$up_seq_end = '\N';
+	}
+	$seq_region_strand = 1;
+	  
+	$dbVar->do(qq{UPDATE $table 
+	                 SET up_seq = "$up_seq", down_seq = "$down_seq", up_seq_region_start = $up_seq_start, up_seq_region_end = $up_seq_end, down_seq_region_start=$down_seq_start, down_seq_region_end=$down_seq_end, seq_region_strand=$seq_region_strand 
+							   WHERE variation_id = $variation_id and seq_region_id = $seq_region_id and seq_region_strand = -1});
 }
-
-$sth->finish;
-
-$dbVar->do(qq{CREATE TABLE reversed_flank like flanking_sequence});
-load($dbVar,"reversed_flank","variation_id","up_seq","down_seq","up_seq_region_start","up_seq_region_end","down_seq_region_start","down_seq_region_end","seq_region_id","seq_region_strand");
+	
+$dbVar->do(qq{UPDATE $table SET up_seq = null   WHERE up_seq = 'N'});
+$dbVar->do(qq{UPDATE $table SET down_seq = null WHERE down_seq = 'N'});
