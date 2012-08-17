c2846b8d8518ad9787b5b304f4fd38047c229b43
Sarah Hunt <seh@ebi.ac.uk>
2012-08-17 12:41:35 +0000

    Limit source.name column to be compatible with BioMart

---
+++ sql/patch_68_69_e.sql
@@ -0,0 +1,8 @@
+Limit source.name column to be a maximum of 24 characters to be compatible with BioMart
+##################
+
+# patch identifier
+INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL,'patch', 'patch_68_69_e.sql|length change for source.name ');
+
+
+ALTER TABLE source MODIFY COLUMN name  varchar(24) NOT NULL;
