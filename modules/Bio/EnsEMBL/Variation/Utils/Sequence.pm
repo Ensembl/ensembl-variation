353b80be35ecda669115872d862596991a524f5b
Sarah Hunt <seh@ebi.ac.uk>
2012-10-29 11:49:13 +0000

    handle tandems

--- modules/Bio/EnsEMBL/Variation/Utils/Sequence.pm
+++ modules/Bio/EnsEMBL/Variation/Utils/Sequence.pm
@@ -74,6 +74,7 @@ use vars qw(@ISA @EXPORT_OK);
     &get_all_validation_states
     &get_validation_code
     &add_validation_state
+    &revcomp_tandem
 );
 
 # List of validation states. Order must match that of set in database
@@ -881,5 +882,52 @@ sub get_all_validation_states {
 sub get_validation_code {
     return array_to_bitval(shift,\@VALIDATION_STATES);
 }
+
+
+
+=head2 revcomp_tandem
+
+  Arg [1]    : string eg. "(AC)17/(AC)19"
+  Example    : $rc_string = revcomp_tandem("(AC)17/(AC)19");
+  Description: reverse compliments tandem repeat descriptions 
+  Returntype : string eg. "(GT)17/(GT)19"
+  Exceptions : none
+  Caller     : Variation::Pipeline::VariantQC::VariantQC
+  Status     : At Risk
+
+=cut
+
+sub revcomp_tandem{
+
+    
+  my $allele_string = shift;
+
+  my $new_allele_string;
+
+  my @parts = split/\//, $allele_string;
+  
+  foreach my $part (@parts){
+  
+    if( $part =~/\d+$/){
+
+      my ($seq, $num ) = split/\)/,$part;
+      $seq =~ s/\(//g;     
+      reverse_comp(\$seq);
+
+      $new_allele_string .= "(" . $seq .")" . $num . "/";
+    }
+    else{
+      reverse_comp(\$part);
+
+      $new_allele_string .= $part. "/";;
+    }
+  }
+  $new_allele_string =~ s/\/$//;
+  
+  return $new_allele_string ;
+}
+
+
+
 
 1;
