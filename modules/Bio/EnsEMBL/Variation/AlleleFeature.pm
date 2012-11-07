085d7a1c4c9b3fb313b8f2ad00d10261a5f28fba
William McLaren <wm2@ebi.ac.uk>
2012-11-07 09:48:02 +0000

    fix

--- modules/Bio/EnsEMBL/Variation/AlleleFeature.pm
+++ modules/Bio/EnsEMBL/Variation/AlleleFeature.pm
@@ -187,7 +187,7 @@ sub new_fast {
 
 sub allele_string{
   my $self = shift;
-  return $self->{'allele_string'} = shift if(@_);
+  return $self->{'allele_string'} = shift if $_[0];
   
   return $self->{'allele_string'} if ($self->{'_half_genotype'}); #for half genotypes
   return join('|',split (//,unambiguity_code($self->{'allele_string'}))); #for heterozygous alleles
