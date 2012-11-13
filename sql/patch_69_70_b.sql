49bc4e7bff577bb146f56121a7e715306d5ef21d
Anja Thormann <anja@ebi.ac.uk>
2012-11-13 17:28:21 +0000

    drop table flanking_sequence

---
+++ sql/patch_69_70_b.sql
@@ -0,0 +1,5 @@
+# drop table flanking_sequence
+DROP TABLE flanking_sequence;
+
+# patch identifier
+INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_69_70_b.sql|drop table flanking_sequence');
