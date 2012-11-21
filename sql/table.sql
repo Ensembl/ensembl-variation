4e3a00258baa64eb7f6963f1669e57bb94168659
Anja Thormann <anja@ebi.ac.uk>
2012-11-21 13:33:27 +0000

    added regulation variation tables

--- sql/table.sql
+++ sql/table.sql
@@ -903,6 +903,159 @@ CREATE TABLE transcript_variation (
     KEY somatic_feature_idx             (feature_stable_id, somatic)
 
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+/**
+@table motif_feature_variation
+
+@desc This table relates a single allele of a variation_feature to a motif feature (see Regulation documentation). It contains the consequence of the allele.
+
+@column motif_feature_variation_id  Primary key, internal identifier.
+@column variation_feature_id        Foreign key references to the @link variation_feature table.
+@column feature_stable_id		    Foreign key to regulation databases. Unique stable id of related regulatory_feature.
+@column motif_feature_id            Foreign key to regulation databases. Internal id of related motif_feature.
+@column allele_string               Shows the reference sequence and variant sequence of this allele.
+@column somatic                     Flags if the associated variation is known to be somatic.
+@column consequence_types		    The consequence(s) of the variant allele on this motif_feature.
+@column motif_name                  The display label of the motif.
+@column motif_start                 The start position of the variation in the motif.
+@column motif_end                   The end position of the variation in the motif.
+@column motif_score_delta           The deviation from the score (that is derived from alignment software (e.g. MOODS)) caused by the variation.
+@column in_informative_position     Flags if the variation is in an informative position.
+
+@see variation_feature
+*/
+
+CREATE TABLE IF NOT EXISTS motif_feature_variation (
+    motif_feature_variation_id          int(11) unsigned NOT NULL AUTO_INCREMENT,
+    variation_feature_id                int(11) unsigned NOT NULL,
+    feature_stable_id                   varchar(128) DEFAULT NULL,
+    motif_feature_id                    int(11) unsigned NOT NULL,
+    allele_string                       text,
+    somatic                             tinyint(1) NOT NULL DEFAULT 0,
+    consequence_types                   set (
+                                            'splice_acceptor_variant',
+                                            'splice_donor_variant',
+                                            'stop_lost',
+                                            'coding_sequence_variant',
+                                            'missense_variant',
+                                            'stop_gained',
+                                            'synonymous_variant',
+                                            'frameshift_variant',
+                                            'nc_transcript_variant',
+                                            'non_coding_exon_variant',
+                                            'mature_miRNA_variant',
+                                            'NMD_transcript_variant',
+                                            '5_prime_UTR_variant',
+                                            '3_prime_UTR_variant',
+                                            'incomplete_terminal_codon_variant',
+                                            'intron_variant',
+                                            'splice_region_variant',
+                                            'downstream_gene_variant',
+                                            'upstream_gene_variant',
+                                            'initiator_codon_variant',
+                                            'stop_retained_variant',
+                                            'inframe_insertion',
+                                            'inframe_deletion',
+                                            'transcript_ablation',
+                                            'transcript_fusion',
+                                            'transcript_amplification',
+                                            'transcript_translocation',
+                                            'TF_binding_site_variant',
+                                            'TFBS_ablation',
+                                            'TFBS_fusion',
+                                            'TFBS_amplification',
+                                            'TFBS_translocation',
+                                            'regulatory_region_variant',
+                                            'regulatory_region_ablation',
+                                            'regulatory_region_fusion',
+                                            'regulatory_region_amplification',
+                                            'regulatory_region_translocation',
+                                            'feature_elongation',
+                                            'feature_truncation'
+                                        ),
+    motif_name                          text,
+    motif_start                         int(11) unsigned,
+    motif_end                           int(11) unsigned,
+    motif_score_delta                   float DEFAULT NULL,
+    in_informative_position             tinyint(1) NOT NULL DEFAULT 0,
+
+    PRIMARY KEY                         (motif_feature_variation_id),
+    KEY variation_feature_idx           (variation_feature_id),
+    KEY feature_idx                     (feature_stable_id),
+    KEY consequence_type_idx            (consequence_types),
+    KEY somatic_feature_idx             (feature_stable_id, somatic)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+/**
+@table regulatory_feature_variation
+
+@desc This table relates a single allele of a variation_feature to a regulatory feature (see Regulation documentation). It contains the consequence of the allele.
+@column regulatory_feature_variation_id  Primary key, internal identifier.
+@column variation_feature_id             Foreign key references to the @link variation_feature table.
+@column feature_stable_id		         Foreign key to regulation databases. Unique stable id of related regulatory_feature.
+@column feature_type                     The name of the feature type.
+@column allele_string                    Shows the reference sequence and variant sequence of this allele.
+@column somatic                          Flags if the associated variation is known to be somatic.
+@column consequence_types		         The consequence(s) of the variant allele on this regulatory feature.
+
+@see variation_feature
+*/
+
+CREATE TABLE IF NOT EXISTS regulatory_feature_variation (
+    regulatory_feature_variation_id     int(11) unsigned NOT NULL AUTO_INCREMENT,
+    variation_feature_id                int(11) unsigned NOT NULL,
+    feature_stable_id                   varchar(128) DEFAULT NULL,
+    feature_type                        text, 
+    allele_string                       text,
+    somatic                             tinyint(1) NOT NULL DEFAULT 0,
+    consequence_types                   set (
+                                            'splice_acceptor_variant',
+                                            'splice_donor_variant',
+                                            'stop_lost',
+                                            'coding_sequence_variant',
+                                            'missense_variant',
+                                            'stop_gained',
+                                            'synonymous_variant',
+                                            'frameshift_variant',
+                                            'nc_transcript_variant',
+                                            'non_coding_exon_variant',
+                                            'mature_miRNA_variant',
+                                            'NMD_transcript_variant',
+                                            '5_prime_UTR_variant',
+                                            '3_prime_UTR_variant',
+                                            'incomplete_terminal_codon_variant',
+                                            'intron_variant',
+                                            'splice_region_variant',
+                                            'downstream_gene_variant',
+                                            'upstream_gene_variant',
+                                            'initiator_codon_variant',
+                                            'stop_retained_variant',
+                                            'inframe_insertion',
+                                            'inframe_deletion',
+                                            'transcript_ablation',
+                                            'transcript_fusion',
+                                            'transcript_amplification',
+                                            'transcript_translocation',
+                                            'TF_binding_site_variant',
+                                            'TFBS_ablation',
+                                            'TFBS_fusion',
+                                            'TFBS_amplification',
+                                            'TFBS_translocation',
+                                            'regulatory_region_variant',
+                                            'regulatory_region_ablation',
+                                            'regulatory_region_fusion',
+                                            'regulatory_region_amplification',
+                                            'regulatory_region_translocation',
+                                            'feature_elongation',
+                                            'feature_truncation'
+                                        ),
+
+    PRIMARY KEY                         (regulatory_feature_variation_id),
+    KEY variation_feature_idx           (variation_feature_id),
+    KEY feature_idx                     (feature_stable_id),
+    KEY consequence_type_idx            (consequence_types),
+    KEY somatic_feature_idx             (feature_stable_id, somatic)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 	
 /**
 @table seq_region
