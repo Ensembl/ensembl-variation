6a87537d50acd5390bcb593f441a4aefb30b9910
Anja Thormann <anja@ebi.ac.uk>
2012-11-21 13:37:22 +0000

    add: if exists

--- sql/patch_69_70_b.sql
+++ sql/patch_69_70_b.sql
@@ -1,5 +1,5 @@
 # drop table flanking_sequence
-DROP TABLE flanking_sequence;
+DROP TABLE IF EXISTS flanking_sequence;
 
 # patch identifier
 INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_69_70_b.sql|drop table flanking_sequence');
