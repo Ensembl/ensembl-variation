f5ec0d3ce6fd556e0a798d8419718f19b7bad94f
Sarah Hunt <seh@ebi.ac.uk>
2012-11-30 17:13:23 +0000

    Don't check MERGE genotype table

--- modules/Bio/EnsEMBL/Variation/Pipeline/VariantQC/CheckdbSNPImport.pm
+++ modules/Bio/EnsEMBL/Variation/Pipeline/VariantQC/CheckdbSNPImport.pm
@@ -17,6 +17,13 @@
  Questions may also be sent to the Ensembl help desk at
  <helpdesk@ensembl.org>.
 
+=head1 DESCRIPTION
+
+This module is used in the ehive variant quality control process. 
+It runs checks on a new dbSNP import to identify obvious missing data
+before the full QC process starts.
+If problems are discovered, the job dies killing the hive process
+
 =cut
 
 package Bio::EnsEMBL::Variation::Pipeline::VariantQC::CheckdbSNPImport;
@@ -28,14 +35,23 @@ use base qw(Bio::EnsEMBL::Variation::Pipeline::BaseVariationProcess);
 
 my $DEBUG = 0;
 
-## This runs some basic checks on the raw dbSNP import before full QC
-## If serious errors are present, the job dies killing the hive process
+
 
 sub run {
    
     my $self = shift;
 
     my $dir = $self->required_param('pipeline_dir');
+
+    ## don't check final tmp_individual_genotype_single_bp for human as merge table
+    my @genotype_tables_to_check = ("population_genotype", "individual_genotype_multiple_bp");
+    if($self->required_param('species') =~/homo|human/){
+	push @genotype_tables_to_check, "tmp_individual_genotype_single_bp_SubInd_ch22";
+    }
+    else{
+	push @genotype_tables_to_check, "tmp_individual_genotype_single_bp";
+    }
+
 
     open my $report, ">", "$dir/preQC_report.txt" || die "Failed to open preQC_report.txt : $!\n";
     print $report "\n\tChecking pre-QC tables \n\n";
@@ -53,12 +69,14 @@ sub run {
     my $var_no_ss_allele     = $self->count_no_ss_allele();
     my $var_no_allele_string = $self->count_no_allele_string();
 
-    my $geno_no_sample       = $self->count_sampleless_geno();
-    my $geno_no_subsnp       = $self->count_geno_ss_problem();
-    my $geno_no_allele       = $self->count_alleleless_geno();
+    my $geno_no_sample       = $self->count_sampleless_geno(\@genotype_tables_to_check);
+    my $geno_no_subsnp       = $self->count_geno_ss_problem(\@genotype_tables_to_check);
+    my $geno_no_allele       = $self->count_alleleless_geno(\@genotype_tables_to_check);
 
     my $varfeat_no_pos       = $self->count_bad_varfeat();
     my $varfeat_no_seqreg    = $self->count_seq_region_problem();
+
+    my $complimented_desc    = $self->check_complimented_desc();
     
 
     print $report "Post-import preQC check
@@ -80,6 +98,8 @@ VariationFeature without start/end: $varfeat_no_pos
 VariationFeature without seqregion: $varfeat_no_seqreg
 
 \n";  
+
+    print $report "ERROR: $complimented_desc complimented descriptions found - to be fixed manually\n\n" if $complimented_desc >0;
 
     if($var_no_ss_allele    > 0  || 
        $var_no_allele_string > 0 || 
@@ -168,6 +188,11 @@ sub count_failed_variation{
     defined $no_allele_count->[0]->[0]  ?  return $no_allele_count->[0]->[0] : return 0;
 }
 
+=head2 count_no_allele_string
+
+  Look for variants without allele data
+
+=cut
 sub count_no_allele_string{
 
     my $self = shift;
@@ -184,8 +209,12 @@ sub count_no_allele_string{
  
 }
 
-## checks on variation_feature
 
+=head2 count_bad_varfeat
+
+  Look for variants without coordinates
+
+=cut
 sub count_bad_varfeat{
 
     my $self = shift;
@@ -197,14 +226,19 @@ sub count_bad_varfeat{
 
     defined $varfeat_count->[0]->[0]  ?  return $varfeat_count->[0]->[0] : return 0;
 }
+=head2 count_seq_region_problem
 
+  Look for variants without sequence locations data
+
+=cut
 sub count_seq_region_problem{
 
     my $self = shift;
     my $var_dba   = $self->get_species_adaptor('variation');
 
     my $varfeat_ext_sth = $var_dba->dbc->prepare(qq[ select count(*) from variation_feature 
-                                                     where seq_region_id  not in (select seq_region_id from seq_region)  ]);
+                                                     left outer join  seq_region on ( variation_feature.seq_region_id =  seq_region.seq_region_id)
+                                                     where  seq_region.name is null  ]);
 
     $varfeat_ext_sth->execute()|| die "Failed to extract failed seq_region_id count\n";   
     my $varfeat_count   = $varfeat_ext_sth->fetchall_arrayref();
@@ -215,40 +249,52 @@ sub count_seq_region_problem{
 
 ### Checks on genotype tables
 
+=head2 count_sampleless_geno
 
+  Look for genotypes without sample data
+
+=cut
 sub count_sampleless_geno{
 
-    my $self = shift;
+    my $self   = shift;
+    my $tables = shift;
+
     my $var_dba   = $self->get_species_adaptor('variation');
 
     my $tot = 0;
 
-   foreach my $table ("tmp_individual_genotype_single_bp", "population_genotype", "tmp_individual_genotype_single_bp" ){ 
 
-      my $no_sample_ext_sth  = $var_dba->dbc->prepare(qq[select count(*) from $table where sample_id not in (select sample_id from sample) ]);
+   foreach my $table ( @{$tables} ){ 
 
-      $no_sample_ext_sth->execute() || die "Failed to extract no sample count for $table\n";
-      my $no_sample_count    = $no_sample_ext_sth->fetchall_arrayref();
+       my $no_sample_ext_sth  = $var_dba->dbc->prepare(qq[select count(*) from $table where sample_id not in (select sample_id from sample) ]);
+       $self->warning("At sampleless_geno with table  $table");
+       $no_sample_ext_sth->execute() || die "Failed to extract no sample count for $table\n";
+       my $no_sample_count    = $no_sample_ext_sth->fetchall_arrayref();
 
-      if (defined $no_sample_count->[0]->[0]){
-         $tot += $no_sample_count->[0]->[0];
-      }
-
+       if (defined $no_sample_count->[0]->[0]){
+	   $tot += $no_sample_count->[0]->[0];
+       }
    }
 
    return $tot;
 
 }
 
+=head2 count_alleleless_geno
 
+  Look for genotypes without allele data
+
+=cut
 sub count_alleleless_geno{
 
-    my $self = shift;
+    my $self   = shift;
+    my $tables = shift;
+
     my $var_dba   = $self->get_species_adaptor('variation');
 
     my $total_problem_genotypes = 0;
 
-   foreach my $table ("tmp_individual_genotype_single_bp", "population_genotype", "tmp_individual_genotype_single_bp" ){ 
+   foreach my $table ( @{$tables} ){ 
 
      my $no_allele_ext_sth  = $var_dba->dbc->prepare(qq[select count(*) from $table where allele_1 is null or allele_2 is null ]);
 
@@ -264,10 +310,16 @@ sub count_alleleless_geno{
 
 }
 
+=head2 count_sampleless_geno
 
+  Look for genotypes without subsnp ids
+
+=cut
 sub count_geno_ss_problem{
 
-    my $self = shift;
+    my $self   = shift;
+    my $tables = shift;
+
     my $var_dba   = $self->get_species_adaptor('variation');
 
    ## useful (slow) SQL:
@@ -280,7 +332,7 @@ sub count_geno_ss_problem{
     my $max_subsnp = $max_subsnp_ext_sth->fetchall_arrayref();
     unless(defined  $max_subsnp->[0]->[0]){ die "Failed to find max subsnp_id\n";}
 
-   foreach my $table ("tmp_individual_genotype_single_bp", "population_genotype", "tmp_individual_genotype_single_bp" ){ 
+   foreach my $table ( @{$tables} ){ 
     
     my $geno_ext_sth = $var_dba->dbc->prepare(qq[ select count(*) from $table where subsnp_id >  $max_subsnp->[0]->[0] ]);
 
@@ -293,5 +345,28 @@ sub count_geno_ss_problem{
 }
 
 
+=head2 check_complimented_desc
+
+  Look for described alleles complimented in error
+     eg "(318 BP DELETION)" =>  ")NOIAELEH PV 813("
+  These can be complex, so are flagged for manual fixing
+
+=cut
+sub check_complimented_desc{
+
+    my $self = shift;
+
+    my $var_dba   = $self->get_species_adaptor('variation');
+
+    my $data_ext_sth = $var_dba->dbc->prepare(qq[ select count(*) from allele_string 
+                                                  where allele_string like '%NOIAELEH%'
+                                                  or allele_string like '%NOIAYESNI%']);
+
+    $data_ext_sth->execute()||die "Failed to check for complimented allele strings\n";
+
+    my $count = $data_ext_sth->fetchall_arrayref();
+
+    defined $count->[0]->[0]  ?  return $count->[0]->[0] : return 0;
+}
 
 1;
