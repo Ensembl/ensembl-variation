2bdab14a64c8ea80cb5025db4832358fb3218bf0
Anja Thormann <anja@ebi.ac.uk>
2012-12-10 17:15:13 +0000

    add methods to represent data from new motif_feature_variation table

--- modules/Bio/EnsEMBL/Variation/MotifFeatureVariation.pm
+++ modules/Bio/EnsEMBL/Variation/MotifFeatureVariation.pm
@@ -51,14 +51,70 @@ sub new {
     return $self;
 }
 
-sub motif_feature_stable_id {
-    my $self = shift;
-    return $self->SUPER::feature_stable_id(@_);
-}
-
 sub motif_feature {
     my ($self, $mf) = @_;
     return $self->SUPER::feature($mf, 'MotifFeature');
+}
+
+sub motif_feature_id {
+    my $self = shift;
+    unless ($self->{motif_feature_id}) {
+       $self->{motif_feature_id} = $self->motif_feature->dbID;
+    }
+    return $self->{motif_feature_id};
+}
+
+sub regulatory_feature {
+    my $self = shift;
+    unless ($self->{regulatory_feature}) {
+        my $funcgen_db = $self->{adaptor}->db->get_db_adaptor('funcgen');
+        unless ($funcgen_db) {
+            warn("Ensembl Funcgen database is not available.");
+        }
+        my $rfa = $funcgen_db->get_RegulatoryFeatureAdaptor;
+        if ($self->motif_feature) {
+            my $rf = $rfa->fetch_all_by_attribute_feature($self->motif_feature)->[0];
+            $self->{regulatory_feature} = $rf;
+        }
+    }
+    return $self->{regulatory_feature}; 
+}
+
+sub feature_stable_id {
+    my $self = shift;
+    unless ($self->{regulatory_feature_stable_id}) {
+        $self->{regulatory_feature_stable_id} = $self->regulatory_feature->stable_id;
+    }
+    return $self->{regulatory_feature_stable_id};    
+}
+
+=head2 motif_name
+
+  Arg [1]    : string $newval (optional)
+               The new value to set the motif_name attribute to
+  Example    : $motif_name = $obj->motif_name()
+  Description: Getter/Setter for the motif_name attribute.  This is the
+               name of the motif associated with this feature.
+  Returntype : string
+  Exceptions : none
+  Caller     : general
+  Status     : Stable
+
+=cut
+
+sub motif_name {
+  my $self = shift;
+  unless ($self->{motif_name}) {
+    $self->{motif_name} = $self->motif_feature->display_label;
+  }
+  return $self->{motif_name};
+}
+
+sub _motif_feature_seq {
+    my $self = shift;
+    my $mf = $self->motif_feature;
+    my $mf_seq = $mf->{_variation_effect_feature_cache}->{seq} ||= $mf->seq;
+    return $mf_seq;
 }
 
 sub add_MotifFeatureVariationAllele {
@@ -79,16 +135,6 @@ sub get_all_alternate_MotifFeatureVariationAlleles {
 sub get_all_MotifFeatureVariationAlleles {
     my $self = shift;
     return $self->SUPER::get_all_VariationFeatureOverlapAlleles(@_);
-}
-
-sub _motif_feature_seq {
-    my $self = shift;
-    
-    my $mf = $self->motif_feature;
-    
-    my $mf_seq = $mf->{_variation_effect_feature_cache}->{seq} ||= $mf->seq;
-    
-    return $mf_seq;
 }
 
 1;
