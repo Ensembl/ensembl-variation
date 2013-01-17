7859ace724329d3fa97e8a9d0b27e76d43958646
Sarah Hunt <seh@ebi.ac.uk>
2013-01-17 17:01:00 +0000

    updated licence URL

--- scripts/import/parse_hapmap_genotye.pl
+++ scripts/import/parse_hapmap_genotye.pl
@@ -1,4 +1,25 @@
 #!/usr/bin/env perl 
+
+=head1 LICENSE
+
+  Copyright (c) 1999-2013 The European Bioinformatics Institute and
+  Genome Research Limited.  All rights reserved.
+
+  This software is distributed under a modified Apache license.
+  For license details, please see
+
+    http://www.ensembl.org/info/about/legal/code_licence.html
+
+=head1 CONTACT
+
+  Please email comments or questions to the public Ensembl
+  developers list at <dev@ensembl.org>.
+
+  Questions may also be sent to the Ensembl help desk at
+  <helpdesk.org>.
+
+=cut
+
 # this script read hapmap genotyping data into individual, 
 #individual_genotype, population, population_genotype tables
 
