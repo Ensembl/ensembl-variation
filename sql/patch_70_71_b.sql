cfee5e0d01d2defafb4d3e12bb7efdbdfb69dd00
Sarah Hunt <seh@ebi.ac.uk>
2013-01-18 09:30:01 +0000

    new evidence column for variation and variation_feature

---
+++ sql/patch_70_71_b.sql
@@ -0,0 +1,8 @@
+# add ensembl evidence status
+
+alter table variation add column evidence SET('Multiple_observations', 'Frequency','HapMap','1000Genomes', 'Cited');
+alter table variation_feature add column evidence SET('Multiple_observations', 'Frequency','HapMap','1000Genomes', 'Cited');
+
+
+# patch identifier
+#INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_70_71_b.sql|add evidence column');
