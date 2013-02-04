1617952647960fcf5fb6106268cadb16aca473c4
Sarah Hunt <seh@ebi.ac.uk>
2013-02-04 14:41:03 +0000

    fix meta

--- sql/patch_70_71_c.sql
+++ sql/patch_70_71_c.sql
@@ -5,4 +5,4 @@ alter table variation modify column minor_allele varchar(10);
 alter table variation_feature modify column minor_allele varchar(10);
 
 # patch identifier
-#INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_70_71_c.sql|increase size of minor_allele');
+INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_70_71_c.sql|increase size of minor_allele');
