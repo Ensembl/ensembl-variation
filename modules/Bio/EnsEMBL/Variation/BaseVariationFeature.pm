8fe982caeb36396fac5f694ba8de454871e46b69
William McLaren <wm2@ebi.ac.uk>
2013-02-19 16:58:22 +0000

    move consequence_type and display_consequence to BaseVariationFeature

--- modules/Bio/EnsEMBL/Variation/BaseVariationFeature.pm
+++ modules/Bio/EnsEMBL/Variation/BaseVariationFeature.pm
@@ -1,12 +1,12 @@
 =head1 LICENSE
 
- Copyright (c) 1999-2012 The European Bioinformatics Institute and
+ Copyright (c) 1999-2013 The European Bioinformatics Institute and
  Genome Research Limited.  All rights reserved.
 
  This software is distributed under a modified Apache license.
  For license details, please see
 
-   http://www.ensembl.org/info/about/code_licence.html
+   http://www.ensembl.org/info/about/legal/code_licence.html
 
 =head1 CONTACT
 
@@ -49,5 +49,83 @@ package Bio::EnsEMBL::Variation::BaseVariationFeature;
 use Bio::EnsEMBL::Feature;
 
 our @ISA = ('Bio::EnsEMBL::Feature');
+
+=head2 consequence_type
+
+  Arg [1]    : (optional) String $term_type
+  Description: Get a list of all the unique consequence terms of this 
+               VariationFeature. By default returns Ensembl display terms
+               (e.g. 'NON_SYNONYMOUS_CODING'). $term_type can also be 'label'
+               (e.g. 'Non-synonymous coding'), 'SO' (Sequence Ontology, e.g.
+               'non_synonymous_codon') or 'NCBI' (e.g. 'missense')
+  Returntype : listref of strings
+  Exceptions : none
+  Status     : Stable
+
+=cut
+
+sub consequence_type {
+    
+    my $self = shift;
+    my $term_type = shift;
+    
+    my $method_name;
+    
+    # delete cached term
+    if(defined($term_type)) {
+        delete $self->{consequence_types};
+        $method_name = $term_type.($term_type eq 'label' ? '' : '_term');
+        $method_name = 'SO_term' unless @{$self->get_all_OverlapConsequences} && $self->get_all_OverlapConsequences->[0]->can($method_name);
+    }
+    
+    $method_name ||= 'SO_term';
+
+    if (exists($self->{current_consequence_method}) && $self->{current_consequence_method} ne $method_name) {
+        delete $self->{consequence_type};
+    }
+
+    unless ($self->{consequence_types}) {
+
+        # work out the terms from the OverlapConsequence objects
+        
+        $self->{consequence_types} = 
+            [ map { $_->$method_name } @{ $self->get_all_OverlapConsequences } ];
+    }
+
+    $self->{current_consequence_method} = $method_name;
+    
+    return $self->{consequence_types};
+}
+
+=head2 display_consequence
+
+  Arg [1]    : (optional) String $term_type
+  Description: Get the term for the most severe consequence of this 
+               VariationFeature. By default returns Ensembl display terms
+               (e.g. 'NON_SYNONYMOUS_CODING'). $term_type can also be 'label'
+               (e.g. 'Non-synonymous coding'), 'SO' (Sequence Ontology, e.g.
+               'non_synonymous_codon') or 'NCBI' (e.g. 'missense')
+  Returntype : string
+  Exceptions : none
+  Status     : Stable
+
+=cut
+
+sub display_consequence {
+    my $self = shift;
+    my $term_type = shift;
+    
+    my $method_name;
+    
+    # delete cached term
+    if(defined($term_type)) {
+        $method_name = $term_type.($term_type eq 'label' ? '' : '_term');
+        $method_name = 'SO_term' unless @{$self->get_all_OverlapConsequences} && $self->get_all_OverlapConsequences->[0]->can($method_name);
+    }
+    
+    $method_name ||= 'SO_term';
+    
+    return $self->most_severe_OverlapConsequence->$method_name;
+}
 
 1;
