fdd9541bd332139fcb7b4949950e146e3718cc60
William McLaren <wm2@ebi.ac.uk>
2013-02-21 13:25:32 +0000

    add evidence_values hash, retrofitted to 70

--- modules/Bio/EnsEMBL/Variation/Utils/Sequence.pm
+++ modules/Bio/EnsEMBL/Variation/Utils/Sequence.pm
@@ -1,12 +1,12 @@
 =head1 LICENSE
 
- Copyright (c) 1999-2012 The European Bioinformatics Institute and
+ Copyright (c) 1999-2013 The European Bioinformatics Institute and
  Genome Research Limited.  All rights reserved.
 
  This software is distributed under a modified Apache license.
  For license details, please see
 
-   http://www.ensembl.org/info/about/code_licence.html
+   http://www.ensembl.org/info/about/legal/code_licence.html
 
 =head1 CONTACT
 
@@ -75,10 +75,13 @@ use vars qw(@ISA @EXPORT_OK);
     &get_validation_code
     &add_validation_state
     &revcomp_tandem
+    %EVIDENCE_VALUES
 );
 
 # List of validation states. Order must match that of set in database
 our @VALIDATION_STATES = qw(cluster freq submitter doublehit hapmap 1000Genome failed precious);
+
+our %EVIDENCE_VALUES  = qw(Multiple_observations 1 Frequency 2 HapMap 2 1000Genomes 2 Cited 3);
 
 =head2 ambiguity_code
 
