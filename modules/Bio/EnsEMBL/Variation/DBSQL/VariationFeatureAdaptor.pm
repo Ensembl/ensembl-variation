0553ec5ad3e6366eabe3e6728991f38340ac540b
William McLaren <wm2@ebi.ac.uk>
2013-02-22 09:17:40 +0000

    fix for store method

--- modules/Bio/EnsEMBL/Variation/DBSQL/VariationFeatureAdaptor.pm
+++ modules/Bio/EnsEMBL/Variation/DBSQL/VariationFeatureAdaptor.pm
@@ -150,7 +150,7 @@ sub store {
         (join ",", @{$vf->get_all_validation_states}) || undef,
         $vf->{slice} ? (join ",", @{$vf->consequence_type('SO')}) : 'intergenic_variant',
         $vf->{variation_set_id} || '',
-        $vf->{class_attrib_id} || $self->db->get_AttributeAdaptor->attrib_id_for_type_value('SO_term', $vf->{class_SO_term}) || 18,
+        $vf->{class_attrib_id} || ( $vf->{class_SO_term} && $self->db->get_AttributeAdaptor->attrib_id_for_type_value('SO_term', $vf->{class_SO_term}) ) || 18,
         $vf->is_somatic,
         $vf->minor_allele,
         $vf->minor_allele_frequency,
