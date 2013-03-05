57506a9f5b77c86543b01a2f95b5089984705049
Sarah Hunt <seh@ebi.ac.uk>
2013-03-05 11:24:31 +0000

    Look up & add Pig Consortium variation synonyms from producton db

--- modules/Bio/EnsEMBL/Variation/Pipeline/VariantQC/SpecialCase.pm
+++ modules/Bio/EnsEMBL/Variation/Pipeline/VariantQC/SpecialCase.pm
@@ -58,9 +58,11 @@ sub run {
 
   if ( $self->param('run_Pubmed_check') ==1 ){ $self->check_Pubmed_variants();}
 
+  if ( $self->required_param('species') =~/sus_scrofa/ ){ $self->add_synonym();}
+
   if ( $self->required_param('species') =~/mus_musculus|gallus_gallus|rattus_norvegicus|canis_familiaris/ ){ $self->set_strain_display();}
 
-  if ( $self->required_param('species') =~/gallus_gallus|rattus_norvegicus|canis_familiaris/ ){ $self->fake_read_coverage();}
+  if ( $self->required_param('species') =~/gallus_gallus|canis_familiaris/ ){ $self->fake_read_coverage();}
 
 }
 
@@ -241,8 +243,75 @@ sub fake_read_coverage{
     }
 }
 
+## Pig consortium variation names are supported - copy to new releases
  
+sub add_synonym{
 
 
+    my $self = shift;
+
+    my $var_dba     = $self->get_species_adaptor('variation');
+    my $var_adaptor = $var_dba->get_VariationAdaptor; 
+    
+    ## using ignore here to allow for re-runs of pipeline; could create new working table
+    my $synonym_ins_sth   = $var_dba->prepare(qq[ insert ignore into variation_synonym (variation_id, name, source_id) values (?,?,?) ]);  
+
+    my %source_data = ( "name"           => "Pig SNP Consortium",
+			"version"        =>  '\N',
+			"description"    => 'PorcineSNP60 BeadChip', 
+			"somatic_status" => "germline"
+	);
+
+    my $source_id = get_source($var_dba ,\%source_data);
+
+    my $all_synonym = $self->get_synonym();
+    
+     
+    foreach my $l(@{$all_synonym}){
+
+	my $var = $var_adaptor->fetch_by_name($l->[0]);
+	if(defined $var){
+
+	    $synonym_ins_sth->execute( $var->dbID, $l->[1], $source_id );
+	}
+	else{
+	    $self->warning( 'No database id for expected pig variant $l->[0] ');
+	}
+    }
+}
+
+
+sub get_source{
+
+    my $var_dba     = shift;
+    my $source_data = shift;
+
+    my $source_ins_sth    = $var_dba->prepare(qq[ insert into source (name, version, description) values (?,?,?) ]);
+    my $source_ext_sth    = $var_dba->prepare(qq[ select source_id from source where name = ? ]);
+
+    $source_ext_sth->execute($source_data->{name})||die;
+    my $id = $source_ext_sth->fetchall_arrayref();
+
+    unless(defined $id ->[0]->[0]){
+
+	$source_ins_sth->execute($source_data->{name}, $source_data->{version}, $source_data->{description} )||die;
+	$id = $source_ext_sth->fetchall_arrayref();
+    }
+    return  $id ->[0]->[0];
+}
+
+sub get_synonym{
+    
+    my $self = shift;
+
+    my $int_dba  = $self->get_adaptor('multi', 'intvar');
+
+    my $synonym_ext_sth = $int_dba->prepare(qq[ select rs_name, synonym_name from pig_synonym ]);
+
+    $synonym_ext_sth->execute();
+    my $all_syn = $synonym_ext_sth->fetchall_arrayref();
+
+    return $all_syn;
+}
 
 1;
