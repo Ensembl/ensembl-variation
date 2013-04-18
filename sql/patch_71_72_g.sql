12c98658bc63cf4f5ce5502f30f996ba9ef73299
Laurent Gil <lgil@ebi.ac.uk>
2013-04-18 14:27:18 +0000

    Copy the study_id from structural_variation to structural_variation_feature

--- sql/patch_71_72_g.sql
+++ sql/patch_71_72_g.sql
@@ -6,8 +6,11 @@ ALTER TABLE structural_variation ADD KEY clinical_attrib_idx (clinical_significa
 
 ALTER TABLE structural_variation_feature ADD COLUMN length INT(10) DEFAULT NULL;
 ALTER TABLE structural_variation_feature ADD COLUMN study_id INT(10) unsigned DEFAULT NULL AFTER source_id; 
+
+UPDATE structural_variation_feature svf, structural_variation sv SET svf.study_id=sv.study_id 
+WHERE svf.structural_variation_id=sv.structural_variation_id and sv.study_id is not NULL;
+
 ALTER TABLE structural_variation_feature ADD KEY study_idx (study_id);
 
-
 # patch identifier
-#INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_71_72_g.sql|Add new columns to the tables structural_variation and structural_variation_feature.');
+INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_71_72_g.sql|Add new columns to the tables structural_variation and structural_variation_feature.');
