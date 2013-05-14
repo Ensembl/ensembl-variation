a6e4035994f35bc2de18d751da919e89e00491ee
Laurent Gil <lgil@ebi.ac.uk>
2013-05-14 14:28:55 +0000

    Sort the populations by name

--- scripts/misc/generate_population_table.pl
+++ scripts/misc/generate_population_table.pl
@@ -80,7 +80,7 @@ foreach my $project (sort(keys(%pops))) {
   print "$table_header\n";
 
 
-  my $stmt = qq{ SELECT population_id, name, size, description FROM population WHERE $constraint name like ? };
+  my $stmt = qq{ SELECT population_id, name, size, description FROM population WHERE $constraint name like ? ORDER BY name};
   my $sth = $dbVar->prepare($stmt);
   $sth->execute($term);
 	
