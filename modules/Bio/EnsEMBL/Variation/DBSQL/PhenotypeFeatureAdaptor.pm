ae00525036ca0e3f79498cf79c3379d96642034f
William McLaren <wm2@ebi.ac.uk>
2013-09-30 13:39:27 +0000

    hack fix for LRGs changing seq_region_id between releases

--- modules/Bio/EnsEMBL/Variation/DBSQL/PhenotypeFeatureAdaptor.pm
+++ modules/Bio/EnsEMBL/Variation/DBSQL/PhenotypeFeatureAdaptor.pm
@@ -768,6 +768,7 @@ sub _objs_from_sth {
     my $slice = $slice_hash{"ID:".$seq_region_id};
     if(!$slice) {
       $slice = $sa->fetch_by_seq_region_id($seq_region_id);
+      next unless $slice;
       $slice_hash{"ID:".$seq_region_id} = $slice;
       $sr_name_hash{$seq_region_id} = $slice->seq_region_name();
       $sr_cs_hash{$seq_region_id} = $slice->coord_system();
