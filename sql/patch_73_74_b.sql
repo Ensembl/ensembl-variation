3f37c2ac1eea0e4af80531e5ff6e13e59874534c
Sarah Hunt <seh@ebi.ac.uk>
2013-10-01 12:02:17 +0000

    Also need UCSC ID

--- sql/patch_73_74_b.sql
+++ sql/patch_73_74_b.sql
@@ -1,8 +1,10 @@
-## Add doi to publication table
+## Add doi and UCSC id to publication table
 
 
 alter table publication add column doi varchar(50) DEFAULT NULL;
 CREATE INDEX doi_idx on publication (doi); 
 
+alter table publication add column ucsc_id varchar(50) DEFAULT NULL;
+
 #patch identifier
-INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_73_74_b.sql|Add doi to publication table');
+INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_73_74_b.sql|Add doi and UCSC id to publication table');
