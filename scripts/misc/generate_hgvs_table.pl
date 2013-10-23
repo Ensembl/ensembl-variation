7c479edd8074389d5bd4a2597a767a8d9c65d12e
Laurent Gil <lgil@ebi.ac.uk>
2013-10-23 13:18:15 +0000

    Change the license to Apache 2.0 license

--- scripts/misc/generate_hgvs_table.pl
+++ scripts/misc/generate_hgvs_table.pl
@@ -1,16 +1,21 @@
 #!/usr/bin/env perl
+# Copyright 2013 Ensembl
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#      http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 
 ## script to create a table of HGVS stings and variation_ids for search index building
 
-=head1 LICENSE
-
-  Copyright (c) 1999-2013 The European Bioinformatics Institute and
-  Genome Research Limited.  All rights reserved.
-
-  This software is distributed under a modified Apache license.
-  For license details, please see
-
-    http://www.ensembl.org/info/about/legal/code_licence.html
 
 =head1 CONTACT
 
