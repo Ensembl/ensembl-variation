b014b5a047771c6b943fa1dffcf160811d0756ad
Laurent Gil <lgil@ebi.ac.uk>
2013-10-30 14:52:01 +0000

    Add new column for the data types

--- scripts/misc/sources2html.pl
+++ scripts/misc/sources2html.pl
@@ -35,7 +35,7 @@ use Getopt::Long;
 ###############
 ### Options ###
 ###############
-my ($e_version,$html_file,$source_id,$source,$s_version,$s_description,$s_url,$s_type,$s_status,$hlist,$phost,$help);
+my ($e_version,$html_file,$source_id,$source,$s_version,$s_description,$s_url,$s_type,$s_status,$s_data_types,$hlist,$phost,$help);
 ## EG options
 my ($site, $etype);
 
@@ -105,17 +105,18 @@ my $html_header = qq{
 };
 
 my $html_title = qq{
-  <h1 style="margin-top:15px">Ensembl Variation - Sources Documentation</h1>
+  <div style="float:left;width:75%">
+    <h1 style="margin-top:15px">Ensembl Variation - Sources Documentation</h1>
 
-  <h2>List of Variation sources for each species - $ecaption $e_version</h2>
+    <h2>List of Variation sources for each species - $ecaption $e_version</h2>  
 };
 
 
 ##############
-### Footer  ##
+### Footer ###
 ##############
 my $html_footer = qq{
-</div>
+  </div>
 </body>
 </html>};
 
@@ -136,11 +137,8 @@ foreach my $hostname (@hostnames) {
     $dbname =~ /^(.+)_variation/;
     my $s_name = $1;
 
-    my %phe_sources;
-
     if ($etype) { # EG site - need to filter out species
       my $img_thumb = sprintf qq{eg-plugins/%s/htdocs/img/species/thumb_%s.png}, $etype, ucfirst($s_name);
-      #  print "- checking for $img_thumb ... ";
       if (! -e $img_thumb) {
         print "\t... skipping \n";
         next;
@@ -148,9 +146,9 @@ foreach my $hostname (@hostnames) {
     }
     print "\n";
     # Get list of sources from the new databases
-    my $sql2 = qq{SELECT source_id, name, version, description, url, type, somatic_status FROM source};
+    my $sql2 = qq{SELECT source_id, name, version, description, url, type, somatic_status, data_types FROM source};
     my $sth2 = get_connection_and_query($dbname, $hostname, $sql2);
-    $sth2->bind_columns(\$source_id,\$source,\$s_version,\$s_description,\$s_url,\$s_type,\$s_status);
+    $sth2->bind_columns(\$source_id,\$source,\$s_version,\$s_description,\$s_url,\$s_type,\$s_status, \$s_data_types);
     
     
     # Previous database (and sources)
@@ -172,16 +170,8 @@ foreach my $hostname (@hostnames) {
       $is_new_species = 1;
     }
     
-    # Phenotype
-    my $sql_phe = qq{SELECT distinct source_id FROM phenotype_feature};
-    my $sth_phe = get_connection_and_query($dbname, $hostname, $sql_phe);
-    while (my @phe = $sth_phe->fetchrow_array) {
-      $phe_sources{$phe[0]} = 1;
-    }
-    $sth_phe->finish();
-    
     $html_content .= qq{\n  <br />\n} if ($start == 1);
-    $html_content .= source_table($s_name,$sth2,$is_new_species,\%p_list,\%phe_sources);
+    $html_content .= source_table($s_name,$sth2,$is_new_species,\%p_list);
     
     $start = 1 if ($start == 0);
   }
@@ -199,12 +189,15 @@ print HTML $html_footer."\n";
 close(HTML);
 
 
+###############
+### Methods ###
+###############
+
 sub source_table {
   my $name        = shift;
   my $sth         = shift;
   my $is_new      = shift;
   my $p_list      = shift;
-  my $phe_sources = shift;
   
   my $species = $name;
      $species =~ s/_/ /;
@@ -214,7 +207,7 @@ sub source_table {
      $s_name =~ s/\s/_/g;
   my $s_name_id = lc($s_name);
   
-  push (@species_list,{name => $species, anchor => $s_name_id});
+  push (@species_list,{name => $species, s_name => $s_name, anchor => $s_name_id});
   
   my $new_species = ($is_new == 1) ? qq{<span style="padding-left:20px;color:#00F;font-weight:bold">New species!</span>} : '';
   
@@ -275,14 +268,36 @@ sub source_table {
       $source = qq{<a href="$s_url">$source</a>};
     }
     
-    $s_version = format_version($s_version);
+    $s_version = format_version($s_version);  
     
     # Somatic status
     my $s_somatic_status = somatic_status($s_status);
     
     # Phenotype
     my $phe_title = "Provides phenotype data";
-    my $s_phenotype = ($phe_sources->{$source_id}) ? qq{<img src="phenotype.png" style="border-radius:5px;border:1px solid #000" alt="$phe_title" title="$phe_title" />} : '';
+    my $s_phenotype = '';
+    
+    # Data types
+    my @data_types = split(",", $s_data_types);
+    my $data_type_string = '';
+    foreach my $dt (@data_types) {
+      $data_type_string .= ',<br />' if ($data_type_string ne '');
+      if ($dt eq 'phenotype_feature') {
+        $data_type_string .= 'Phenotype association';
+        $s_phenotype = qq{<img src="phenotype.png" style="border-radius:5px;border:1px solid #000" alt="$phe_title" title="$phe_title" />};
+      }
+      elsif ($dt eq 'study') {
+        $data_type_string .= '<span class="_ht" title="Data are grouped by study/publication">'.ucfirst($dt).'</span>';
+      }
+      elsif ($dt eq 'variation_synonym') {
+        $data_type_string .= '<span class="_ht" title="Some/all variants already exist in an other source, or are redundant in this source, with different IDs">'.ucfirst($dt).'</span>';
+      }
+      else {
+        $data_type_string .= ucfirst($dt);
+      }
+    }
+    $data_type_string =~ s/_/ /g;
+    $data_type_string = '-' if ($data_type_string eq '');
     
     # New source/version
     my $s_new_stuff = ($s_new_type) ? new_source_or_version($s_new_type) : '';
@@ -296,6 +311,7 @@ sub source_table {
       <td>$source</td>
       <td>$s_version</td>
       <td>$s_description</td>
+      <td>$data_type_string</td>
       $new
       <td style="text-align:center;width:22px;padding:2px 3px">$s_phenotype</td>
       <td style="text-align:center;width:22px;padding:2px 3px">$s_somatic_status</td>
@@ -402,14 +418,14 @@ sub create_menu {
   <!-- Right hand side menu -->
   <div style="float:right">
   <div style="margin-left:8px;margin-top:2px;padding-bottom:2px;background-color:#F2F2F2;color:#333;border-radius:5px">
-    <div style="padding:5px;font-weight:bold;color:#FFF;background-color:#336;border-top-left-radius:5px;border-top-right-radius:5px">
-      <img src="/i/16/rev/info.png" style="vertical-align:top" />
+    <div style="padding:5px;font-weight:bold;color:#FFF;background-color:#336;border-top-left-radius:5px;border-top-right-radius:5px;margin-bottom:5px">
+      <img src="/i/16/rev/info.png" style="vertical-align:top" alt="info" />
       Species list
-    </div> 
-    <ul style="padding:5px 5px 5px 25px;margin-bottom:0px">
+    </div>
   };
   foreach my $species (@species_list) {
     my $name = $species->{name};
+    my $s_name = $species->{s_name};
     my $anchor = $species->{anchor};
     my $new_data = '';
     if ($species_news{$species->{name}}) {
@@ -420,13 +436,22 @@ sub create_menu {
         $new_data .= qq{<span style="$label_style;margin-right:5px;background-color:$label_colour" title="$label_desc"></span>};
       }
     }
-    $html .= qq{\n      <li><a href="#$anchor" style="margin-right:5px">$name</a>$new_data</li>};
+    #$html .= qq{\n      <li><a href="#$anchor" style="margin-right:5px">$name</a>$new_data</li>};
+    my $img = $name;
+    $html .= qq{
+    <div style="margin-left:5px;margin-bottom:5px">
+      <img src="/i/species/16/$s_name.png" alt="$name" style="border-radius:4px;margin-right:4px;vertical-align:middle" />
+      <a href="#$anchor" style="margin-right:5px">$name</a>$new_data
+    </div>
+    };
   }
   my $v_colour = $colours{'version'};
   my $s_colour = $colours{'source'};
   my $v_label  = $desc{'version'};
   my $s_label  = $desc{'source'};
   
+  my $legend_div_id = 'legend';
+
   $html .= sprintf ( qq{
     </ul>
     <span style="$label_style;margin-left:5px;background-color:$v_colour"></span><small> : $v_label</small>
@@ -434,7 +459,7 @@ sub create_menu {
     <span style="$label_style;margin-left:5px;background-color:$s_colour"></span><small> : $s_label</small>
   </div>
   <br />
-  <div id="legend" style="margin-left:8px;margin-top:2px;background-color:#F2F2F2;color:#333;border-radius:5px">
+  <div id="$legend_div_id" style="margin-left:8px;margin-top:2px;background-color:#F2F2F2;color:#333;border-radius:5px">
     <div style="padding:5px;font-weight:bold;color:#FFF;background-color:#336;border-top-left-radius:5px;border-top-right-radius:5px">
       <img src="/i/16/rev/info.png" style="vertical-align:top" />
       Icons legend
@@ -442,7 +467,7 @@ sub create_menu {
     <table>
       <tr>
         <td style="padding-top:8px;text-align:center">%s</td>
-        <td style="padding-top:6px"><b>New version</b> of the data source<br />in this release for the species</td>
+        <td style="padding-top:6px"><b>New version</b> of the data<br />source in this release<br />for the species</td>
       </tr>
       <tr>
         <td style="padding-top:8px;text-align:center">%s</td>
@@ -467,6 +492,23 @@ sub create_menu {
         <td style="padding-top:6px">The source contains both<br />germline and somatic data</td>
       </tr>
     </table>
+
+    <!-- Javascript used to fix the legend on the right handside when you scroll down -->
+    <script language="Javascript" type="text/javascript">
+      var legend_element = document.getElementById("$legend_div_id");
+      var legend_element_pos = legend_element.offsetTop;
+      window.onscroll = function() {
+        if (document.body.scrollTop > legend_element_pos)  {
+          legend_element.style.position = "fixed";
+          legend_element.style.top = "10px";
+        }
+        else {
+          legend_element.style.position = "relative";
+          legend_element.style.top = "auto";
+        }
+      };
+    </script>
+
   </div>
 </div>
   },
@@ -493,17 +535,17 @@ sub somatic_status {
   my $html;
   if ($type eq 'germline') {
      $html .= qq{
-       <div style="margin-left:auto;margin-right:auto;border-radius:5px;border:1px solid #000;width:20px;height:20px;background-color:#00C;" title="$type data"></div>
+       <div class="_ht" style="margin-left:auto;margin-right:auto;border-radius:5px;border:1px solid #000;width:20px;height:20px;background-color:#00C;" title="$type data"></div>
      };
   }
   elsif ($type eq 'somatic') {
     $html .= qq{
-      <div style="margin-left:auto;margin-right:auto;border-radius:5px;border:1px solid #000;width:20px;height:20px;background-color:#C00;" title="$type data"></div>
+      <div class="_ht" style="margin-left:auto;margin-right:auto;border-radius:5px;border:1px solid #000;width:20px;height:20px;background-color:#C00;" title="$type data"></div>
     };
   }
   elsif ($type eq 'mixed') {
     $html .= qq{
-      <div style="margin-left:auto;margin-right:auto;border-radius:5px;border:1px solid #000;width:20px;height:20px;background-color:#00C;" title="$type data">
+      <div class="_ht" style="margin-left:auto;margin-right:auto;border-radius:5px;border:1px solid #000;width:20px;height:20px;background-color:#00C;" title="$type data">
         <div style="width:0px;height:0px;border-style:solid;border-width:0 0 20px 20px;border-color:transparent transparent #C00 transparent"></div>
       </div>
     };
@@ -521,10 +563,10 @@ sub table_header {
   
   my $header_col;
   if ($flag->{$type}) {
-    my $alt_text = qq{Click here to go to the icons table description};
+    my $alt_text = qq{See the icons description on the table on the right handside of the page};
     $header_col = qq{
     <th colspan=3 style="width:56px;text-align:center;border-left:1px solid #BBB;background-color:#BBB">
-      Other<a href="#legend" class="ht _ht" title="$alt_text"><img src="/i/16/info.png" style="position:relative;top:2px;width:12px;height:12px;margin-left:3px" alt="$alt_text"/></a>
+       Other<span class="_ht ht" title="$alt_text"><img src="/i/16/info.png" style="position:relative;top:2px;width:12px;height:12px;margin-left:3px" title="$alt_text" alt="info"/></span>
     </th>}; 
   }
   else {  
@@ -534,9 +576,10 @@ sub table_header {
   return qq{
     <br />
     <div>
-      <table class="ss" style="width:75%">
-        <tr><th colspan="2">$name</th><th>Version</th><th>Description</th></th>$header_col</tr>
+      <table class="ss">
+        <tr><th colspan="2">$name</th><th>Version</th><th>Description</th><th>Data type(s)</th></th>$header_col</tr>
     };
+#<table class="ss" style="width:75%">
 }
 
 sub usage {
