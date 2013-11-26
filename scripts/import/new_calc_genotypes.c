0b7a89bb2bb5e77c1c09ada7e8fdb776fe22d935
Laurent Gil <lgil@ebi.ac.uk>
2013-11-26 11:37:25 +0000

    Minor update in the Apache 2 license

--- scripts/import/new_calc_genotypes.c
+++ scripts/import/new_calc_genotypes.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 Ensembl
+ * Copyright [1999-2013] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
