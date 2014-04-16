d7f2786056c12824425fdce679a14002b2d4a9b2
William McLaren <wm2@ebi.ac.uk>
2014-04-16 09:54:51 +0100

    fixes for pileup input

--- modules/Bio/EnsEMBL/Variation/Utils/VEP.pm
+++ modules/Bio/EnsEMBL/Variation/Utils/VEP.pm
@@ -313,7 +313,7 @@ sub detect_format {
         $data[0] =~ /(chr)?\w+/ &&
         $data[1] =~ /^\d+$/ &&
         $data[3] =~ /^[ACGTN\-\.]+$/i &&
-        $data[4] =~ /^([\.ACGTN\-]+\,?)+|(\<[A-Z]+\>)$/i
+        $data[4] && $data[4] =~ /^([\.ACGTN\-]+\,?)+|(\<[A-Z]+\>)$/i
     ) {
         return 'vcf';
     }
@@ -712,7 +712,7 @@ sub parse_pileup {
             $var = $data[3];
         }
         else {
-            ($var = unambiguity_code($data[3])) =~ s/$data[2]//ig;
+            ($var = (unambiguity_code($data[3]) || $data[3])) =~ s/$data[2]//ig;
         }
         
         for my $alt(split //, $var){
