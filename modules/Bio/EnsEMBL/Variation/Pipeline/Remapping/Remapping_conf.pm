42009552e6a708749baff197aeccef23a25902a2
Anja Thormann <anja@ebi.ac.uk>
2014-05-06 16:45:55 +0100

    Add new parameters and add default values for some parameters

--- modules/Bio/EnsEMBL/Variation/Pipeline/Remapping/Remapping_conf.pm
+++ modules/Bio/EnsEMBL/Variation/Pipeline/Remapping/Remapping_conf.pm
@@ -37,14 +37,24 @@ sub default_options {
 # make it an option here, contact the variation team to discuss
 # this - patches are welcome!
     return {
-        hive_force_init       => 1,
-        hive_use_param_stack  => 0,
-        hive_use_triggers     => 0,
-        hive_root_dir         => $ENV{'HOME'} . '/DEV/ensembl-hive',
-        ensembl_cvs_root_dir  => $ENV{'HOME'} . '/DEV',
-        hive_db_port          => 3306,
-        hive_db_user          => 'ensadmin',
-
+        hive_force_init         => 1,
+        hive_use_param_stack    => 0,
+        hive_use_triggers       => 0,
+        hive_root_dir           => $ENV{'HOME'} . '/DEV/ensembl-hive',
+        ensembl_cvs_root_dir    => $ENV{'HOME'} . '/DEV',
+        hive_db_port            => 3306,
+        hive_db_user            => 'ensadmin',
+        debug                   => 0,
+        generate_fasta_files    => 0,
+        flank_seq_length        => 150,
+        algn_score_threshold    => 0.95,
+        use_prior_for_filtering => 0,
+        entries_per_file        => 50000,
+        pipeline_dir            => $self->o('pipeline_dir'),
+        dump_features_dir       => $self->o('pipeline_dir') . '/dump_features',
+        fasta_files_dir         => $self->o('pipeline_dir') . '/fasta_files',
+        bam_files_dir           => $self->o('pipeline_dir') . '/bam_files',
+        mapping_results_dir     => $self->o('pipeline_dir') . '/mapping_results', 
         pipeline_db => {
             -host   => $self->o('hive_db_host'),
             -port   => $self->o('hive_db_port'),
@@ -61,6 +71,11 @@ sub pipeline_wide_parameters {
     return {
         %{$self->SUPER::pipeline_wide_parameters},          # here we inherit anything from the base class
         registry_file                => $self->o('registry_file'),
+        registry_file_newasm         => $self->o('registry_file_newasm'),
+        dump_features_dir            => $self->o('dump_features'),
+        filtered_mappings_dir        => $self->o('filtered_mappings'),
+        load_features_dir            => $self->o('load_features'),
+        statistics_dir               => $self->o('statistics'),
         fasta_files_dir              => $self->o('fasta_files'),
         bam_files_dir                => $self->o('bam_files'),
         mapping_results_dir          => $self->o('mapping_results'),
@@ -73,6 +88,10 @@ sub pipeline_wide_parameters {
         pipeline_dir                 => $self->o('pipeline_dir'),
         flank_seq_length             => $self->o('flank_seq_length'),	
         generate_fasta_files         => $self->o('generate_fasta_files'),
+        feature_table                => $self->o('feature_table'),
+        algn_score_threshold         => $self->o('algn_score_threshold'),
+        use_prior_for_filtering      => $self->o('use_prior_for_filtering'),
+        entries_per_file             => $self->o('entries_per_file'),
         debug                        => $self->o('debug'),
     };
 }
