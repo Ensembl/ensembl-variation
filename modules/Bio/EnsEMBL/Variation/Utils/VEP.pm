0e98c711294f6a8942072c83d291cec3de0039cf
William McLaren <wm2@ebi.ac.uk>
2014-05-19 09:39:42 +0100

    check defined before testing order

--- modules/Bio/EnsEMBL/Variation/Utils/VEP.pm
+++ modules/Bio/EnsEMBL/Variation/Utils/VEP.pm
@@ -1229,7 +1229,7 @@ sub get_all_consequences {
     
     # check and order
     my $test = $return[0];
-    if(ref($test) ne 'HASH' && $$test =~ /^\#\#\#ORDER\#\#\#/) {
+    if(defined($test) && ref($test) ne 'HASH' && $$test =~ /^\#\#\#ORDER\#\#\#/) {
       @return = sort {$$a cmp $$b} @return;
       $$_ =~ s/\#\#\#ORDER\#\#\# \d+ // for @return; 
     }
