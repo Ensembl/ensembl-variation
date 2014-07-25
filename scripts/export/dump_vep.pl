31698c3415b22502979ee9b9e30f0899503d1656
William McLaren <wm2@ebi.ac.uk>
2014-07-25 10:36:41 +0100

    fix whitespace

--- scripts/export/dump_vep.pl
+++ scripts/export/dump_vep.pl
@@ -32,32 +32,32 @@ use DBI;
 my $config = {};
 
 my %special_options = (
-	'homo_sapiens'      => ' --sift b --polyphen b --regulatory'.
-	                       ' --freq_file /nfs/ensembl/wm2/VEP/cache/ALL_1KG_ESP_freqs_with_alleles.txt,AFR,AMR,ASN,EUR,AA,EA',
-	'mus_musculus'      => ' --regulatory --sift b',
-	'bos_taurus'        => ' --sift b',
-	'canis_familiaris'  => ' --sift b',
-	'danio_rerio'       => ' --sift b',
-	'equus_caballus'    => ' --sift b',
-	'gallus_gallus'     => ' --sift b',
-	'rattus_norvegicus' => ' --sift b',
-	'sus_scrofa'        => ' --sift b',
+  'homo_sapiens'      => ' --sift b --polyphen b --regulatory'.
+                         ' --freq_file /nfs/ensembl/wm2/VEP/cache/ALL_1KG_ESP_freqs_with_alleles.txt,AFR,AMR,ASN,EUR,AA,EA',
+  'mus_musculus'      => ' --regulatory --sift b',
+  'bos_taurus'        => ' --sift b',
+  'canis_familiaris'  => ' --sift b',
+  'danio_rerio'       => ' --sift b',
+  'equus_caballus'    => ' --sift b',
+  'gallus_gallus'     => ' --sift b',
+  'rattus_norvegicus' => ' --sift b',
+  'sus_scrofa'        => ' --sift b',
 );
 
 GetOptions(
     $config,
-	'hosts|h=s',
-	'user|u=s',
-	'port|P=i',
-	'password|p=s',
-	'dir=s',
-	'command=s',
-	'mem=i',
-	'pattern=s',
+  'hosts|h=s',
+  'user|u=s',
+  'port|P=i',
+  'password|p=s',
+  'dir=s',
+  'command=s',
+  'mem=i',
+  'pattern=s',
   'exclude_pattern=s',
-	'queue=s',
-	'version=i',
-	'overwrite',
+  'queue=s',
+  'version=i',
+  'overwrite',
   'refseq',
 ) or die "ERROR: Could not parse command line options\n";
 
@@ -78,55 +78,55 @@ die "ERROR: No Ensembl DB version defined - use --version [version]\n" unless de
 
 # check command supplied looks sensible
 die "ERROR: Supplied command doesn't look right, it should look something like:\n\nperl -I /include/perl/libs/ /path/to/variant_effect_predictor.pl --build all\n\n"
-	unless $config->{command} =~ /variant_effect_predictor.+\-build (all|\w+)/;
+  unless $config->{command} =~ /variant_effect_predictor.+\-build (all|\w+)/;
 
 # parse hosts
 $config->{hosts} = [split /\,/, $config->{hosts}];
 
 foreach my $host(@{$config->{hosts}}) {
-	debug("Getting species list for host $host");
-	my $species_list = get_species_list($config, $host);
-	debug("Found ".(scalar @$species_list)." valid databases\n");
-	
-	foreach my $species_hash(@$species_list) {
+  debug("Getting species list for host $host");
+  my $species_list = get_species_list($config, $host);
+  debug("Found ".(scalar @$species_list)." valid databases\n");
+  
+  foreach my $species_hash(@$species_list) {
     my ($species, $assembly) = ($species_hash->{species}, $species_hash->{assembly});
     
-		debug("Running VEP dump for $species");
-		dump_vep($config, $host, $species, $assembly);
-		
-		debug("Compressing dump file");
-		tar($config, $species, $assembly);
-	}
+    debug("Running VEP dump for $species");
+    dump_vep($config, $host, $species, $assembly);
+    
+    debug("Compressing dump file");
+    tar($config, $species, $assembly);
+  }
 }
 
 sub get_species_list {
-	my $config = shift;
-	my $host   = shift;
+  my $config = shift;
+  my $host   = shift;
 
-	my $connection_string = sprintf(
-			"DBI:mysql(RaiseError=>1):host=%s;port=%s",
-			$host,
-			$config->{port}
-		);
-	
-	# connect to DB
-	my $dbc = DBI->connect(
-	    $connection_string, $config->{user}, $config->{password}
-	);
-	
-	my $version = $config->{version};
+  my $connection_string = sprintf(
+      "DBI:mysql(RaiseError=>1):host=%s;port=%s",
+      $host,
+      $config->{port}
+    );
+  
+  # connect to DB
+  my $dbc = DBI->connect(
+      $connection_string, $config->{user}, $config->{password}
+  );
+  
+  my $version = $config->{version};
 
-	my $sth = $dbc->prepare(qq{
-		SHOW DATABASES LIKE '%\_core\_$version%'
-	});
-	$sth->execute();
-	
-	my $db;
-	$sth->bind_columns(\$db);
-	
-	my @dbs;
-	push @dbs, $db while $sth->fetch;
-	$sth->finish;
+  my $sth = $dbc->prepare(qq{
+    SHOW DATABASES LIKE '%\_core\_$version%'
+  });
+  $sth->execute();
+  
+  my $db;
+  $sth->bind_columns(\$db);
+  
+  my @dbs;
+  push @dbs, $db while $sth->fetch;
+  $sth->finish;
   
   # refseq?
   if(defined($config->{refseq})) {
@@ -140,18 +140,18 @@ sub get_species_list {
     $sth->finish;
   }
 
-	# remove master and coreexpression
-	@dbs = grep {$_ !~ /master|express/} @dbs;
+  # remove master and coreexpression
+  @dbs = grep {$_ !~ /master|express/} @dbs;
 
-	# filter on pattern if given
-	my $pattern = $config->{pattern};
+  # filter on pattern if given
+  my $pattern = $config->{pattern};
   my $exclude = $config->{exclude_pattern};
-	@dbs = grep {$_ =~ /$pattern/} @dbs if defined($pattern);
-	@dbs = grep {$_ !~ /$exclude/} @dbs if defined($exclude);
+  @dbs = grep {$_ =~ /$pattern/} @dbs if defined($pattern);
+  @dbs = grep {$_ !~ /$exclude/} @dbs if defined($exclude);
 
-	my @species;
+  my @species;
 
-	foreach my $current_db_name (@dbs) {
+  foreach my $current_db_name (@dbs) {
     
     # special case otherfeatures
     # check it has refseq transcripts
@@ -203,107 +203,107 @@ sub get_species_list {
       
       die("ERROR: Problem getting species and assembly names from $current_db_name; check meta table\n") unless $count;
     }
-	}
+  }
 
-	return \@species;
+  return \@species;
 }
 
 sub dump_vep {
-	my $config = shift;
-	my $host   = shift;
-	my $sp     = shift;
+  my $config = shift;
+  my $host   = shift;
+  my $sp     = shift;
   my $ass    = shift;
   
-	# check if dir exists
-	if(!defined($config->{overwrite}) && -e $config->{dir}.'/'.$sp.'/'.$config->{version}.'_'.$ass) {
-		debug("Existing dump directory found for $sp $ass, skipping (use --overwrite to overwrite)\n");
-		return;
-	}
-	
+  # check if dir exists
+  if(!defined($config->{overwrite}) && -e $config->{dir}.'/'.$sp.'/'.$config->{version}.'_'.$ass) {
+    debug("Existing dump directory found for $sp $ass, skipping (use --overwrite to overwrite)\n");
+    return;
+  }
+  
   my $species = $sp;
   my $refseq = $species =~ s/\_refseq$//;
-	
-	my $command = join " ", (
-		sprintf(
-			'bsub -K -J %s_%s -M %i -R"select[mem>%i] rusage[mem=%i]" -q %s -o %s -e %s',
-			$species,
+  
+  my $command = join " ", (
+    sprintf(
+      'bsub -K -J %s_%s -M %i -R"select[mem>%i] rusage[mem=%i]" -q %s -o %s -e %s',
+      $species,
       $ass,
-			$config->{mem},
-			$config->{mem},
-			$config->{mem},
-			$config->{queue},
-			$config->{dir}.'/'.$sp.'_vep_dump.farmout',
-			$config->{dir}.'/'.$sp.'_vep_dump.farmerr',
-		),
-		$config->{command},
-		defined $special_options{$species} ? $special_options{$species} : "",
-		"--no_adaptor",
-		"--species ".$species,
-		"--host ".$host,
-		"--user ".$config->{user},
-		"--port ".$config->{port},
-		"--dir ".$config->{dir},
-		defined $config->{password} ? "--password ".$config->{pass} : "",
+      $config->{mem},
+      $config->{mem},
+      $config->{mem},
+      $config->{queue},
+      $config->{dir}.'/'.$sp.'_'.$ass.'_vep_dump.farmout',
+      $config->{dir}.'/'.$sp.'_'.$ass.'_vep_dump.farmerr',
+    ),
+    $config->{command},
+    defined $special_options{$species} ? $special_options{$species} : "",
+    "--no_adaptor",
+    "--species ".$species,
+    "--host ".$host,
+    "--user ".$config->{user},
+    "--port ".$config->{port},
+    "--dir ".$config->{dir},
+    defined $config->{password} ? "--password ".$config->{password} : "",
     $refseq ? '--refseq' : '',
-	);
-	
-	debug("Use \"tail -f ".$config->{dir}.'/'.$species.'_vep_dump.farmout'."\" to check progress");
-	system($command);
+  );
+  
+  debug("Use \"tail -f ".$config->{dir}.'/'.$species.'_vep_dump.farmout'."\" to check progress");
+  system($command);
   #print "$command\n";
 }
 
 sub tar {
-	my $config   = shift;
-	my $species  = shift;
+  my $config   = shift;
+  my $species  = shift;
   my $assembly = shift;
-	
-	my $tar_file = $config->{dir}."/".$species."_vep_".$config->{version}."_".$assembly.".tar.gz";
-	
-	# check if tar exists
-	if(!defined($config->{overwrite}) && -e $tar_file) {
-		debug("Existing dump file found for $species, skipping (use --overwrite to overwrite)\n");
-		return;
-	}
-	
-	# check dir exists
-	my $root_dir = $config->{dir};
-	my $sub_dir  = $species."/".$config->{version}."_".$assembly;
-	
-	die("ERROR: VEP dump directory $root_dir/$sub_dir not found") unless -e $root_dir.'/'.$sub_dir;
-	
-	my $command = "tar -cz -C $root_dir -f $tar_file $sub_dir";
-	system($command);# or die "ERROR: Failed to create tar file $tar_file\n";
+  
+  my $tar_file = $config->{dir}."/".$species."_vep_".$config->{version}."_".$assembly.".tar.gz";
+  
+  # check if tar exists
+  if(!defined($config->{overwrite}) && -e $tar_file) {
+    debug("Existing dump file found for $species, skipping (use --overwrite to overwrite)\n");
+    return;
+  }
+  
+  # check dir exists
+  my $root_dir = $config->{dir};
+  my $sub_dir  = $species."/".$config->{version}."_".$assembly;
+  
+  die("ERROR: VEP dump directory $root_dir/$sub_dir not found") unless -e $root_dir.'/'.$sub_dir;
+  
+  my $command = "tar -cz -C $root_dir -f $tar_file $sub_dir";
+  system($command);# or die "ERROR: Failed to create tar file $tar_file\n";
   #print "$command\n";
 }
 
 # gets time
 sub get_time() {
-    my @time = localtime(time());
+  my @time = localtime(time());
 
-    # increment the month (Jan = 0)
-    $time[4]++;
+  # increment the month (Jan = 0)
+  $time[4]++;
 
-    # add leading zeroes as required
-    for my $i(0..4) {
-        $time[$i] = "0".$time[$i] if $time[$i] < 10;
-    }
+  # add leading zeroes as required
+  for my $i(0..4) {
+    $time[$i] = "0".$time[$i] if $time[$i] < 10;
+  }
 
-    # put the components together in a string
-    my $time =
-        ($time[5] + 1900)."-".
-        $time[4]."-".
-        $time[3]." ".
-        $time[2].":".
-        $time[1].":".
-        $time[0];
+  # put the components together in a string
+  my $time =
+    ($time[5] + 1900)."-".
+    $time[4]."-".
+    $time[3]." ".
+    $time[2].":".
+    $time[1].":".
+    $time[0];
 
-    return $time;
+  return $time;
 }
 
 # prints debug output with time
 sub debug {
-    my $text = (@_ ? (join "", @_) : "No message");
-    my $time = get_time;
-    
-    print $time." - ".$text.($text =~ /\n$/ ? "" : "\n");
+  my $text = (@_ ? (join "", @_) : "No message");
+  my $time = get_time;
+  
+  print $time." - ".$text.($text =~ /\n$/ ? "" : "\n");
 }
