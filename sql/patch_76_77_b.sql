908098216abe6281cd104fe292b2e23e9336909b
Sarah Hunt <seh@ebi.ac.uk>
2014-09-25 12:51:33 +0100

    improve data update statement

--- sql/patch_76_77_b.sql
+++ sql/patch_76_77_b.sql
@@ -2,7 +2,9 @@
 
 ALTER TABLE variation ADD COLUMN display int(1) DEFAULT 1;
 
-UPDATE variation SET display = 0 WHERE variation_id in (SELECT variation_id FROM failed_variation);
+UPDATE variation SET display = 0 WHERE variation_id in (SELECT failed_variation.variation_id FROM failed_variation
+left outer join variation_citation on failed_variation.variation_id = variation_citation.variation_id 
+where variation_citation.publication_id is null );
 
 
 #patch identifier
