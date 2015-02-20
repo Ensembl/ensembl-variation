fc3e9f024cd54ab5d1fa5e1d030154d4e1cce244
Laurent Gil <lgil@ebi.ac.uk>
2015-02-20 10:49:33 +0000

    Update the SV classes colour scheme

--- scripts/misc/generate_classes_table.pl
+++ scripts/misc/generate_classes_table.pl
@@ -59,21 +59,22 @@ $registry->load_registry_from_db(
 );
 
 my %colour = (
-    'copy_number_variation'         => '#000000',
-    'insertion'                     => '#FFCC00',
+    'copy_number_variation'         => '#8601AF',
+    'insertion'                     => '#000099',
     'copy_number_gain'              => '#0000CC', 
     'copy_number_loss'              => '#CC0000',
-    'inversion'                     => '#9933FF', 
-    'complex_structural_alteration' => '#99CCFF',
+    'inversion'                     => '#C530FF',
+    'complex_structural_alteration' => '#007FFF',
     'tandem_duplication'            => '#732E00',
-    'mobile_element_insertion'      => '#FFCC00',
-    'novel_sequence_insertion'      => '#FFCC00',
+    'mobile_element_insertion'      => '#000099',
+    'novel_sequence_insertion'      => '#000099',
     'translocation'                 => '#C3A4FF',
     'deletion'                      => '#CC0000',
-    'duplication'                   => '#000000',
+    'duplication'                   => '#0000CC',
+    'indel'                         => 'gold',
     'probe'                         => '#4682B4',
 );
-my $default_colour = '#B2B2B2';
+my $default_colour = '#000000';
 
 
 my %type = (
