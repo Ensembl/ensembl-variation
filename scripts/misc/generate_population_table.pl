1eb25a40710b719cd9327cd8b48dd1e679fd907d
Laurent Gil <lgil@ebi.ac.uk>
2015-03-12 13:51:27 +0000

    Add the 'NextGen Project' populations (sheep)

--- scripts/misc/generate_population_table.pl
+++ scripts/misc/generate_population_table.pl
@@ -80,7 +80,12 @@ my %pops = ('1000 Genomes'                   => { 'order'      => 1,
                                                   'species'  => 'Mus musculus',
                                                   'term'     => 'Mouse_Genomes_Project',
                                                   'url'      => 'http://www.sanger.ac.uk/resources/mouse/genomes/'
-                                                }
+                                                },
+            'NextGen Project'                => { 'order'    => 5,
+                                                  'species'  => 'Ovis_aries',
+                                                  'term'     => 'NextGen Project',
+                                                  'url'      => 'http://projects.ensembl.org/nextgen/'
+                                                },                                   
            );
            
 my $server_name = 'http://static.ensembl.org';
