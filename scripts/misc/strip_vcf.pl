cf5c271e16953ed93b174110118a2bd1c6688ac6
William McLaren <wm2@ebi.ac.uk>
2015-04-08 16:25:57 +0100

    script for stripping VCF files for use with VCFCollection API

---
+++ scripts/misc/strip_vcf.pl
@@ -0,0 +1,173 @@
+#!/usr/bin/env perl
+
+use strict;
+use Getopt::Long;
+use FileHandle;
+
+## configure
+my $config = {
+  keep_headers  => 'CHROM,fileformat',
+  delete_fields => 'QUAL,FILTER,INFO'
+};
+
+GetOptions(
+  $config,
+  'help|h',
+  'input_file|i=s',
+  'output_file|o=s',
+  'keep_headers|k=s',
+  'delete_fields|d=s'
+);
+
+if($config->{help}) {
+  usage();
+  exit(0);
+}
+
+$config->{$_} = [split(/\,/, ($config->{$_} || ''))] for qw(include_headers delete_fields);
+
+## input
+my $in = *STDIN;
+
+# file provided?
+if($config->{input_file}) {
+  my $file = $config->{input_file};
+  
+  die("ERROR: Input file not found\n") unless -e $file;
+  
+  $in = FileHandle->new();
+  
+  # gzipped?
+  if($file =~ /\.gz$/ || -B $file) {
+    $in->open('gzip -dc '.$file.' | ') or die("ERROR: Could not read from input file ".$file."\n");
+  }
+  
+  # uncompressed
+  else {
+    die("ERROR: Input file looks like a binary (is it gzipped but not named [file].gz?)\n") if -B $file;
+    $in->open($file) or die("ERROR: Could not read from input file ".$file."\n");
+  }
+}
+
+## output
+my $out = *STDOUT;
+
+if($config->{output_file}) {
+  my $file = $config->{output_file};
+  $out = FileHandle->new();
+  
+  if($file =~ /\.gz/) {
+    $out->open('| bgzip -c > '.$file) or die("ERROR: Could not write to output file ".$file."\n");
+  }
+  else {
+    $out->open('> '.$file) or die("ERROR: Could not write to output file ".$file."\n");
+  }
+}
+
+my $meta = {};
+my $headers = {};
+
+while(<$in>) {
+  
+  # headers
+  if(/^\#/) {
+      
+    # parse column headers
+    if(/^\#CHROM/) {
+      my $l = $_;
+      chomp($l);
+      my @data = split(/\t/, $l);
+    
+      $headers->{$data[$_]} = $_ for (0..$#data);
+    }
+    
+    if(scalar @{$config->{keep_headers}} && $config->{keep_headers}->[0] eq 'ALL') {
+      print;
+      next;
+    }
+    
+    foreach my $inc(@{$config->{keep_headers}}) {
+      
+      if(/^\#+$inc/) {
+        print $out $_;
+        next;
+      }
+    }
+  }
+  
+  # data
+  else {
+    die("ERROR: Failed to parse headers\n") unless scalar keys %$headers;
+    
+    chomp;
+    my @data = split /\t/, $_;
+    
+    # has genotypes?
+    if(!exists($meta->{has_genotypes})) {
+      
+      # default to no
+      $meta->{has_genotypes} = 0;
+      
+      if($headers->{FORMAT}) {
+        my $part_num = 0;
+        
+        foreach my $part(split(/\:/, $data[$headers->{FORMAT}] || '')) {
+          $meta->{has_genotypes} = ++$part_num if $part eq 'GT';
+        }
+      }
+    }
+    
+    # delete/null fields 
+    $data[$headers->{$_}] = '.' for @{$config->{delete_fields}};
+    
+    # remove non-genotype bits
+    if($meta->{has_genotypes}) {
+      
+      $data[$headers->{FORMAT}] = 'GT';
+      
+      my $from = $headers->{FORMAT} + 1;
+      my $gt_part = $meta->{has_genotypes} - 1;
+    
+      for my $i($from..$#data) {
+        my @parts = split(/\:/, $data[$i]);
+        $data[$i] = $parts[$gt_part];
+      }
+    }
+    
+    print $out join("\t", @data)."\n";
+  }
+}
+
+sub usage {
+    my $usage =<<END;
+
+Script to strip down a VCF file's size, for use with VCFCollection API.
+
+NB: Stripped VCFs should NOT be shared except for use with the Ensembl Variation API!
+
+Usage:
+perl $0 [arguments]
+
+Arguments
+=============
+
+-h | --help            Display this message and quit
+
+-i | --input_file      Input file (reads from STDIN if not specified)
+                       File may be uncompressed, gzipped or bgzipped.
+                       
+-o | --output_file     Output file (writes to STDOUT if not specified)
+                       If file name ends in ".gz", output will be compressed with bgzip
+                       
+-k | --keep_headers    Comma separated list of header line keys to keep
+                       By default retains the fileformat and CHROM header lines
+                       Set to "ALL" to retain all header lines.
+                       
+-d | --delete_fields   Comma separated list of fields/columns to "blank" i.e. set to "."
+                       By default deletes QUAL, FILTER and INFO.
+                       NB: future API versions may use information from the INFO field.
+
+END
+
+    print $usage;
+}
