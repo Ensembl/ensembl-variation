1dc57ed9772cdd18117f9d21df761e76639ee491
Anja Thormann <anja@ebi.ac.uk>
2015-05-08 13:28:02 +0100

    fix indentation

--- modules/Bio/EnsEMBL/Variation/Pipeline/ReleaseDataDumps/CleanUp.pm
+++ modules/Bio/EnsEMBL/Variation/Pipeline/ReleaseDataDumps/CleanUp.pm
@@ -6,7 +6,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-     http://www.apache.org/licenses/LICENSE-2.0
+http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -19,11 +19,11 @@ limitations under the License.
 
 =head1 CONTACT
 
- Please email comments or questions to the public Ensembl
- developers list at <http://lists.ensembl.org/mailman/listinfo/dev>.
+Please email comments or questions to the public Ensembl
+developers list at <http://lists.ensembl.org/mailman/listinfo/dev>.
 
- Questions may also be sent to the Ensembl help desk at
- <http://www.ensembl.org/Help/Contact>.
+Questions may also be sent to the Ensembl help desk at
+<http://www.ensembl.org/Help/Contact>.
 
 =cut
 package Bio::EnsEMBL::Variation::Pipeline::ReleaseDataDumps::CleanUp;
@@ -33,40 +33,40 @@ use strict;
 use base ('Bio::EnsEMBL::Variation::Pipeline::ReleaseDataDumps::BaseDataDumpsProcess');
 
 sub fetch_input {
-	my $self = shift;
+  my $self = shift;
 }
 
 sub run {
-    my $self = shift;
-	my $data_dump_dir = $self->param('pipeline_dir');
-	my $tmp_dir = $self->param('tmp_dir');
-	my $file_type = $self->param('file_type');
-    my $all_species = $self->get_all_species();	
+  my $self = shift;
+  my $data_dump_dir = $self->param('pipeline_dir');
+  my $tmp_dir = $self->param('tmp_dir');
+  my $file_type = $self->param('file_type');
+  my $all_species = $self->get_all_species();	
 
-	foreach my $species (keys %$all_species) {
-		my $working_dir = "$data_dump_dir/$file_type/$species/";			
-		`mv -f $working_dir/*.out $tmp_dir`;
-		`mv -f $working_dir/*.err $tmp_dir`;			
-		opendir(DIR, $working_dir) or die $!;
-		while (my $file = readdir(DIR))	{
-			next if ($file =~ m/^\./);
-			if ($file =~ m/gvf/) {
-                if ($file =~ m/generic/) {
-				    my $current_name = $file;
-                    $file =~ s/_generic//;
-				    `mv $working_dir/$current_name $working_dir/$file`; # rename file name
-                }
-				`gzip $working_dir/$file`;
-			} elsif ($file =~ m/\.vcf$/) {
-				`mv -f $working_dir/$file $tmp_dir`;
-			} 
-		}				
-		closedir(DIR);
-	}	
+  foreach my $species (keys %$all_species) {
+    my $working_dir = "$data_dump_dir/$file_type/$species/";			
+    `mv -f $working_dir/*.out $tmp_dir`;
+    `mv -f $working_dir/*.err $tmp_dir`;			
+    opendir(DIR, $working_dir) or die $!;
+    while (my $file = readdir(DIR))	{
+      next if ($file =~ m/^\./);
+      if ($file =~ m/gvf/) {
+        if ($file =~ m/generic/) {
+          my $current_name = $file;
+          $file =~ s/_generic//;
+          `mv $working_dir/$current_name $working_dir/$file`; # rename file name
+        }
+        `gzip $working_dir/$file`;
+      } elsif ($file =~ m/\.vcf$/) {
+        `mv -f $working_dir/$file $tmp_dir`;
+      } 
+    }				
+    closedir(DIR);
+  }	
 }
 
 sub write_output {
-	my $self = shift;
+  my $self = shift;
 }
 
 1;
