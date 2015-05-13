5e261b8182eaa989abd277d6b4afaf8cdd1393b3
Anja Thormann <anja@ebi.ac.uk>
2015-05-13 16:08:58 +0100

    schema update

---
+++ sql/patch_80_81_a.sql
@@ -0,0 +1,6 @@
+# update the schema_version entry in the meta table
+UPDATE meta SET meta_value = 81 WHERE meta_key = 'schema_version';
+
+# patch identifier
+INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_80_81_a.sql|schema version');
+
