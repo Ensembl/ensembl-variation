754e9c0ba522b4667dab274ca38e9a7f21696224
Anja Thormann <anja@ebi.ac.uk>
2015-05-18 11:36:51 +0100

    Fix create index

--- sql/patch_80_81_c.sql
+++ sql/patch_80_81_c.sql
@@ -4,20 +4,20 @@
 RENAME TABLE individual_population TO sample_population;
 ALTER TABLE sample_population CHANGE individual_id sample_id int(10) unsigned NOT NULL;
 DROP INDEX `individual_idx` ON sample_population;
-CREATE INDEX sample_idx ON sample_population(sample_id(10));
+CREATE INDEX sample_idx ON sample_population(sample_id);
 
 RENAME TABLE individual_genotype_multiple_bp TO sample_genotype_multiple_bp;
 ALTER TABLE sample_genotype_multiple_bp CHANGE individual_id sample_id int(10) unsigned DEFAULT NULL;
 DROP INDEX `individual_idx` ON sample_genotype_multiple_bp;
-CREATE INDEX sample_idx ON sample_genotype_multiple_bp(sample_id(10));
+CREATE INDEX sample_idx ON sample_genotype_multiple_bp(sample_id);
 
 ALTER TABLE compressed_genotype_region CHANGE individual_id sample_id int(10) unsigned NOT NULL;
 DROP INDEX `individual_idx` ON compressed_genotype_region;
-CREATE INDEX sample_idx ON compressed_genotype_region(sample_id(10));
+CREATE INDEX sample_idx ON compressed_genotype_region(sample_id);
 
 ALTER TABLE structural_variation_sample CHANGE individual_id sample_id int(10) unsigned DEFAULT NULL;
 DROP INDEX `individual_idx` ON structural_variation_sample;
-CREATE INDEX sample_idx ON structural_variation_sample (sample_id(10));
+CREATE INDEX sample_idx ON structural_variation_sample (sample_id);
 
 ALTER TABLE read_coverage CHANGE individual_id sample_id int(10) unsigned NOT NULL;
 
