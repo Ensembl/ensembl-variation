f863d8b08075840a844441e75ccabf45a7deae43
Anja Thormann <anja@ebi.ac.uk>
2015-06-23 17:10:19 +0100

    Drop tmp tables

--- modules/Bio/EnsEMBL/Variation/Pipeline/FinishRegulationEffect.pm
+++ modules/Bio/EnsEMBL/Variation/Pipeline/FinishRegulationEffect.pm
@@ -36,11 +36,17 @@ use base qw(Bio::EnsEMBL::Variation::Pipeline::BaseVariationProcess);
 
 sub run {
     my $self = shift;
+
     my $consequence_types = "'intergenic_variant','splice_acceptor_variant','splice_donor_variant','stop_lost','coding_sequence_variant','missense_variant','stop_gained','synonymous_variant','frameshift_variant','non_coding_transcript_variant','non_coding_transcript_exon_variant','mature_miRNA_variant','NMD_transcript_variant','5_prime_UTR_variant','3_prime_UTR_variant','incomplete_terminal_codon_variant','intron_variant','splice_region_variant','downstream_gene_variant','upstream_gene_variant','start_lost','stop_retained_variant','inframe_insertion','inframe_deletion','transcript_ablation','transcript_fusion','transcript_amplification','transcript_translocation','TFBS_ablation','TFBS_fusion','TFBS_amplification','TFBS_translocation','regulatory_region_ablation','regulatory_region_fusion','regulatory_region_amplification','regulatory_region_translocation','feature_elongation','feature_truncation','protein_altering_variant','regulatory_region_variant','TF_binding_site_variant'";
 
     if ($self->param('update_vf') || $self->param('only_update_vf')) {
         my $vdba = $self->get_species_adaptor('variation');
         my $dbc = $vdba->dbc;
+
+        # pre-clean-up:
+        foreach my $table (qw/regulatory_region_consequences variation_feature_overlap_regulation variation_feature_consequences/) {
+          $dbc->do(qq{DROP TABLE IF EXISTS $table;})
+        }
 
         my @regulatory_tables = ('motif_feature_variation', 'regulatory_feature_variation');
 
@@ -102,12 +108,11 @@ sub run {
 
         $self->warning('Completed update of variation_feature table.');
 
-        # drop tmp tables
-#        $dbc->do(qw{ DROP TABLE regulatory_region_consequences;}) or warn "Failed to drop table regulatory_region_consequences.";
-#        $dbc->do(qw{ DROP TABLE variation_feature_overlap_regulation;}) or warn "Failed to drop table variation_feature_overlap_regulation.";
-#        $dbc->do(qw{ DROP TABLE variation_feature_consequences;}) or warn "Failed to drop table variation_feature_consequences.";
+        # post-clean-up:
+        foreach my $table (qw/regulatory_region_consequences variation_feature_overlap_regulation variation_feature_consequences/) {
+          $dbc->do(qq{DROP TABLE IF EXISTS $table;})
+        }
     }
 }
-
 
 1;
