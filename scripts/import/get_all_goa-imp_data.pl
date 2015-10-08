4bd855225e092af2e114c69327c9bc363d0e3fd4
Laurent Gil <lgil@ebi.ac.uk>
2015-10-08 11:25:06 +0100

    Update the script to retrieve GOA data from several species

--- scripts/import/get_all_goa-imp_data.pl
+++ scripts/import/get_all_goa-imp_data.pl
@@ -31,124 +31,210 @@ use Bio::EnsEMBL::Registry;
 use HTTP::Tiny;
 use Getopt::Long;
 
-our ($species, $output_file, $registry_file, $help);
+our ($output_dir, $e_version, $species, $hlist, $user, $port, $help);
 
-GetOptions('species=s'     => \$species,
-           'output_file=s' => \$output_file,
-           'registry=s'    => \$registry_file,
-           'help!'         => \$help,
+GetOptions('output_dir=s' => \$output_dir,
+           'v=s'          => \$e_version,
+           'species=s'    => \$species,
+           'hlist=s'      => \$hlist,
+           'user=s'       => \$user,
+           'port=i'       => \$port,
+           'help!'        => \$help,
           );
-          
-$registry_file ||= "./ensembl.registry";
-$species ||= 'human';
 
 usage() if ($help);
 
-usage('-output_file argument is required') if (!$output_file);
+usage('-output_dir argument is required') if (!$output_dir);
+die ("Output dir '$output_dir' doesn't exist") unless (-d $output_dir);
+
+if (!$e_version) {
+  print "> Error! Please give an Ensembl version, using the option '-v' \n";
+  usage();
+}
+if (!$hlist) {
+  print "> Error! Please give the list of host names where the new databases are stored using the option '-hlist'\n";
+  usage();
+}
+if (!$user) {
+  print "> Error! Please give user name using the option '-user'\n";
+  usage();
+}
 
 my $ens_type    = 'Translation';
 my $ens_linkage = 'IMP';
-my $ens_prot_db = 'UniProtKB/Swiss-Prot';
+my $ens_prot_db = '%UniProtKB%';
 my $pmid_col    = 'REFERENCE';
 my $rest_url    = 'http://www.ebi.ac.uk/QuickGO/GAnnotation?format=tsv&goid=%s&evidence=%s&protein=%s';
 
-# connect to databases
-Bio::EnsEMBL::Registry->load_all( $registry_file );
-my $db_adaptor = Bio::EnsEMBL::Registry->get_DBAdaptor($species,'core');
-my $dbCore = $db_adaptor->dbc->db_handle;
+my $database = "";
+my $pswd = "";
+my $db_core_type = 'core';
+my $db_var_type  = 'variation';
+my $default_port = 3306;
+$port ||= $default_port;
+my @hostnames = split /,/, $hlist;
+my %species_list;
+%species_list = map {$_ => 1} split /,/, $species if ($species);
+my %species_found;
+
+
+my $sql  = qq{SHOW DATABASES LIKE '%$db_var_type\_$e_version%'};
 
 my $sql1 = qq{
   SELECT t.translation_id, t.stable_id, t.transcript_id, x1.dbprimary_acc, x2.display_label, x2.description
-  FROM translation t, ontology_xref o, object_xref obj, xref x1, xref x2
+  FROM translation t, ontology_xref o, object_xref obj, xref x1, xref x2, external_db e
   WHERE
     o.object_xref_id=obj.object_xref_id AND
     o.source_xref_id=x1.xref_id AND
     t.translation_id=obj.ensembl_id AND
     obj.xref_id=x2.xref_id AND
+    x1.external_db_id=e.external_db_id AND
+    e.db_display_name like "$ens_prot_db" AND
     o.linkage_type="$ens_linkage" AND
     obj.ensembl_object_type="$ens_type"         
 };
 
-my $sql2 = qq{
-  SELECT g.stable_id FROM gene g, transcript t WHERE t.gene_id=g.gene_id AND t.transcript_id=?
-};
 
-
-my %gene_list;
-my %go_uniprot;
-
-my $sth1 = $db_adaptor->dbc->prepare($sql1);
-my $sth2 = $db_adaptor->dbc->prepare($sql2);
-
-my ($translation_id, $translation, $transcript_id, $uniprot_id, $go_term, $go_desc);
-$sth1->execute();
-$sth1->bind_columns(\$translation_id, \$translation, \$transcript_id, \$uniprot_id, \$go_term, \$go_desc);
-my $count_query_result = 0;
-while(my @res = $sth1->fetchrow_array()) {
+foreach my $hostname (@hostnames) {
   
-  # Gene
-  my $gene;
-  $sth2->execute($transcript_id);
-  $sth2->bind_columns(\$gene);
-  $sth2->fetch();
-
-  $go_uniprot{$go_term}{'go_desc'} = $go_desc;
+  my $sth = get_connection_and_query($database, $hostname, $sql);
   
-  # UniProt
-  $gene_list{$gene}{$go_term}{'uniprot_list'}{$uniprot_id} = 1;
-  $go_uniprot{$go_term}{'uniprot_list'}{$uniprot_id} = 1;
+  # loop over databases
+  while (my ($dbname) = $sth->fetchrow_array) {
+    next if ($dbname =~ /^master_schema/);
+    next if ($dbname =~ /sample$/);
+    
+    my %gene_list;
+    my %go_uniprot;
+    my %gene_tr;
+    
+    print "# $dbname\n";
+    print STDERR "# $dbname";
+    $dbname =~ /^(.+)_variation/;
+    my $s_name = $1;
+    
+    if (%species_list) {
+      if (!$species_list{$s_name}) {
+        print STDERR " ... skipped\n";
+        next;
+      }
+      $species_found{$s_name} = 1;
+    }
+    print STDERR "\n";
+    
+    my $output_file = $output_dir."/".$s_name."_imp.txt"; 
+    
+    my $core_dbname = $dbname;
+       $core_dbname =~ s/variation/core/i;
 
-  $sth2->finish();
-  $count_query_result++;
-}
-$sth1->finish();
+    my ($translation_id, $translation, $transcript_id, $uniprot_id, $go_term, $go_desc);
+    my $sth1 = get_connection_and_query($core_dbname, $hostname, $sql1);
+    $sth1->bind_columns(\$translation_id, \$translation, \$transcript_id, \$uniprot_id, \$go_term, \$go_desc);
+    my $count_query_result = 0;
+    
+    while(my @res = $sth1->fetchrow_array()) {
+  
+      # Gene
+      my $gene = ($gene_tr{$transcript_id}{'gene'}) ? $gene_tr{$transcript_id}{'gene'} : undef;
+      my $transcript = ($gene_tr{$transcript_id}{'transcript'}) ? $gene_tr{$transcript_id}{'transcript'} : undef;
+      if (!$gene || !$transcript) {
+        my $sql2 = qq{
+          SELECT g.stable_id, t.stable_id FROM gene g, transcript t WHERE t.gene_id=g.gene_id AND t.transcript_id=$transcript_id
+        };      
+        my $sth2 = get_connection_and_query($core_dbname, $hostname, $sql2);
+        $sth2->bind_columns(\$gene, \$transcript);
+        $sth2->fetch();
+        $sth2->finish();
+        $gene_tr{$transcript_id} = {'gene' => $gene, 'transcript' => $transcript};
+      }
+      
+      next if (!$gene && !$transcript);
+
+      $go_uniprot{$go_term}{'go_desc'} = $go_desc;
+  
+      # Ensembl transcript
+      $gene_list{$gene}{$go_term}{'transcript_list'}{$transcript} = 1;
+  
+      # UniProt
+      $gene_list{$gene}{$go_term}{'uniprot_list'}{$uniprot_id} = 1;
+      $go_uniprot{$go_term}{'uniprot_list'}{$uniprot_id} = 1;
+
+      $count_query_result++;
+    }
+
+    $sth1->finish();
 
 
-######
-# 1 - Run web services
-# 2 - Parse results (search results with PMIDs)
-# 3 - Export data in a tabulated datafile
-# 4 - Import the file using the script "import_phenotype_data.pl"
-######
-my $http = HTTP::Tiny->new();
-my $server = 'http://www.ebi.ac.uk/QuickGO/GAnnotation?format=tsv&goid=%s&evidence=%s&protein=%s';
+    if (scalar(keys(%go_uniprot)) == 0 || !%go_uniprot) {
+      print STDERR "No data\n";
+      next;
+    }
+    
+    print STDERR "Count query results = $count_query_result\n";
+    
+    ######
+    # 1 - Run web services
+    # 2 - Parse results (search results with PMIDs)
+    # 3 - Export data in a tabulated datafile
+    # 4 - Import the file using the script "import_phenotype_data.pl"
+    ######
+    my $http = HTTP::Tiny->new();
 
-foreach my $go (keys(%go_uniprot)) {
-  foreach my $uniprot (keys(%{$go_uniprot{$go}{'uniprot_list'}})) {
-    #my $rest_url_with_params = "goid=$go&evidence=$ens_linkage&protein=$uniprot";
-    my $rest_url_with_params = sprintf($rest_url, $go, $ens_linkage, $uniprot);
-    my $response = $http->get($rest_url_with_params);
+    foreach my $go (keys(%go_uniprot)) {
+      foreach my $uniprot (keys(%{$go_uniprot{$go}{'uniprot_list'}})) {
+        my $rest_url_with_params = sprintf($rest_url, $go, $ens_linkage, $uniprot);
+        my $response = $http->get($rest_url_with_params);
        
-    die "Failed for GO: $go and UniProt: $uniprot! ($server.$ext)\n" unless $response->{success};
+        die "Failed for GO: $go and UniProt: $uniprot! ($rest_url_with_params)\n" unless $response->{success};
     
-    my $pmids = parse_result($response->{content});
+        my $pmids = parse_result($response->{content});
     
-    $go_uniprot{$go}{'uniprot_list'}{$uniprot} = join(',',@{$pmids});
+        $go_uniprot{$go}{'uniprot_list'}{$uniprot} = join(',',@{$pmids});
+      }
+    }
+
+    
+    open OUT, "> $output_file" or die $!;
+    print OUT "Gene_ID\tGO_term\tGO_description\tUniProt_IDs\tPMIDs\n";
+    my $count_rows = 0;
+    foreach my $gene (keys(%gene_list)) {
+      foreach my $go_term (keys(%{$gene_list{$gene}})) {
+        my %uniprot_ids;
+        my %pmids;
+        my $go_desc = $go_uniprot{$go_term}{'go_desc'};
+        foreach my $uniprot (keys(%{$gene_list{$gene}{$go_term}{'uniprot_list'}})) {
+      
+          next if (!$go_uniprot{$go_term}{'uniprot_list'}{$uniprot});
+          
+          $uniprot_ids{$uniprot} = 1;
+          
+          if ($go_uniprot{$go_term}{'uniprot_list'}{$uniprot} != 1) {
+            my @pubmeds_list = split(',',$go_uniprot{$go_term}{'uniprot_list'}{$uniprot});
+            $pmids{$_} = 1 for @pubmeds_list;
+          }
+        }
+        
+        if (scalar keys(%uniprot_ids)) {
+          print OUT "$gene\t$go_term\t$go_desc\t".join(',',keys(%uniprot_ids))."\t".join(',',keys(%pmids))."\n";
+          $count_rows++;
+          print STDERR "$count_rows entries done\n" if ($count_rows =~ /^\d+000$/);
+        }
+      }
+    }
+    close(OUT);
+
+    print STDERR "Count rows = $count_rows\n";
   }
 }
 
-open OUT, "> $output_file" or die $!;
-print OUT "Gene_ID\tGO_term\tGO_description\tUniProt_ID\tPMID(s)\n";
-my $count_rows = 0;
-foreach my $gene (keys(%gene_list)) {
-  foreach my $go_term (keys(%{$gene_list{$gene}})) {
-    foreach my $uniprot (keys(%{$gene_list{$gene}{$go_term}{'uniprot_list'}})) {
-    
-      next if (!$go_uniprot{$go_term}{'uniprot_list'}{$uniprot});
-      
-      my $go_desc = $go_uniprot{$go_term}{'go_desc'};
-      my $pmids = ($go_uniprot{$go_term}{'uniprot_list'}{$uniprot} == 1) ? '' : $go_uniprot{$go_term}{'uniprot_list'}{$uniprot};
-      
-      print OUT "$gene\t$go_term\t$go_desc\t$uniprot\t$pmids\n";
-      $count_rows++;
+# List the species not found
+if (%species_list) {
+  if (scalar(keys(%species_list)) != scalar(keys(%species_found))) {
+    foreach my $sp (keys(%species_list)) {
+      print STDERR "Species '$sp' not found in $hlist\n" if (!$species_found{$sp});
     }
   }
 }
-close(OUT);
-
-print STDOUT "Count query results = $count_query_result\n";
-print STDOUT "Count rows = $count_rows\n";
-
 
 sub parse_result {
   my $content = shift;
@@ -177,26 +263,48 @@ sub parse_result {
   return \@pmids_list;
 }
 
+
+# Connects and execute a query
+sub get_connection_and_query {
+  my $dbname = shift;
+  my $hname  = shift;
+  my $sql    = shift;
+  
+  my ($host, $port) = split /\:/, $hname;
+  
+  # DBI connection 
+  my $dsn = "DBI:mysql:$dbname:$host:$port";
+  my $dbh = DBI->connect($dsn, $user, $pswd) or die "Connection failed";
+
+  my $sth = $dbh->prepare($sql);
+  $sth->execute;
+  return $sth;
+}
+
+
 sub usage {
   my $msg = shift;
      $msg ||= '';
   
   print qq{$msg
   
-  Usage: perl get_go-imp_data.pl [OPTION]
+  Usage: perl get_all_goa-imp_data.pl [OPTION]
   
   Fetch and store the GO-IMP data into a tabulated text file. This file will be used to import the data in the Variation
   database, using the script "import_phenotype_data.pl" with the option "-source go-imp"
   
   Options:
     
-      -help              Print this message
-      
-      -species       Species from which you want to get the data. (optional)
-                     The default value is 'human'.
-      -output_file   Path and name of the output text file. (Required)
-      -registry      Path to the registry file. (optional)
-                     The default value is './ensembl.registry'.
+      -help        Print this message
+
+      -v           Ensembl version, e.g. 82 (Required)
+      -output_dir  Path to the output files. (Required)
+      -hlist       The list of host names where the new databases are stored, separated by a coma,
+                   e.g. ensembldb.ensembl.org1,ensembldb.ensembl.org2 (Required)
+      -user        MySQL user name (Required)
+      -port        MySQL port. 3306 by default (optional)
+      -species     The list of species from which the GOA data will be retrieved, separated by a coma,
+                   e.g. homo_sapiens,mus_musculus. By default the script retrieves GOA data from every species (optional)
   } . "\n";
   exit(0);
 }
