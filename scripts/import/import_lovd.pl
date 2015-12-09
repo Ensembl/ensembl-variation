f7ff337c27664d2f2cc7f616461ab735c0f8d675
Laurent Gil <lgil@ebi.ac.uk>
2015-12-09 09:52:03 +0000

    Update the license

--- scripts/import/import_lovd.pl
+++ scripts/import/import_lovd.pl
@@ -1,4 +1,6 @@
-# Copyright [1999-2013] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+#!/usr/bin/env perl
+
+# Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,19 +13,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-
-
-
-=head1 CONTACT
-
-  Please email comments or questions to the public Ensembl
-  developers list at <dev@ensembl.org>.
-
-  Questions may also be sent to the Ensembl help desk at
-  <helpdesk.org>.
-
-=cut
 
 # Import LOVD data into an Ensembl variation schema database
 
