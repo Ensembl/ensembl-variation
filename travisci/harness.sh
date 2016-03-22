41f632f6a10bf4995103c26ec2a56ab96239b183
Rishi Nag <rishi@ebi.ac.uk>
2016-03-22 10:21:08 +0000

    Use the htslib command line tabix

--- travisci/harness.sh
+++ travisci/harness.sh
@@ -2,7 +2,7 @@
 
 export PERL5LIB=$PWD/bioperl-live-bioperl-release-1-2-3:$PWD/ensembl-test/modules:$PWD/ensembl/modules:$PWD/ensembl-hive/modules:$PWD/modules:$PWD/scripts/import/:$PWD/ensembl-io/modules:$PWD/ensembl-funcgen/modules
 
-export PATH=$PATH:$PWD/C_code
+export PATH=$PATH:$PWD/C_code:$PWD/htslib
 
 echo "Running test suite"
 echo "Using $PERL5LIB"
