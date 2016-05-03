42b93dfd6a255d3d9b82d0634c9b091306138526
Laurent Gil <lgil@ebi.ac.uk>
2016-05-03 10:46:39 +0100

    Dan Bolser 'making a sensible schema change' by making the 'attrib_id' auto_increment. Needed to change the patch number (c to d)

---
+++ sql/patch_84_85_d.sql
@@ -0,0 +1,4 @@
+-- Making attrib_id auto_increment
+ALTER TABLE attrib MODIFY COLUMN attrib_id int(11) unsigned auto_increment;
+
+INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_85_86_d.sql|Making attrib_id auto_increment');
