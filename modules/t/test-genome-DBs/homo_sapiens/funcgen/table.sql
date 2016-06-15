515d413995ef973fc3b1eba417bda9627c21fcca
Anja Thormann <anja@ebi.ac.uk>
2016-06-15 15:42:24 +0100

    apply patches for release 85

--- modules/t/test-genome-DBs/homo_sapiens/funcgen/table.sql
+++ modules/t/test-genome-DBs/homo_sapiens/funcgen/table.sql
@@ -172,35 +172,6 @@ CREATE TABLE `binding_matrix` (
   KEY `feature_type_idx` (`feature_type_id`)
 ) ENGINE=MyISAM  ;
 
-CREATE TABLE `cell_type` (
-  `cell_type_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(120) NOT NULL,
-  `display_label` varchar(30) NOT NULL,
-  `description` varchar(80) DEFAULT NULL,
-  `gender` enum('male','female','hermaphrodite','mixed') DEFAULT NULL,
-  `efo_id` varchar(20) DEFAULT NULL,
-  `tissue` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`cell_type_id`),
-  UNIQUE KEY `name_idx` (`name`)
-) ENGINE=MyISAM  ;
-
-CREATE TABLE `cell_type_lineage` (
-  `cell_type_id` int(10) unsigned NOT NULL,
-  `lineage_id` int(10) unsigned NOT NULL,
-  `most_specific` tinyint(1) DEFAULT NULL,
-  PRIMARY KEY (`cell_type_id`,`lineage_id`)
-) ENGINE=MyISAM ;
-
-CREATE TABLE `channel` (
-  `channel_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `experimental_chip_id` int(10) unsigned DEFAULT NULL,
-  `sample_id` varchar(20) DEFAULT NULL,
-  `dye` varchar(20) DEFAULT NULL,
-  `type` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`channel_id`),
-  KEY `experimental_chip_idx` (`experimental_chip_id`)
-) ENGINE=MyISAM ;
-
 CREATE TABLE `coord_system` (
   `coord_system_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(40) NOT NULL,
@@ -245,42 +216,47 @@ CREATE TABLE `dbfile_registry` (
   `table_id` int(10) unsigned NOT NULL,
   `table_name` varchar(32) NOT NULL,
   `path` varchar(255) NOT NULL,
-  PRIMARY KEY (`table_id`,`table_name`),
+  `file_type` enum('BAM','BAMCOV','BIGBED','BIGWIG','VCF','CRAM','DIR') NOT NULL DEFAULT 'BAM',
+  `md5sum` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`table_id`,`table_name`,`file_type`),
   UNIQUE KEY `table_id_name_path_idx` (`table_id`,`table_name`,`path`)
+) ENGINE=MyISAM ;
+
+CREATE TABLE `epigenome` (
+  `epigenome_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(120) NOT NULL,
+  `display_label` varchar(30) NOT NULL,
+  `description` varchar(80) DEFAULT NULL,
+  `production_name` varchar(120) DEFAULT NULL,
+  `gender` enum('male','female','hermaphrodite','mixed') DEFAULT NULL,
+  `ontology_accession` varchar(20) DEFAULT NULL,
+  `ontology` enum('EFO','CL') DEFAULT NULL,
+  `tissue` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`epigenome_id`),
+  UNIQUE KEY `name_idx` (`name`)
+) ENGINE=MyISAM  ;
+
+CREATE TABLE `epigenome_lineage` (
+  `epigenome_id` int(10) unsigned NOT NULL,
+  `lineage_id` int(10) unsigned NOT NULL,
+  `most_specific` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`epigenome_id`,`lineage_id`)
 ) ENGINE=MyISAM ;
 
 CREATE TABLE `experiment` (
   `experiment_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(100) DEFAULT NULL,
   `experimental_group_id` smallint(6) unsigned DEFAULT NULL,
-  `primary_design_type` varchar(30) DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  `mage_xml_id` int(10) unsigned DEFAULT NULL,
+  `control_id` int(10) unsigned DEFAULT NULL,
+  `is_control` tinyint(3) unsigned DEFAULT '0',
   `feature_type_id` int(10) unsigned NOT NULL,
-  `cell_type_id` int(10) unsigned DEFAULT NULL,
+  `epigenome_id` int(10) unsigned DEFAULT NULL,
   `archive_id` varchar(60) DEFAULT NULL,
-  `display_url` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`experiment_id`),
   UNIQUE KEY `name_idx` (`name`),
-  KEY `design_idx` (`primary_design_type`),
   KEY `experimental_group_idx` (`experimental_group_id`),
   KEY `feature_type_idx` (`feature_type_id`),
-  KEY `cell_type_idx` (`cell_type_id`)
-) ENGINE=MyISAM ;
-
-CREATE TABLE `experimental_chip` (
-  `experimental_chip_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `unique_id` varchar(20) NOT NULL,
-  `experiment_id` int(10) unsigned DEFAULT NULL,
-  `array_chip_id` int(10) unsigned DEFAULT NULL,
-  `feature_type_id` int(10) unsigned DEFAULT NULL,
-  `cell_type_id` int(10) unsigned DEFAULT NULL,
-  `biological_replicate` varchar(100) DEFAULT NULL,
-  `technical_replicate` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`experimental_chip_id`),
-  KEY `experiment_idx` (`experiment_id`),
-  KEY `feature_type_idx` (`feature_type_id`),
-  KEY `unique_id_idx` (`unique_id`)
+  KEY `epigenome_idx` (`epigenome_id`)
 ) ENGINE=MyISAM ;
 
 CREATE TABLE `experimental_group` (
@@ -296,14 +272,14 @@ CREATE TABLE `experimental_group` (
 ) ENGINE=MyISAM ;
 
 CREATE TABLE `external_db` (
-  `external_db_id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+  `external_db_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `db_name` varchar(100) NOT NULL,
   `db_release` varchar(255) DEFAULT NULL,
   `status` enum('KNOWNXREF','KNOWN','XREF','PRED','ORTH','PSEUDO') NOT NULL,
   `dbprimary_acc_linkable` tinyint(1) NOT NULL DEFAULT '1',
   `priority` int(11) NOT NULL,
   `db_display_name` varchar(255) DEFAULT NULL,
-  `type` enum('ARRAY','ALT_TRANS','MISC','LIT','PRIMARY_DB_SYNONYM','ENSEMBL') DEFAULT NULL,
+  `type` enum('ARRAY','ALT_TRANS','ALT_GENE','MISC','LIT','PRIMARY_DB_SYNONYM','ENSEMBL') DEFAULT NULL,
   `secondary_db_name` varchar(255) DEFAULT NULL,
   `secondary_db_table` varchar(255) DEFAULT NULL,
   `description` text,
@@ -339,7 +315,7 @@ CREATE TABLE `feature_set` (
   `feature_set_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `feature_type_id` int(10) unsigned NOT NULL,
   `analysis_id` smallint(5) unsigned NOT NULL,
-  `cell_type_id` int(10) unsigned DEFAULT NULL,
+  `epigenome_id` int(10) unsigned DEFAULT NULL,
   `name` varchar(100) DEFAULT NULL,
   `type` enum('annotated','regulatory','external','segmentation','mirna_target') DEFAULT NULL,
   `description` varchar(80) DEFAULT NULL,
@@ -348,8 +324,8 @@ CREATE TABLE `feature_set` (
   PRIMARY KEY (`feature_set_id`),
   UNIQUE KEY `name_idx` (`name`),
   KEY `feature_type_idx` (`feature_type_id`),
-  KEY `cell_type_idx` (`cell_type_id`),
-  KEY `experiment_idx` (`experiment_id`)
+  KEY `experiment_idx` (`experiment_id`),
+  KEY `epigenome_idx` (`epigenome_id`)
 ) ENGINE=MyISAM  ;
 
 CREATE TABLE `feature_type` (
@@ -379,35 +355,14 @@ CREATE TABLE `identity_xref` (
   PRIMARY KEY (`object_xref_id`)
 ) ENGINE=MyISAM ;
 
-CREATE TABLE `input_set` (
-  `input_set_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `experiment_id` int(10) unsigned DEFAULT NULL,
-  `feature_type_id` int(10) unsigned DEFAULT NULL,
-  `cell_type_id` int(10) unsigned DEFAULT NULL,
-  `name` varchar(100) NOT NULL,
-  `type` enum('annotated','result','segmentation','dna_methylation') DEFAULT NULL,
-  `replicate` tinyint(3) unsigned NOT NULL,
-  `analysis_id` smallint(5) unsigned NOT NULL,
-  PRIMARY KEY (`input_set_id`),
-  UNIQUE KEY `name_idx` (`name`),
-  KEY `experiment_idx` (`experiment_id`),
-  KEY `feature_type_idx` (`feature_type_id`),
-  KEY `cell_type_idx` (`cell_type_id`)
-) ENGINE=MyISAM   MAX_ROWS=100000000 AVG_ROW_LENGTH=30;
-
-CREATE TABLE `input_set_input_subset` (
-  `input_set_id` int(10) unsigned NOT NULL,
-  `input_subset_id` int(10) unsigned NOT NULL,
-  UNIQUE KEY `iset_subset_table_idx` (`input_subset_id`,`input_set_id`)
-) ENGINE=MyISAM ;
-
 CREATE TABLE `input_subset` (
   `input_subset_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `cell_type_id` int(10) unsigned DEFAULT NULL,
+  `epigenome_id` int(10) unsigned DEFAULT NULL,
   `experiment_id` int(10) unsigned NOT NULL,
   `feature_type_id` int(10) unsigned NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `replicate` tinyint(3) unsigned NOT NULL,
+  `name` varchar(300) NOT NULL,
+  `biological_replicate` tinyint(3) unsigned NOT NULL DEFAULT '1',
+  `technical_replicate` tinyint(3) unsigned NOT NULL DEFAULT '1',
   `is_control` tinyint(3) unsigned NOT NULL,
   `analysis_id` smallint(5) unsigned NOT NULL,
   PRIMARY KEY (`input_subset_id`),
@@ -425,12 +380,6 @@ CREATE TABLE `lineage` (
   UNIQUE KEY `name_idx` (`name`),
   UNIQUE KEY `efo_idx` (`efo_id`),
   KEY `parent_linage_idx` (`parent_lineage_id`)
-) ENGINE=MyISAM ;
-
-CREATE TABLE `mage_xml` (
-  `mage_xml_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `xml` text,
-  PRIMARY KEY (`mage_xml_id`)
 ) ENGINE=MyISAM ;
 
 CREATE TABLE `meta` (
@@ -497,7 +446,7 @@ CREATE TABLE `motif_feature` (
 CREATE TABLE `object_xref` (
   `object_xref_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `ensembl_id` int(10) unsigned NOT NULL,
-  `ensembl_object_type` enum('AnnotatedFeature','Experiment','ExternalFeature','FeatureType','MirnaTargetFeature','Probe','ProbeFeature','ProbeSet','RegulatoryFeature') NOT NULL,
+  `ensembl_object_type` enum('Epigenome','Experiment','RegulatoryFeature','ExternalFeature','AnnotatedFeature','FeatureType','MirnaTargetFeature','ProbeSet','Probe','ProbeFeature') NOT NULL,
   `xref_id` int(10) unsigned NOT NULL,
   `linkage_annotation` varchar(255) DEFAULT NULL,
   `analysis_id` smallint(5) unsigned NOT NULL,
@@ -554,92 +503,76 @@ CREATE TABLE `probe_set` (
   KEY `name` (`name`)
 ) ENGINE=MyISAM ;
 
-CREATE TABLE `regbuild_string` (
-  `regbuild_string_id` int(10) NOT NULL AUTO_INCREMENT,
-  `name` varchar(150) NOT NULL,
-  `species_id` smallint(5) unsigned NOT NULL DEFAULT '1',
-  `string` text NOT NULL,
-  PRIMARY KEY (`regbuild_string_id`),
-  UNIQUE KEY `name_species_idx` (`species_id`,`name`)
-) ENGINE=MyISAM ;
+CREATE TABLE `regulatory_activity` (
+  `regulatory_activity_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `regulatory_feature_id` int(10) unsigned DEFAULT NULL,
+  `activity` enum('INACTIVE','REPRESSED','POISED','ACTIVE','NA') DEFAULT NULL,
+  `epigenome_id` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`regulatory_activity_id`),
+  UNIQUE KEY `uniqueness_constraint_idx` (`epigenome_id`,`regulatory_feature_id`),
+  KEY `regulatory_feature_idx` (`regulatory_feature_id`)
+) ENGINE=MyISAM  ;
 
-CREATE TABLE `regulatory_attribute` (
-  `regulatory_feature_id` int(10) unsigned NOT NULL,
+CREATE TABLE `regulatory_build` (
+  `regulatory_build_id` int(4) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `version` varchar(50) DEFAULT NULL,
+  `initial_release_date` varchar(50) DEFAULT NULL,
+  `last_annotation_update` varchar(50) DEFAULT NULL,
+  `feature_type_id` int(4) unsigned NOT NULL,
+  `analysis_id` int(4) unsigned NOT NULL,
+  `is_current` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`regulatory_build_id`)
+) ENGINE=MyISAM  ;
+
+CREATE TABLE `regulatory_evidence` (
+  `regulatory_activity_id` int(10) unsigned NOT NULL,
   `attribute_feature_id` int(10) unsigned NOT NULL,
   `attribute_feature_table` enum('annotated','motif') NOT NULL DEFAULT 'annotated',
-  PRIMARY KEY (`regulatory_feature_id`,`attribute_feature_table`,`attribute_feature_id`),
+  PRIMARY KEY (`regulatory_activity_id`,`attribute_feature_table`,`attribute_feature_id`),
   KEY `attribute_feature_idx` (`attribute_feature_id`,`attribute_feature_table`)
-) ENGINE=MyISAM  MAX_ROWS=100000000;
+) ENGINE=MyISAM ;
 
 CREATE TABLE `regulatory_feature` (
   `regulatory_feature_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `feature_type_id` int(10) unsigned DEFAULT NULL,
   `seq_region_id` int(10) unsigned NOT NULL,
+  `seq_region_strand` tinyint(1) NOT NULL,
   `seq_region_start` int(10) unsigned NOT NULL,
   `seq_region_end` int(10) unsigned NOT NULL,
-  `seq_region_strand` tinyint(1) NOT NULL,
-  `display_label` varchar(80) DEFAULT NULL,
-  `feature_type_id` int(10) unsigned DEFAULT NULL,
-  `feature_set_id` int(10) unsigned DEFAULT NULL,
-  `stable_id` varchar(128) DEFAULT NULL,
-  `binary_string` varchar(500) DEFAULT NULL,
-  `projected` tinyint(1) DEFAULT '0',
+  `stable_id` varchar(18) DEFAULT NULL,
   `bound_start_length` mediumint(3) unsigned NOT NULL,
   `bound_end_length` mediumint(3) unsigned NOT NULL,
-  `activity` tinyint(1) DEFAULT NULL,
-  `cell_type_count` smallint(6) DEFAULT NULL,
+  `epigenome_count` smallint(6) DEFAULT NULL,
+  `regulatory_build_id` int(10) unsigned DEFAULT NULL,
   PRIMARY KEY (`regulatory_feature_id`),
-  UNIQUE KEY `fset_seq_region_idx` (`feature_set_id`,`seq_region_id`,`seq_region_start`,`feature_type_id`),
+  UNIQUE KEY `uniqueness_constraint_idx` (`feature_type_id`,`seq_region_id`,`seq_region_strand`,`seq_region_start`,`seq_region_end`,`stable_id`,`bound_start_length`,`bound_end_length`,`regulatory_build_id`),
   KEY `feature_type_idx` (`feature_type_id`),
   KEY `stable_id_idx` (`stable_id`)
-) ENGINE=MyISAM   MAX_ROWS=100000000;
-
-CREATE TABLE `result` (
-  `result_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `probe_id` int(10) unsigned DEFAULT NULL,
-  `score` double DEFAULT NULL,
-  `result_set_input_id` int(10) unsigned NOT NULL,
-  `X` smallint(4) unsigned DEFAULT NULL,
-  `Y` smallint(4) unsigned DEFAULT NULL,
-  PRIMARY KEY (`result_id`),
-  KEY `probe_idx` (`probe_id`),
-  KEY `result_set_input_idx` (`result_set_input_id`)
-) ENGINE=MyISAM  MAX_ROWS=100000000;
-
-CREATE TABLE `result_feature` (
-  `result_feature_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `result_set_id` int(10) unsigned NOT NULL,
-  `seq_region_id` int(10) unsigned NOT NULL,
-  `seq_region_start` int(10) NOT NULL,
-  `seq_region_end` int(10) NOT NULL,
-  `seq_region_strand` tinyint(4) NOT NULL,
-  `scores` longblob NOT NULL,
-  PRIMARY KEY (`result_feature_id`),
-  KEY `set_seq_region_idx` (`result_set_id`,`seq_region_id`,`seq_region_start`)
-) ENGINE=MyISAM ;
+) ENGINE=MyISAM  ;
 
 CREATE TABLE `result_set` (
   `result_set_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `analysis_id` smallint(5) unsigned NOT NULL,
   `name` varchar(100) DEFAULT NULL,
-  `cell_type_id` int(10) unsigned DEFAULT NULL,
+  `epigenome_id` int(10) unsigned DEFAULT NULL,
   `feature_type_id` int(10) unsigned DEFAULT NULL,
   `feature_class` enum('result','dna_methylation','segmentation') DEFAULT NULL,
-  `replicate` tinyint(3) unsigned NOT NULL,
   `experiment_id` int(10) unsigned DEFAULT NULL,
   PRIMARY KEY (`result_set_id`),
   UNIQUE KEY `name_idx` (`name`),
-  KEY `cell_type_idx` (`cell_type_id`),
   KEY `feature_type_idx` (`feature_type_id`),
   KEY `analysis_idx` (`analysis_id`),
   KEY `feature_class_idx` (`feature_class`),
-  KEY `experiment_idx` (`experiment_id`)
+  KEY `experiment_idx` (`experiment_id`),
+  KEY `epigenome_idx` (`epigenome_id`)
 ) ENGINE=MyISAM ;
 
 CREATE TABLE `result_set_input` (
   `result_set_input_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `result_set_id` int(10) unsigned NOT NULL,
   `table_id` int(10) unsigned NOT NULL,
-  `table_name` enum('experimental_chip','channel','input_set','input_subset') DEFAULT NULL,
+  `table_name` enum('input_subset') DEFAULT NULL,
   PRIMARY KEY (`result_set_input_id`,`result_set_id`),
   UNIQUE KEY `rset_table_idname_idx` (`result_set_id`,`table_id`,`table_name`)
 ) ENGINE=MyISAM ;
@@ -696,7 +629,7 @@ CREATE TABLE `unmapped_object` (
   `unmapped_object_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `type` enum('xref','probe2transcript','array_mapping') NOT NULL,
   `analysis_id` smallint(5) unsigned NOT NULL,
-  `external_db_id` smallint(5) unsigned DEFAULT NULL,
+  `external_db_id` int(10) unsigned DEFAULT NULL,
   `identifier` varchar(255) NOT NULL,
   `unmapped_reason_id` smallint(5) unsigned NOT NULL,
   `query_score` double DEFAULT NULL,
@@ -719,12 +652,12 @@ CREATE TABLE `unmapped_reason` (
 
 CREATE TABLE `xref` (
   `xref_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `external_db_id` smallint(5) unsigned NOT NULL,
-  `dbprimary_acc` varchar(40) NOT NULL,
-  `display_label` varchar(128) NOT NULL,
-  `version` varchar(10) NOT NULL DEFAULT '0',
-  `description` varchar(255) DEFAULT NULL,
-  `info_type` enum('PROJECTION','MISC','DEPENDENT','DIRECT','SEQUENCE_MATCH','INFERRED_PAIR','PROBE','UNMAPPED','CODING','TARGET') NOT NULL,
+  `external_db_id` int(10) unsigned DEFAULT NULL,
+  `dbprimary_acc` varchar(512) NOT NULL,
+  `display_label` varchar(512) NOT NULL,
+  `version` varchar(10) DEFAULT NULL,
+  `description` text,
+  `info_type` enum('NONE','PROJECTION','MISC','DEPENDENT','DIRECT','SEQUENCE_MATCH','INFERRED_PAIR','PROBE','UNMAPPED','COORDINATE_OVERLAP','CHECKSUM') NOT NULL DEFAULT 'NONE',
   `info_text` varchar(255) NOT NULL DEFAULT '',
   PRIMARY KEY (`xref_id`),
   UNIQUE KEY `id_index` (`dbprimary_acc`,`external_db_id`,`info_type`,`info_text`,`version`),
