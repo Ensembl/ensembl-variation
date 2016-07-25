015e5858fe0b758eb9e68ba33d88e6cc2796e715
Sarah Hunt <seh@ebi.ac.uk>
2016-07-25 11:33:52 +0100

    schema version & phenotype_onology_accession updates

---
+++ sql/patch_85_86_b.sql
@@ -0,0 +1,9 @@
+-- add qualifier & index to phenotype_onology_accession
+
+ALTER TABLE phenotype_ontology_accession add index accession_idx(accession);
+
+ALTER TABLE phenotype_ontology_accession CHANGE column linked_by_attrib mapped_by_attrib set('437','438','439','440','441','442','443','444') ;
+
+ALTER TABLE phenotype_ontology_accession add column ( mapping_type enum('is','involves') default NULL ) ;
+
+INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_84_85_b.sql|add qualifier & index to phenotype_onology_accession');
