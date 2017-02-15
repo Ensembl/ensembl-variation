4eb982ef4d4d0a67dcb419839a5aa5d64b37f8f2
Sarah Hunt <seh@ebi.ac.uk>
2017-02-15 15:38:55 +0000

    fix ontology accession

--- scripts/import/import_phenotype_data.pl
+++ scripts/import/import_phenotype_data.pl
@@ -1568,6 +1568,9 @@ sub parse_cancer_gene_census {
     my $accession = $row_data[5];
     my $pmids     = $row_data[6];
 
+    ## change accession format
+    $accession =~ s/\_/\:/;
+
     my $gene = $ga->fetch_by_stable_id($gene_id);
 
     if(!$gene) {
