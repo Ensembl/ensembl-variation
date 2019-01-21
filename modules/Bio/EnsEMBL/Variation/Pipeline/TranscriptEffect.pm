b706e927d27db9b95691f6796987cbc543da13b9
at7 <anja@ebi.ac.uk>
2019-01-21 13:22:16 +0000

    divide into two modes by gene and by transcript

--- modules/Bio/EnsEMBL/Variation/Pipeline/TranscriptEffect.pm
+++ modules/Bio/EnsEMBL/Variation/Pipeline/TranscriptEffect.pm
@@ -37,10 +37,9 @@ use Bio::EnsEMBL::Variation::TranscriptVariation;
 use Bio::EnsEMBL::Variation::Utils::VariationEffect qw(MAX_DISTANCE_FROM_TRANSCRIPT overlap);
 use Bio::EnsEMBL::Variation::Utils::FastaSequence qw(setup_fasta);
 
-use ImportUtils qw(load);
 use FileHandle;
+use ImportUtils qw(load);
 use Fcntl qw(:flock SEEK_END);
-use Digest::MD5 qw(md5_hex);
 
 use base qw(Bio::EnsEMBL::Variation::Pipeline::BaseVariationProcess);
 
@@ -49,32 +48,17 @@ my $DEBUG   = 0;
 sub run {
   my $self = shift;
 
-  my $gene_id = $self->required_param('gene_stable_id'); 
-
   my $disambiguate_sn_alleles = 
     $self->param('disambiguate_single_nucleotide_alleles');
-
   my $mtmp = $self->param('mtmp_table');
-  
+  my $max_distance = $self->param('max_distance');
+  my $by_transcript = ($self->param('analysis') eq 'by_transcript') ? 1 : 0;
+
   my $variations_to_include;
-  
   # if (my $vars = $self->param('variations_to_include')) {
   #   # turn the list of variation names into a hash to speed up checking
   #   $variations_to_include = { map { $_ => 1 } @$vars };
   # }
-
-  # set max distance
-  my $opt_max_distance = $self->param('max_distance');
-  my $max_distance;
-
-  if(defined($opt_max_distance)) {
-    $max_distance = $opt_max_distance;
-    $Bio::EnsEMBL::Variation::Utils::VariationEffect::UPSTREAM_DISTANCE = $opt_max_distance;
-    $Bio::EnsEMBL::Variation::Utils::VariationEffect::DOWNSTREAM_DISTANCE = $opt_max_distance;
-  }
-  else {
-    $max_distance = MAX_DISTANCE_FROM_TRANSCRIPT;
-  }
 
   # clear the registry here
   # this hopefully prevents any sequence caching issues
@@ -82,25 +66,18 @@ sub run {
   Bio::EnsEMBL::Registry->clear();
 
   my $core_dba = $self->get_species_adaptor('core');
-  $core_dba->dbc->reconnect_when_lost(1);
 
   my $var_dba = $self->get_species_adaptor('variation');
-  $var_dba->dbc->reconnect_when_lost(1);
   
-  my $ga = $core_dba->get_GeneAdaptor;
   my $sa = $core_dba->get_SliceAdaptor;
   
   my $tva = $var_dba->get_TranscriptVariationAdaptor;
-
-  # we need to include failed variations
   $tva->db->include_failed_variations(1);
 
   if((my $fasta = $self->param('fasta')) && !$Bio::EnsEMBL::Slice::_fasta_redefined && !$Bio::EnsEMBL::Slice::fasta_db) {
-
     # we need to find the assembly version to tell it about PARs
     my ($highest_cs) = @{$core_dba->get_CoordSystemAdaptor->fetch_all()};
     my $assembly = $highest_cs->version();
-
     setup_fasta(-FASTA => $fasta, -ASSEMBLY => $assembly);
   }
   else {
@@ -108,90 +85,63 @@ sub run {
     # this prevents repeated DB lookups for sequence for HGVS e.g. in long introns
     # to do this we fetch a sequence adaptor
     my $seq_ad = $core_dba->get_SequenceAdaptor;
-
     # then reset its cache
     # the first param passed to this method is the "chunk power"
     # essentially the length in bp of cached sequence will be 2 ^ chunk_power
     $seq_ad->_init_seq_instance($seq_ad->chunk_power + 2);
   }
 
-  print STDERR "Fetching gene $gene_id\n" if $DEBUG;
-
-  my $gene = $ga->fetch_by_stable_id($gene_id) 
-    or die "failed to fetch gene for stable id: $gene_id";
-
-  my $gene_name;
-  $gene_name = $gene->display_xref->display_id if defined $gene->display_xref();
-
-  my $slice = $sa->fetch_by_gene_stable_id(
-    $gene->stable_id, 
-    $max_distance
-  ) or die "failed to get slice around gene: ".$gene->stable_id;
-  
-  # call seq here to help cache
-  $slice->seq();
-
-  $gene = $gene->transfer($slice);
-
-  print STDERR "Getting variation features\n" if $DEBUG;
+  my $slice;
+  my $stable_id;
+  my @transcripts = ();
+  if ($by_transcript) {
+    my $transcript_stable_id = $self->param('transcript_stable_id');
+    $stable_id = $transcript_stable_id;
+    my $ta = $core_dba->get_TranscriptAdaptor;
+    my $transcript = $ta->fetch_by_stable_id($transcript_stable_id) 
+      or die "failed to fetch transcript for stable id: $transcript_stable_id";
+    $slice = $sa->fetch_by_transcript_stable_id(
+      $transcript_stable_id, 
+      $max_distance
+    ) or die "failed to get slice around transcript: $transcript_stable_id";
+    # call seq here to help cache
+    $slice->seq();
+    $transcript = $transcript->transfer($slice);
+    push @transcripts, $transcript;
+  } else {
+    my $gene_stable_id =  $self->param('gene_stable_id');
+    $stable_id = $gene_stable_id;
+    my $ga = $core_dba->get_GeneAdaptor;
+    my $gene = $ga->fetch_by_stable_id($gene_stable_id) 
+      or die "failed to fetch gene for stable id: $gene_stable_id";
+    $slice = $sa->fetch_by_gene_stable_id(
+      $gene_stable_id, 
+      $max_distance
+    ) or die "failed to get slice around gene: $gene_stable_id";
+    # call seq here to help cache
+    $slice->seq();
+    $gene = $gene->transfer($slice);
+    push @transcripts, @{ $gene->get_all_Transcripts };
+  }
 
   my $vfa = $var_dba->get_VariationFeatureAdaptor();
-
   my @vfs = (
     @{ $vfa->fetch_all_by_Slice_SO_terms($slice) },
     @{ $vfa->fetch_all_somatic_by_Slice_SO_terms($slice) }
   );
-
-  my $table_files_dir = $self->get_table_files_prefix($gene_id);
-
-  print STDERR "STUB $table_files_dir\n";
-
-  # write VFs to table file for web search indexes
-  if($gene_name) {
-    my $fh = FileHandle->new();
-    $fh->open(">".$table_files_dir."_variation_genename.txt") or die "Cannot open dump file ".$table_files_dir."_variation_genename.txt: $!";
-    print $fh "$_\t$gene_name\n" for map {$_->get_Variation_dbID()} @vfs;
-    $fh->close();
-  }
+  my $table_files_dir = $self->get_table_files_prefix($stable_id, $tva);
 
   # get a fh for the hgvs file too
   my $hgvs_fh = FileHandle->new();
   $hgvs_fh->open(">".$table_files_dir."_variation_hgvs.txt") or die "Cannot open dump file ".$table_files_dir."_variation_hgvs.txt: $!";
 
-
-  my @write_data;
-  my $t0;
-
+#  my @write_data;
   my $hgvs_by_var = {};
   my $var_count = 0;
 
-  # initialise a hash of files
-  my $files = {
-    transcript_variation      => { 'cols' => [$tva->_write_columns],      },
-    MTMP_transcript_variation => { 'cols' => [$tva->_mtmp_write_columns], },
-    # variation_genename        => { 'cols' => [qw(variation_id gene_name)] },
-    # variation_hgvs            => { 'cols' => [qw(variation_id hgvs_name)] },
-  };
+  my $files = $self->get_dump_files($stable_id, $tva);
 
-  # create filenames, file handles etc
-  my $tmpdir = '/tmp';
-  $ImportUtils::TMP_DIR = $tmpdir;
-
-  # create file handles
-  for my $table(keys %$files) {
-    
-    my $hash = $files->{$table};
-
-    $hash->{filename} = sprintf('%i_%s_%s.txt', $$, $gene->stable_id, $table);
-    $hash->{filepath} = sprintf('%s/%s', $tmpdir, $hash->{filename});
-
-    my $fh = FileHandle->new();
-    $fh->open(">".$hash->{filepath}) or throw("ERROR: Could not write to ".$hash->{filepath});
-    $hash->{fh} = $fh;
-  }
-
-  for my $transcript (@{ $gene->get_all_Transcripts }) {
-
+  for my $transcript (@transcripts) {
     for my $vf(@vfs) {
 
       if (defined $variations_to_include) {
@@ -278,11 +228,15 @@ sub run {
   ## end block
 
   ## uncomment this if using BEHAVIOUR 3 above
+  ## LOADING HAS BEEN MOVED TO FinishTranscriptEffect.pm if run in by_transcript analysis mode
   foreach my $table(keys %$files) {
-    print STDERR "Importing data to $table\n" if $DEBUG;
-    $ImportUtils::TMP_FILE = $files->{$table}->{filename};
     $files->{$table}->{fh}->close();
-    load($var_dba->dbc, ($table, @{$files->{$table}->{cols}}));
+    if (!$by_transcript) {
+      my $tmpdir = $self->param('pipeline_dir');
+      $ImportUtils::TMP_DIR = $tmpdir;
+      $ImportUtils::TMP_FILE = $files->{$table}->{filename};
+      load($var_dba->dbc, ($table, @{$files->{$table}->{cols}}));
+    }
   }
   ## end block
 
@@ -300,19 +254,6 @@ sub write_output {
   my $self = shift;
 }
 
-sub get_table_files_prefix {
-  my $self = shift;
-  my $id = shift;
-
-  my $dir = $self->required_param('pipeline_dir').'/table_files/'.substr(md5_hex($id), 0, 2);
-
-  unless(-d $dir) {
-    mkdir($dir) or die "ERROR: Could not make directory $dir\n";
-  }
-
-  return $dir.'/'.$id;
-}
-
 sub dump_hgvs_var {
   my ($self, $hgvs_by_var, $fh) = @_;
 
@@ -322,5 +263,28 @@ sub dump_hgvs_var {
     }
   }
 }
+
+sub get_dump_files {
+  my $self = shift;
+  my $stable_id = shift;
+  my $tva = shift;
+  # initialise a hash of files
+  my $files = {
+    transcript_variation      => { 'cols' => [$tva->_write_columns],      },
+    MTMP_transcript_variation => { 'cols' => [$tva->_mtmp_write_columns], },
+  };
+  my $tmpdir = $self->param('pipeline_dir');
+  # create file handles
+  for my $table(keys %$files) {
+    my $hash = $files->{$table};
+    $hash->{filename} = sprintf('%s_%s.txt', $stable_id, $table);
+    $hash->{filepath} = sprintf('%s/%s', $tmpdir, $hash->{filename});
+    my $fh = FileHandle->new();
+    $fh->open(">".$hash->{filepath}) or throw("ERROR: Could not write to ".$hash->{filepath});
+    $hash->{fh} = $fh;
+  }
+  return $files;
+}
+
 
 1;
