33116029518da8ef8e91edcce4f72c71fa77fcd4
at7 <anja@ebi.ac.uk>
2019-01-21 13:21:15 +0000

    repurpose module. Only load all transcript files

--- modules/Bio/EnsEMBL/Variation/Pipeline/FinishTranscriptEffect.pm
+++ modules/Bio/EnsEMBL/Variation/Pipeline/FinishTranscriptEffect.pm
@@ -32,97 +32,43 @@ package Bio::EnsEMBL::Variation::Pipeline::FinishTranscriptEffect;
 use strict;
 use warnings;
 use ImportUtils qw(load);
-use Sys::Hostname;
 use FileHandle;
-use File::Path qw(rmtree);
 
 use base qw(Bio::EnsEMBL::Variation::Pipeline::BaseVariationProcess);
-
-my $DEBUG = 0;
 
 sub run {
   my $self = shift;
 
-  $self->rejoin_table_files();
+  my @transcript_ids = @{$self->param('transcript_ids_funnel')};
 
-  my $dbc = $self->get_species_adaptor('variation')->dbc;
+  my $var_dba = $self->get_species_adaptor('variation');
+  my $tva = $var_dba->get_TranscriptVariationAdaptor;
 
-  my $dir = $self->required_param('pipeline_dir');
-  $ImportUtils::TMP_DIR = $dir;
+ # initialise a hash of files
+  my $files = {
+    transcript_variation      => { 'cols' => [$tva->_write_columns],      },
+    MTMP_transcript_variation => { 'cols' => [$tva->_mtmp_write_columns], },
+  };
 
-  my $host = hostname;
-
-  # do unique sort on command line, it's faster than relying on MySQL's unique index
-  foreach my $file(grep {-e "$dir/$_"} qw(variation_hgvs.txt variation_genename.txt)) {
-    system("gzip -c $dir/$file > $dir/$file\_bak.gz");
-    system(
-      sprintf(
-        'cat %s/%s | sort -T %s -u > %s/%s.unique',
-        $dir, $file, $dir, $dir, $file, 
-      )
-    ) and die("ERROR: Failed to unique sort $file");
-    unlink("$dir/$file\.gz") if -e "$dir/$file\.gz";
-    system("gzip $dir/$file");# unlink("$dir/$file");
+  # create filenames, file handles etc
+  my $tmpdir = $self->param('pipeline_dir');
+  $ImportUtils::TMP_DIR = $tmpdir;
+  # COPY FILES FIRST IN CASE OF ERROR?
+  # create file handles
+  foreach my $transcript_id (@transcript_ids) {
+    for my $table(keys %$files) {
+      my $hash = $files->{$table};
+      $hash->{filename} = sprintf('%s_%s.txt', $transcript_id, $table);
+      $hash->{filepath} = sprintf('%s/%s', $tmpdir, $hash->{filename});
+    }
+    foreach my $table(keys %$files) {
+      $ImportUtils::TMP_FILE = $files->{$table}->{filename};
+      load($var_dba->dbc, ($table, @{$files->{$table}->{cols}}));
+    }
   }
-
-  if(-e $dir.'/variation_hgvs.txt.unique') {
-    $ImportUtils::TMP_FILE = 'variation_hgvs.txt.unique';
-    load($dbc, qw(variation_hgvs variation_id hgvs_name));
-  }
-
-  if(-e $dir.'/variation_genename.txt.unique') {
-    $ImportUtils::TMP_FILE = 'variation_genename.txt.unique';
-    load($dbc, qw(variation_genename variation_id gene_name));
-  }
-
-  $self->update_meta();
 
   return;
 }
 
-sub rejoin_table_files {
-  my $self = shift;
-
-  my $dir = $self->required_param('pipeline_dir');
-
-  my $gene_fh = FileHandle->new();
-  $gene_fh->open(">".$dir."/variation_genename.txt") or die $!;
-  my $hgvs_fh = FileHandle->new();
-  $hgvs_fh->open(">".$dir."/variation_hgvs.txt") or die $!;
-
-  opendir DIR, $dir."/table_files";
-  foreach my $hex_stub(grep {!/^\./} readdir DIR) {
-
-    opendir HEX, "$dir/table_files/$hex_stub";
-    foreach my $file(grep {!/^\./} readdir HEX) {
-
-      my $fh = $file =~ /hgvs/ ? $hgvs_fh : $gene_fh;
-
-      open IN, "$dir/table_files/$hex_stub/$file" or die $!;
-      while(<IN>) {
-        print $fh $_;
-      }
-      close IN;
-    }
-  }
-
-  rmtree($dir."/table_files");
-}
-
-sub update_meta{
-
-  my $self = shift;
-
-  my $var_dba  = $self->get_species_adaptor('variation');
-
-  my $var_dbh = $var_dba->dbc->db_handle;
-
-  my $update_meta_sth = $var_dbh->prepare(qq[ insert ignore into meta
-                                              ( meta_key, meta_value) values (?,?)
-                                            ]);
-
-  $update_meta_sth->execute('TranscriptEffect_run_date', $self->run_date() );
-
-}
 1;
 
