b14fda4b595cfe26f9e86df635c809707d041fa6
dglemos <dlemos@ebi.ac.uk>
2019-02-20 17:33:34 +0000

    Support HGVS protein deletions

--- modules/Bio/EnsEMBL/Variation/DBSQL/VariationFeatureAdaptor.pm
+++ modules/Bio/EnsEMBL/Variation/DBSQL/VariationFeatureAdaptor.pm
@@ -1872,7 +1872,12 @@ sub fetch_by_hgvs_notation {
 
       my $possible_prot;
       eval {
+        if($description =~ /del$/){
+          $possible_prot = _parse_hgvs_protein_position_del($description, $reference, $transcript);
+        } 
+        else{
         $possible_prot = _parse_hgvs_protein_position($description, $reference, $transcript);
+        } 
         throw("Could not uniquely determine nucleotide change from $hgvs") if scalar @$possible_prot > 1 && !$multiple_ok;
         $slice = $slice_adaptor->fetch_by_region($transcript->coord_system_name(), $transcript->seq_region_name());
       };
@@ -2265,6 +2270,72 @@ sub _parse_hgvs_protein_position{
   #exit(0); 
 }
 
+sub _parse_hgvs_protein_position_del{
+
+  my ($description, $reference, $transcript ) = @_;
+
+  my ($from, $pos, $from2, $pos2, $to); 
+
+  # if the is a deletion of several amino acids eg. Lys1110_Gln1111del  
+  if($description =~ /_/){
+    my ($first_residue,$second_residue) = split('_', $description); 
+
+    # get the first altered amino acid and position 
+    ($from, $pos) = $first_residue =~ /^(\w+?)(\d+)$/;
+    
+    # get the last altered amino acid and position  
+    ($from2, $pos2, $to) = $second_residue =~ /^(\w+?)(\d+)(\w+?|\*)$/;
+  }
+  
+  # only one amino acid is deleted eg. Lys1110del
+  else{
+    ($from, $pos, $to) = $description =~ /^(\w+?)(\d+)(\w+?|\*)$/; 
+  } 
+  # conver three letter AA to single for both amino acids (convert first altered amino acid)  
+  $from = $Bio::SeqUtils::ONECODE{$from} || $from;
+
+  # get genomic position 
+  my $tr_mapper = $transcript->get_TranscriptMapper(); 
+
+  my @coords = defined($pos2) ? $tr_mapper->pep2genomic($pos, $pos2) : $tr_mapper->pep2genomic($pos, $pos);  
+  
+  my $strand     = $coords[0]->strand(); 
+  my $start      = $coords[0]->start(); 
+  my $end        = defined($pos2) ? $coords[0]->end() : $coords[0]->start() + 2;  
+  my $seq_length = ($end-$start) + 1; 
+  
+  ## find reference sequence 
+  my $slice = $transcript->slice();
+
+  ## make a small slice for sequence look-up
+  my $from_slice = Bio::EnsEMBL::Slice->new(-coord_system => $slice->coord_system(),
+                                              -start => $start,
+                                              -end => $end,
+                                              -strand => $slice->strand(),
+                                              -seq_region_name => $slice->seq_region_name,
+                                              -seq_region_length => $seq_length, 
+                                              -adaptor => $slice->adaptor); 
+
+  my $from_codon_ref = $from_slice->seq();  
+   throw ("Unable to find the genomic reference sequence for protein $reference") unless defined $from_codon_ref; 
+ 
+  ## correct for strand
+  reverse_comp(\$from_codon_ref) if $strand <0;
+ 
+  # get correct codon table 
+  # my $attrib = $transcript->slice->get_all_Attributes('codon_table')->[0];
+
+  # default to the vertebrate codon table which is denoted as 1 
+  # my $codon_table = Bio::Tools::CodonTable->new( -id => ($attrib ? $attrib->value : 1)); 
+
+  # check genomic codon is compatible with input HGVS
+  # my $check_prot = $codon_table->translate($from_codon_ref); 
+
+  my @results; 
+  push @results, [$from_codon_ref, '-', $start, $end, $strand];
+
+  return \@results; 
+} 
 
 =head2 fetch_by_dbID
 
