762fbae4e82926f8b7def6f615141e425bd7eadd
Irina Armean <iarmean@ebi.ac.uk>
2019-04-02 17:15:47 +0100

    update Orphanet

--- modules/Bio/EnsEMBL/Variation/Pipeline/PhenotypeAnnotation/ImportOrphanet.pm
+++ modules/Bio/EnsEMBL/Variation/Pipeline/PhenotypeAnnotation/ImportOrphanet.pm
@@ -28,6 +28,14 @@ limitations under the License.
 
 =cut
 
+
+=head1 ImportOrphanet
+
+This module imports Orphanet data. The module fetches the data from
+www.orphadata.org.
+
+=cut
+
 package Bio::EnsEMBL::Variation::Pipeline::PhenotypeAnnotation::ImportOrphanet;
 
 use warnings;
@@ -37,57 +45,59 @@ use File::Path qw(make_path);
 use LWP::Simple;
 use XML::LibXML;
 use POSIX 'strftime';
-use Data::Dumper; #TODO: remove if not needed
-use Bio::EnsEMBL::Registry;
 
 use base ('Bio::EnsEMBL::Variation::Pipeline::PhenotypeAnnotation::BasePhenotypeAnnotation');
 
 my %source_info;
 my $workdir;
+my ($logFH, $errFH);
+my %special_characters;
+
 my $core_dba;
 my $variation_dba;
-my $phenotype_dba;
-
-my %special_characters;
 
 my $debug;
 
 sub fetch_input {
-    my $self = shift;
+  my $self = shift;
 
-    my $pipeline_dir = $self->required_param('pipeline_dir');
-    my $species      = $self->required_param('species');
+  my $pipeline_dir = $self->required_param('pipeline_dir');
+  my $species      = $self->required_param('species');
 
-    $core_dba    = $self->get_species_adaptor('core');
-    $variation_dba  = $self->get_species_adaptor('variation'); #TODO: why did the init -> Base class -> this not work?
-    $phenotype_dba  = $variation_dba->get_PhenotypeAdaptor; 
+  $core_dba        = $self->get_species_adaptor('core');
+  $variation_dba   = $self->get_species_adaptor('variation');
 
-    $debug        = $self->param('debug_mode');
+  $debug        = $self->param('debug_mode');
+  $self->SUPER::set_debug($self->param('debug_mode'));
 
-    %special_characters = %{$self->get_special_characters};
-    #TODO: 
-    # original call: perl ensembl-variation/scripts/import/import_phenotype_data.pl -host ${host} -user ${user} -pass ${pass}  -dbname ${dbname} \
-    #-source nhgri -infile ${datadir}/gwascatalog.txt -verbose -version 20121031
+  %special_characters = %{$self->get_special_characters};
 
-    my $orphanet_data_url = 'http://www.orphadata.org/data/xml/en_product6.xml';
+  my $orphanet_data_url = 'http://www.orphadata.org/data/xml/en_product6.xml';
 
-    %source_info = (source_description => 'The portal for rare diseases and drugs',
-                    source_url => 'http://www.orpha.net/',
-                    object_type => 'Gene',
+  %source_info = (source_description => 'The portal for rare diseases and drugs',
+                  source_url => 'http://www.orpha.net/',
+                  object_type => 'Gene',
+                  source_status => 'germline',
+                  source_name => 'Orphanet',        #source name in the variation db
+                  source_name_short => 'Orphanet',  #source identifier in the pipeline
+                #  source => 'orphanet', TODO: remove
+                #  source_version is set based on record contained in file
+                  );
 
-                    source_name => 'Orphanet', #TODO: figure out where this is used
-                    source_status => 'germline',
-                    source => 'orphanet',
-                    );
+  #get input file Orphanet:
+  $workdir = $pipeline_dir."/".$source_info{source_name_short}."/".$species;
+  make_path($workdir);
 
-    #get input file Orphanet:
-    $workdir = $pipeline_dir."/".$source_info{source_name}."/".$species;
-    make_path($workdir);
-    my $file_orphanet = "en_product6.xml";
-    getstore($orphanet_data_url, $workdir."/".$file_orphanet) unless -e $workdir."/".$file_orphanet;
+  open ($logFH, ">", $workdir."/".'log_import_out_'.$source_info{source_name_short}.'_'.$species);
+  open ($errFH, ">", $workdir."/".'log_import_err_'.$source_info{source_name_short}.'_'.$species);
+  $self->SUPER::set_logFH($logFH);
+  $self->SUPER::set_errFH($errFH);
 
-    print "Found files (".$workdir."/".$file_orphanet."), will skip new fetch\n" if -e $workdir."/".$file_orphanet;
-    $self->param('orphanet_file', $file_orphanet);
+  my $file_orphanet = "en_product6.xml";
+  getstore($orphanet_data_url, $workdir."/".$file_orphanet) unless -e $workdir."/".$file_orphanet;
+
+  print $logFH "Found files (".$workdir."/".$file_orphanet."), will skip new fetch\n" if -e $workdir."/".$file_orphanet;
+  $self->param('orphanet_file', $file_orphanet);
 }
 
 sub run {
@@ -95,13 +105,9 @@ sub run {
 
   my $file_orphanet = $self->required_param('orphanet_file');
 
-  local (*STDOUT, *STDERR);
-  open STDOUT, ">>", $workdir."/".'log_import_out_'.$file_orphanet; #TODO: what is best error/out log naming convention?
-  open STDERR, ">>", $workdir."/".'log_import_err_'.$file_orphanet;
-
   # get phenotype data + save it (all in one method)
   my ($results,$source_date) = parse_orphanet($workdir."/".$file_orphanet, $core_dba);
-  print "Got ".(scalar @{$results->{'phenotypes'}})." phenotypes \n" if $debug ;
+  print $logFH "Got ".(scalar @{$results->{'phenotypes'}})." phenotypes \n" if $debug ;
 
   #save source_date
   $source_info{source_version} = $source_date;
@@ -109,24 +115,33 @@ sub run {
   # save phenotypes
   $self->save_phenotypes(\%source_info, $results, $core_dba, $variation_dba);
 
-  my %param_source = (source_name => $source_info{source_name},
-                      type => $source_info{object_type}); #TODO: why is type used again
+  my %param_source = (source_name => $source_info{source_name_short},
+                      type => $source_info{object_type});
   $self->param('output_ids', { source => \%param_source,
                                species => $self->required_param('species')
                              });
+  close($logFH);
+  close($errFH);
 }
 
 sub write_output {
   my $self = shift;
 
+  if ($self->param('debug_mode')) {
+    open (my $logPipeFH, ">", $workdir."/".'log_import_debug_pipe');
+    print $logPipeFH "Passing $source_info{source_name} import (".$self->required_param('species').") for checks (check_phenotypes)\n";
+    close ($logPipeFH);
+  }
   $self->dataflow_output_id($self->param('output_ids'), 1);
-  print "Passing Orphanet import for checks\n" if $self->param('debug_mode');
 }
 
 # Orphanet specific phenotype parsing method
 sub parse_orphanet {
   my $infile = shift;
   my $core_db_adaptor = shift;
+
+  my $errFH1;
+  open ($errFH1, ">", $workdir."/".'log_import_err_'.$infile) ;
 
   my $ga = $core_db_adaptor->get_GeneAdaptor;
   die("ERROR: Could not get gene adaptor") unless defined($ga);
@@ -198,7 +213,7 @@ sub parse_orphanet {
 
         if ( scalar(@$genes) > 0) {
           if(scalar @$genes != 1) {
-            print STDERR "WARNING: Found ".(scalar @$genes)." matching Ensembl genes for HGNC ID $ref or Ens IDs ".join(",", keys %ens_ids)."\n";
+            print $errFH1 "WARNING: Found ".(scalar @$genes)." matching Ensembl genes for HGNC ID $ref or Ens IDs ".join(",", keys %ens_ids)."\n";
           }
 
           next unless scalar @$genes;
@@ -220,6 +235,8 @@ sub parse_orphanet {
       }
     }
   }
+  close($errFH1);
+
   my %result = ('phenotypes' => \@phenotypes);
   return (\%result, $date);
 }
