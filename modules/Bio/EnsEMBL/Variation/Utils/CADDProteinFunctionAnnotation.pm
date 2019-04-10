041c92d8312b87625065e3b240326ff4131c083d
at7 <anja@ebi.ac.uk>
2019-04-10 12:54:50 +0100

    testable modules for cadd and dbnsfp pipeline

---
+++ modules/Bio/EnsEMBL/Variation/Utils/CADDProteinFunctionAnnotation.pm
@@ -0,0 +1,141 @@
+=head1 LICENSE
+
+Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+Copyright [2016-2019] EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=cut
+
+
+
+=head1 CONTACT
+
+ Please email comments or questions to the public Ensembl
+ developers list at <http://lists.ensembl.org/mailman/listinfo/dev>.
+
+ Questions may also be sent to the Ensembl help desk at
+ <http://www.ensembl.org/Help/Contact>.
+
+=head1 DESCRIPTION
+
+Module is used in protein function prediction pipeline for
+annotating all possible amino acid substitutions in a translation
+with CADD scores and predictions. 
+
+=cut
+
+
+use strict;
+use warnings;
+
+package Bio::EnsEMBL::Variation::Utils::CADDProteinFunctionAnnotation;
+
+use Bio::EnsEMBL::Variation::Utils::BaseProteinFunctionAnnotation;
+our @ISA = ('Bio::EnsEMBL::Variation::Utils::BaseProteinFunctionAnnotation');
+
+my $CADD_CUTOFF = 30;
+
+=head2 new
+  Example    :
+  my $cadd = Bio::EnsEMBL::Variation::Utils::CADDProteinFunctionAnnotation->new(
+    -species => 'Homo_sapiens',
+    -annotation_file => 'cadd_v1.3_grch37.txt.gz',
+    -assembly => 'GRCh37',
+    -annotation_file_version => 'v1.3',
+    -pipeline_mode => 0,
+    -debug_mode => 1,
+  );
+
+  Description: Constructor. Instantiates a new CADDProteinFunctionAnnotation object.
+  Returntype : CADDProteinFunctionAnnotation
+  Exceptions : throws on unsupported version
+  Caller     : Bio::EnsEMBL::Variation::Pipeline::ProteinFunction::RunCADD::run
+  Status     : Stable
+=cut
+
+sub new {
+  my $caller = shift;
+  my $class = ref($caller) || $caller;
+
+  my $self = $class->SUPER::new(@_);
+
+  if (! grep {$_ eq $self->annotation_file_version} ('v1.3', 'v1.4', 'v1.5')) {
+    die "CADD version " . $self->annotation_file_version . " is not supported.";
+  }
+
+  $self->analysis([qw/cadd/]);
+
+  return $self;
+}
+
+sub load_predictions_for_triplets {
+  my $self = shift;
+  my $all_triplets = shift; 
+  foreach my $entry (@$all_triplets) {
+    my $aa = $entry->{aa};
+    $self->amino_acids($aa);
+    next if $aa eq 'X';
+    my @coords = @{$entry->{coords}};
+    my $chrom = $entry->{chrom};
+    my $triplet_seq = $entry->{triplet_seq};
+    my $i = $entry->{aa_position};
+    my $new_triplets = $entry->{new_triplets};
+    foreach my $coord (@coords) {
+      my $triplet_start = $coord->[0];
+      my $triplet_end = $coord->[1];
+      my $iter = $self->get_tabix_iterator($chrom, $triplet_start, $triplet_end);
+      while (my $line = $iter->next) {
+        my $data = $self->get_CADD_row($line);
+        my $chr = $data->{'#Chr'};
+        my $pos = $data->{'Pos'};
+        my $ref = $data->{'Ref'};
+        my $alt = $data->{'Alt'};
+        my $cadd_phred = $data->{'PHRED'};
+        next if ($alt eq $ref);
+        my $nucleotide_position = ($self->reverse) ? $triplet_end - $pos : $pos - $triplet_start;
+        my $mutated_triplet =  $new_triplets->{$triplet_seq}->{$nucleotide_position}->{$alt};
+        my $mutated_aa = $self->codon_table->translate($mutated_triplet);
+        $self->add_predictions($data, $i, $mutated_aa);
+      }
+    }
+  }
+} 
+
+sub add_predictions {
+  my ($self, $data, $i, $mutated_aa) = @_;
+  if ($data->{'PHRED'} ne '.') {
+    my $prediction = ($data->{'PHRED'} >= $CADD_CUTOFF) ? 'likely deleterious' : 'likely benign';
+    $self->add_prediction($i, $mutated_aa, 'cadd', $data->{'PHRED'}, $prediction);
+  }
+}
+
+=head2 get_CADD_row
+  Arg 1      : String $line from parser
+  Description: Join header column with row value
+  Returntype : Hashref mapping header column to row value
+  Exceptions : None
+  Caller     : load_predictions_for_triplets() 
+  Status     : 
+=cut
+sub get_CADD_row {
+  my $self = shift;
+  my $line = shift;
+  $line =~ s/\r$//g;
+  my @split = split /\t/, $line;
+  my $header = $self->header;
+  my %data = map {$header->[$_] => $split[$_]} (0..(scalar @{$header} - 1));
+  return \%data;
+}
+
+1;
---
+++ modules/Bio/EnsEMBL/Variation/Utils/DbNSFPProteinFunctionAnnotation.pm
@@ -0,0 +1,205 @@
+=head1 LICENSE
+
+Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+Copyright [2016-2019] EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=cut
+
+
+
+=head1 CONTACT
+
+ Please email comments or questions to the public Ensembl
+ developers list at <http://lists.ensembl.org/mailman/listinfo/dev>.
+
+ Questions may also be sent to the Ensembl help desk at
+ <http://www.ensembl.org/Help/Contact>.
+
+=head1 DESCRIPTION
+
+Module is used in protein function prediction pipeline for
+annotating all possible amino acid substitutions in a translation
+with dbNSFP (revel, meta_lr and mutation_assessor) scores and predictions.
+
+=cut
+
+
+use strict;
+use warnings;
+
+package Bio::EnsEMBL::Variation::Utils::DbNSFPProteinFunctionAnnotation;
+
+use Bio::EnsEMBL::Variation::Utils::BaseProteinFunctionAnnotation;
+our @ISA = ('Bio::EnsEMBL::Variation::Utils::BaseProteinFunctionAnnotation');
+
+my $REVEL_CUTOFF = 0.5;
+
+=head2 new
+  Example    :
+  my $cadd = Bio::EnsEMBL::Variation::Utils::DbNSFPProteinFunctionAnnotation->new(
+    -species => 'Homo_sapiens',
+    -annotation_file => 'dbNSFP3.5a_grch37.txt.gz',
+    -assembly => 'GRCh37',
+    -annotation_file_version => '3.5a',
+    -pipeline_mode => 0,
+    -debug_mode => 1,
+  );
+
+  Description: Constructor. Instantiates a new DbNSFPProteinFunctionAnnotation object.
+  Returntype : DbNSFPProteinFunctionAnnotation
+  Exceptions : throws on unsupported version
+  Caller     : Bio::EnsEMBL::Variation::Pipeline::ProteinFunction::RunDbNSFP::run
+  Status     : Stable
+=cut
+sub new {
+  my $caller = shift;
+  my $class = ref($caller) || $caller;
+
+  my $self = $class->SUPER::new(@_);
+
+  if (! grep {$_ eq $self->annotation_file_version} ('3.5a')) {
+    die "dbNSFP version " . $self->annotation_file_version . " is not supported.";
+  }
+
+  $self->analysis([qw/dbnsfp_revel dbnsfp_meta_lr dbnsfp_mutation_assessor/]);
+
+  return $self;
+}
+
+my $predictions = {
+  dbnsfp_meta_lr => {
+    T => 'tolerated',
+    D => 'damaging',
+  },
+  dbnsfp_mutation_assessor => {
+    H => 'high',
+    M => 'medium',
+    L => 'low',
+    N => 'neutral',
+  }
+};
+
+my $column_names = {
+  '3.5a' => {
+    assembly_unspecific => {
+      chr => '#chr',
+      ref => 'ref',
+      refcodon => 'refcodon',
+      alt => 'alt',
+      aaalt => 'aaalt',
+      aaref => 'aaref',
+      revel_score => 'REVEL_score',
+      meta_lr_score => 'MetaLR_score',
+      meta_lr_pred => 'MetaLR_pred',
+      mutation_assessor_score => 'MutationAssessor_score_rankscore',
+      mutation_assessor_pred => 'MutationAssessor_pred',
+    },
+    'assembly_specific' => {
+      'GRCh37' => {
+        pos => 'hg19_pos(1-based)'
+      },
+      'GRCh38' => {
+        pos => 'pos(1-based)'
+      },
+    },
+  },
+  '4.0b1' => {
+
+  }
+};
+
+sub load_predictions_for_triplets {
+  my $self = shift;
+  my $triplets = shift; 
+  foreach my $entry (@$triplets) {
+    my $aa = $entry->{aa};
+    $self->amino_acids($aa);
+    next if $aa eq 'X';
+    my @coords = @{$entry->{coords}};
+    my $chrom = $entry->{chrom};
+    my $triplet_seq = $entry->{triplet_seq};
+    my $i = $entry->{aa_position};
+    my $new_triplets = $entry->{new_triplets};
+    foreach my $coord (@coords) {
+      my $triplet_start = $coord->[0];
+      my $triplet_end = $coord->[1];
+      my $iter = $self->get_tabix_iterator($chrom, $triplet_start, $triplet_end);
+      while (my $line = $iter->next) {
+        my $data = $self->get_dbNSFP_row($line);
+        my $chr = $data->{'chr'};
+        my $pos = $data->{'pos'};
+        my $ref = $data->{'ref'};
+        my $refcodon = $data->{'refcodon'};
+        my $alt = $data->{'alt'};
+        my $aaalt = $data->{'aaalt'};
+        my $aaref = $data->{'aaref'};
+        next if ($alt eq $ref);
+        my $nucleotide_position = ($self->reverse) ? $triplet_end - $pos : $pos - $triplet_start;
+        my $mutated_triplet =  $new_triplets->{$triplet_seq}->{$nucleotide_position}->{$alt};
+        my $mutated_aa = $self->codon_table->translate($mutated_triplet);
+        next if ($aaalt ne $mutated_aa);
+        $self->add_predictions($data, $i, $mutated_aa);
+      }    
+    }
+  }
+} 
+
+sub add_predictions {
+  my ($self, $data, $i, $mutated_aa) = @_;
+  if ($data->{revel_score} ne '.') {
+    my $prediction = ($data->{revel_score} >= $REVEL_CUTOFF) ? 'likely disease causing' : 'likely benign';
+    $self->add_prediction($i, $mutated_aa, 'dbnsfp_revel', $data->{revel_score}, $prediction);
+  }
+  if ($data->{meta_lr_score} ne '.') {
+    my $prediction = $predictions->{dbnsfp_meta_lr}->{$data->{meta_lr_pred}};
+    $self->add_prediction($i, $mutated_aa, 'dbnsfp_meta_lr', $data->{meta_lr_score}, $prediction);
+  }
+  if ($data->{mutation_assessor_score} ne '.') {
+    my $prediction = $predictions->{dbnsfp_mutation_assessor}->{$data->{mutation_assessor_pred}};
+    $self->add_prediction($i, $mutated_aa, 'dbnsfp_mutation_assessor', $data->{mutation_assessor_score}, $prediction);
+  }
+}
+
+=head2 get_dbNSFP_row
+  Arg 1      : String $line from parser
+  Description: - Join header column with row value
+               - Use assembly and file version specific header
+  Returntype : Hashref mapping header column to row value
+  Exceptions : None
+  Caller     : load_predictions_for_triplets()
+  Status     :
+=cut
+sub get_dbNSFP_row {
+  my $self = shift;
+  my $line = shift;
+  $line =~ s/\r$//g;
+  my @split = split /\t/, $line;
+  my $header = $self->header;
+  my $assembly = $self->assembly;
+  my $dbnsfp_version = $self->annotation_file_version;
+  my %raw_data = map {$header->[$_] => $split[$_]} (0..(scalar @{$header} - 1));
+  my $data = {};
+  my $assembly_unspecific = $column_names->{$dbnsfp_version}->{assembly_unspecific};
+  foreach my $column_name (keys %{$assembly_unspecific}) {
+    $data->{$column_name} = $raw_data{$assembly_unspecific->{$column_name}};
+  }
+  my $assembly_specific =  $column_names->{$dbnsfp_version}->{assembly_specific}->{$assembly}; 
+  foreach my $column_name (keys %{$assembly_specific}) {
+    $data->{$column_name} = $raw_data{$assembly_specific->{$column_name}};
+  }
+  return $data;
+}
+
+1;
