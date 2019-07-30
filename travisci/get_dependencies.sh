6287b328c5d38e3e818410dd0fa1b13a113c131e
at7 <anja@ebi.ac.uk>
2019-07-30 12:40:24 +0100

    upgrade htslib and Bio::DB::HTS

--- travisci/get_dependencies.sh
+++ travisci/get_dependencies.sh
@@ -5,3 +5,9 @@ if [ ! -f v335_base.tar.gz ]; then
   wget https://github.com/ucscGenomeBrowser/kent/archive/v335_base.tar.gz
   tar xzf v335_base.tar.gz && rm -rf v335_base.tar.gz kent-335_base/java kent-335_base/python
 fi
+
+echo 'Getting Bio::DB::HTS'
+if [ ! -d Bio-HTS ]; then
+  git clone --branch 2.11 --depth 1 https://github.com/Ensembl/Bio-HTS.git
+fi
+
