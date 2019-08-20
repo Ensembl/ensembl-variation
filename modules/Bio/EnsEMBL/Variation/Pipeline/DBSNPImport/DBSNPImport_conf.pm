ec1df1948cf318405ec0aca8b1dea565bd9b6c5b
at7 <anja@ebi.ac.uk>
2019-08-20 11:08:28 +0100

    ancestral_fasta_file parameter

--- modules/Bio/EnsEMBL/Variation/Pipeline/DBSNPImport/DBSNPImport_conf.pm
+++ modules/Bio/EnsEMBL/Variation/Pipeline/DBSNPImport/DBSNPImport_conf.pm
@@ -61,6 +61,7 @@ sub default_options {
     rpt_dir                 => $self->o('pipeline_dir') . '/split-rpt',
     script_dir              => $self->o('ensembl_cvs_root_dir') . '/ensembl-variation/scripts/import/dbSNP_v2',
     fasta_file              => $self->o('fasta_file'),
+    ancestral_fasta_file    => $self->o('ancestral_fasta_file'),
     registry_file           => $self->o('pipeline_dir') . '/' . 'ensembl.registry',
 
     pipeline_db => {
