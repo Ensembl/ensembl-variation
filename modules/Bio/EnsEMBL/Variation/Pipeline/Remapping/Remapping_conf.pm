ab3cbda8f8ea128112233505c43bb2cdef2478c9
at7 <anja@ebi.ac.uk>
2021-06-02 13:01:16 +0100

    support env ENSEMBL_CVS_ROOT_DIR

--- modules/Bio/EnsEMBL/Variation/Pipeline/Remapping/Remapping_conf.pm
+++ modules/Bio/EnsEMBL/Variation/Pipeline/Remapping/Remapping_conf.pm
@@ -46,8 +46,8 @@ sub default_options {
         hive_use_triggers       => 0,
         hive_auto_rebalance_semaphores => 0,  # do not attempt to rebalance semaphores periodically by default
         hive_no_init            => 0, # setting it to 1 will skip pipeline_create_commands (useful for topping up)
-        hive_root_dir           => '/hps/software/users/ensembl/repositories/' . $ENV{'USER'} . '/ensembl-hive',
-        ensembl_cvs_root_dir    => '/hps/software/users/ensembl/repositories/' . $ENV{'USER'},
+        ensembl_cvs_root_dir    => $ENV{'ENSEMBL_CVS_ROOT_DIR'} || '/hps/software/users/ensembl/repositories/' . $ENV{'USER'},
+        hive_root_dir           => $self->o('ensembl_cvs_root_dir') . '/ensembl-hive',
         debug                   => 0,
         run_variant_qc          => 1,
         use_fasta_files         => 0,
