b9208408f686e82fe030b47b2e4e78366df256e0
Syed Nakib Hossain <snhossain@ebi.ac.uk>
2022-05-16 09:20:46 +0100

    Database patch prior to variation e108 branching

---
+++ sql/patch_107_108_b.sql
@@ -0,0 +1,20 @@
+-- Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+-- Copyright [2016-2022] EMBL-European Bioinformatics Institute
+-- 
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+-- 
+--      http://www.apache.org/licenses/LICENSE-2.0
+-- 
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+# update the varition_set table with correct desciption of SAS common population
+UPDATE variation_set SET description = 'Variants genotyped in South Asian individuals by the 1000 Genomes project (phase 3) with frequency of at least 1%' WHERE name = '1000 Genomes 3 - SAS - common';
+
+# patch identifier
+INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_107_108_b.sql|fix SAS population description');
