c53a25cb07758e0bd104e302f240de213ff9af00
Ola Austine <83219714+olaaustine@users.noreply.github.com>
2022-06-24 11:20:58 +0100

    Removing space

--- scripts/import/import_vcf.pl
+++ scripts/import/import_vcf.pl
@@ -2935,8 +2935,9 @@ Options
 
 --create_name         Always create a new variation name i.e. don't use ID column.
                       It doesn't apply to Mastermind.
+
 --use_chr			  Fetches chromosome name and seq region id from the seq_region
-					  and adds chr to the chromosome name, important to call this
+                      and adds chr to the chromosome name, important to call this
 					  when importing EVA
 --merge_all_types     Merges all types including indels, if not used indels 
   					  will not be merged in the variation feature table
