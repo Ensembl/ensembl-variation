1c54e1fe24c1a42be6ea89cd656d4da93874169a
Ola Austine <83219714+olaaustine@users.noreply.github.com>
2022-06-24 11:18:34 +0100

    Supporting source_description and usage

--- scripts/import/import_vcf.pl
+++ scripts/import/import_vcf.pl
@@ -1701,7 +1701,7 @@ sub get_source_id{
 	my $config = shift;
 	my $dbVar  = $config->{dbVar};
 	my $source = $config->{source};
-	my $desc   = $config->{desc};
+	my $desc   = $config->{source_description};
 	
 	my $source_id;
 	
@@ -2935,7 +2935,11 @@ Options
 
 --create_name         Always create a new variation name i.e. don't use ID column.
                       It doesn't apply to Mastermind.
-
+--use_chr			  Fetches chromosome name and seq region id from the seq_region
+					  and adds chr to the chromosome name, important to call this
+					  when importing EVA
+--merge_all_types     Merges all types including indels, if not used indels 
+  					  will not be merged in the variation feature table
 --chrom_regexp        Limit processing to CHROM columns matching regexp
 
 --flank               Size of flanking sequence [default: 200]
