87e85f3a71d465ba0834e3f4c9ecee2cd9f8714d
Ola Austine <83219714+olaaustine@users.noreply.github.com>
2022-06-24 11:23:12 +0100

    Removing space

--- scripts/import/import_vcf.pl
+++ scripts/import/import_vcf.pl
@@ -2938,10 +2938,11 @@ Options
 
 --use_chr             Fetches chromosome name and seq region id from the seq_region
                       and adds chr to the chromosome name, important to call this
-		              when importing EVA
+                      when importing EVA
 
 --merge_all_types     Merges all types including indels, if not used indels 
-  					  will not be merged in the variation feature table
+                      will not be merged in the variation feature table
+					  
 --chrom_regexp        Limit processing to CHROM columns matching regexp
 
 --flank               Size of flanking sequence [default: 200]
