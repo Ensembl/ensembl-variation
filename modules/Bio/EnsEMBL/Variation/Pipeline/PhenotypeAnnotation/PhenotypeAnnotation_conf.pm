e062255167d15d8a2b9602cccb825443ea825584
Dionysios Grigoriadis <digri@codon-login-01.ebi.ac.uk>
2022-08-08 09:05:33 +0100

    Changes to improve WormBase Phenotypes import for WormBase ParaSite release 17 (e105)

--- modules/Bio/EnsEMBL/Variation/Pipeline/PhenotypeAnnotation/PhenotypeAnnotation_conf.pm
+++ modules/Bio/EnsEMBL/Variation/Pipeline/PhenotypeAnnotation/PhenotypeAnnotation_conf.pm
@@ -556,6 +556,7 @@ sub pipeline_analyses {
         {   -logic_name => 'import_wormbase',
 	    -module     => 'Bio::EnsEMBL::Variation::Pipeline::PhenotypeAnnotation::ImportWORMBASE',
 	    -parameters => {
+                source_version => $ENV{"WORMBASE_VERSION"},
                 @common_params,
             },
 	    -input_ids  => [],
