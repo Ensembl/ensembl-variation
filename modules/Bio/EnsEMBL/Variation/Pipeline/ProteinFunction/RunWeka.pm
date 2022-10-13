c182f2aaef1dfdd5886c8da0f918b621581854b8
Nuno Agostinho <nuno@ebi.ac.uk>
2022-10-13 10:48:34 +0100

    ProteinFunction: avoid storing null matrices from PolyPhen-2

--- modules/Bio/EnsEMBL/Variation/Pipeline/ProteinFunction/RunWeka.pm
+++ modules/Bio/EnsEMBL/Variation/Pipeline/ProteinFunction/RunWeka.pm
@@ -116,6 +116,7 @@ sub run {
             -translation_md5    => $translation_md5,
         );
 
+        my $results_available = 0;
         while (<RESULT>) {
             if (/^#/) {
                 s/#//g;
@@ -141,6 +142,7 @@ sub run {
             my $position    = $results{o_pos};
 
             next unless $position && $alt_aa;
+            $results_available = 1;
 
             $pred_matrix->add_prediction(
                 $position,
@@ -150,9 +152,9 @@ sub run {
             );
         }
 
-        # save the predictions to the database
+        # save the predictions to the database if the matrix is not null
 
-        $pfpma->store($pred_matrix);
+        $pfpma->store($pred_matrix) if $results_available;
     }
 
     remove_tree($tmp_dir);
