fb87835a998e280352aa72059744aa532da65be5
Syed Nakib Hossain <snhossain@ebi.ac.uk>
2022-11-14 11:27:59 +0000

    Put the changes to trigger test

--- modules/Bio/EnsEMBL/Variation/DBSQL/VariationFeatureAdaptor.pm
+++ modules/Bio/EnsEMBL/Variation/DBSQL/VariationFeatureAdaptor.pm
@@ -2049,9 +2049,7 @@ sub _hgvs_from_components {
   # take alternate allele from genomic reference & coordinates if not supplied in HGVS string for a duplication
   if($description =~ /dup/){ 
     ## special case: handle as insertion for ensembl object purposes 
-    $start = $end ;
-    if($strand  == 1){ $start++; }
-    else{             $end--;   } 
+    $start = $end + 1; 
 
     $ref_allele = "-" ;
     $alt_allele = $refseq_allele;
