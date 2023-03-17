820852c0fa81c5e64d5a9ae4c5ac2710a3ebad5c
Diego Coelho <diegomscoelho@gmail.com>
2023-03-17 17:13:42 +0000

    fix: add chrX + fix regex problem

--- modules/Bio/EnsEMBL/Variation/Pipeline/DBSNPImport/DumpDBSNPFile.pm
+++ modules/Bio/EnsEMBL/Variation/Pipeline/DBSNPImport/DumpDBSNPFile.pm
@@ -51,7 +51,8 @@ my @chrs = (
   "chr6", "chr7", "chr8", "chr9", "chr10",
   "chr11", "chr12", "chr13", "chr14", "chr15",
   "chr16", "chr17", "chr18", "chr19", "chr20",
-  "chr21", "chr22", 'chrY', "chrMT", 'other'
+  "chr21", "chr22", "chrX" , "chrY", "chrMT",
+  "other"
 );
 
 my %ids = (
@@ -92,11 +93,11 @@ sub run {
 
   my $var_dba = $self->get_species_adaptor('variation');
   my $dbh = $var_dba->dbc();
-  my @return_value = sort glob("ls ${TMP_DIR}/split-src/*/tmp*");
 
   # The input_directory is <data-dir>/<sub_dir>
 
   for my $chr (@chrs) {
+    my @return_value = glob("ls ${TMP_DIR}/split-src/${chr}/tmp*");
     my @chr_files = grep( /${chr}/, @return_value);
     for my $file (@chr_files) {
       my $base = basename($file);
