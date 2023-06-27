75b469e2a5e1662af0d544cfb13f593f72a19b14
olaaustine <olaaustine@ebi.ac.uk>
2023-06-27 15:19:30 +0100

    Changes for tv and changes based on review

--- scripts/misc/import_oldhgmd.pl
+++ scripts/misc/import_oldhgmd.pl
@@ -1,4 +1,19 @@
 #!/usr/bin/env perl
+# Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+# Copyright [2016-2023] EMBL-European Bioinformatics Institute
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#      http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 use strict;
 use DBI;
 use Socket;
@@ -39,6 +54,13 @@ my $new_pf_file = "new_pf.txt";
 my $sorted_new_pf = "sorted_new_pf.txt";
 my $sorted_old_pfa = "sorted_old_pfa.txt";
 my $sorted_new_pfa = "sorted_new_pfa.txt";
+my $old_tv_file = "old_tv.txt";
+my $sorted_old_tv_file = "sorted_old_tv.txt";
+my $new_tv_file = "new_tv.txt";
+my $old_mtmp_file = "old_mtmp.txt";
+my $sorted_old_mtmp_file = "sorted_old_mtmp.txt";
+my $new_mtmp_file = "new_mtmp.txt";
+
 
 if ($old_reg_file) {
   $registry->load_all($old_reg_file);
@@ -65,12 +87,14 @@ if ($config->{test}) {
 }
 
 sub test {
-  debug($config, "Running in test mode"); 
 
-  debug($config, "Dumping old variation tables with HGMD as source from old database");
+  debug($config, "Running in test mode"); 
+  
+  my $old_dbname = $old_dbh->dbname;
+  debug($config, "Dumping old phenotype data with HGMD as source from $old_dbname");
   dump_vdata_into_file($old_dbh);
 
-  debug($config, "Dumping old phenotype with HGMD as source from old database");
+  debug($config, "Dumping old phenotype data with HGMD as source from $old_dbname");
   dump_pheno($old_dbh);
 
   debug($config, "Sorting files based on the variation_id column");
@@ -78,60 +102,78 @@ sub test {
   system("sort -k 1 -o ${TMP_DIR}${sorted_old_var} ${TMP_DIR}${old_var_file}");
   system("sort -k 6 -o ${TMP_DIR}${sorted_old_var_feat} ${TMP_DIR}${old_vf_file}");
 
+  if ($config->{transcript}) {
+    debug($config, "Sorting files based on the variation_feature_id column");
+    system("sort -k 2 -o ${TMP_DIR}${$sorted_old_tv_file} ${TMP_DIR}${old_tv_file}");
+    system("sort -k 2 -o ${TMP_DIR}${$sorted_old_mtmp_file} ${TMP_DIR}${old_mtmp_file}");
+  }
+
   debug($config, "Sorting files based on the phenotype_feature_id column");
   system("sort -k 1 -o ${TMP_DIR}${sorted_old_pheno_feat} ${TMP_DIR}${old_pheno_feat_file}");
   system("sort -k 1 -o ${TMP_DIR}${sorted_old_pfa} ${TMP_DIR}${old_pheno_feat_attrib_file}");
-  
-  debug($config, "Manipulating variation ids in each file");
+
+  debug($config, "Assigns new variation_ids and variation_feature_ids using the existing maximum variation_id and variation_feature_id");
   manipulate_var_ids($dbh);
 
   debug($config, "Manipulating Phenotype ids in each file");
   manipulate_pheno_ids($dbh);
 
-  debug($config, "Inserting into variation_feature table but first sort, this is test mode so no insertion");
-  system("sort -k 2,2 -k3,3 -k4,4 -o ${TMP_DIR}${sorted_new_vf} ${TMP_DIR}${new_var_feat}");
+  debug($config, "Sorting the new variation_feature file by seq regions. Test mode no insertion");
+  system("sort -k2,2n -k3,3n -k4,4n -o ${TMP_DIR}${sorted_new_vf} ${TMP_DIR}${new_var_feat}");
 
-  debug($config, "Inserting into phenotype_feature but first sort, test mode so no insertion");
-  system("sort -k7,7 -k8,8 -k9,9 -o ${TMP_DIR}${sorted_new_pf} ${TMP_DIR}${new_pf_file}");
+
+  debug($config, "Sorting the new phenotype_feature file by seq regions. Test mode no insertion"); 
+  system("sort -k7,7n -k8,8n -k9,9n -o ${TMP_DIR}${sorted_new_pf} ${TMP_DIR}${new_pf_file}");
 }
 
 sub main { 
-  debug($config, "Dumping old variation tables with HGMD as source from old database");
+  
+   my $old_dbname = $old_dbh->dbname;
+
+  debug($config, "Dumping HGMD data from tables variation and variation_feature from the $old_dbname");
   dump_vdata_into_file($old_dbh);
 
-  debug($config, "Dumping old phenotype with HGMD as source from old database");
+  debug($config, "Dumping old phenotype data with HGMD as source from $old_dbname");
   dump_pheno($old_dbh);
 
   debug($config, "Sorting files based on the variation_id column");
   $TMP_DIR = $TMP_DIR . "/";
   system("sort -k 1 -o ${TMP_DIR}${sorted_old_var} ${TMP_DIR}${old_var_file}");
   system("sort -k 6 -o ${TMP_DIR}${sorted_old_var_feat} ${TMP_DIR}${old_vf_file}");
+  
+  if ($config->{transcript}) {
+    debug($config, "Sorting files based on the variation_feature_id column");
+    system("sort -k 2 -o ${TMP_DIR}${$sorted_old_tv_file} ${TMP_DIR}${old_tv_file}");
+    system("sort -k 2 -o ${TMP_DIR}${$sorted_old_mtmp_file} ${TMP_DIR}${old_mtmp_file}");
+  }
 
   debug($config, "Sorting files based on the phenotype_feature_id column");
   system("sort -k 1 -o ${TMP_DIR}${sorted_old_pheno_feat} ${TMP_DIR}${old_pheno_feat_file}");
   system("sort -k 1 -o ${TMP_DIR}${sorted_old_pfa} ${TMP_DIR}${old_pheno_feat_attrib_file}");
 
-  debug($config, "Manipulating variation ids in each file");
+  debug($config, "Assigns new variation_ids and variation_feature_ids using the maximum variation_id and variation_feature_id");
   manipulate_var_ids($dbh);
 
-  debug($config, "Manipulating Phenotype ids in each file");
+  debug($config, "Assigns new phenotype_feature id using the existing maximum phenotype_feature_id");
   manipulate_pheno_ids($dbh);
 
-  debug($config, "Inserting into variation table");
+  debug($config, "Inserting HGMD data into variation table");
   insert_into_variation_table($dbh, $new_var);
 
-  debug($config, "Inserting into variation_feature table but first sort");
-  system("sort -k 2,2 -k3,3 -k4,4 -o ${TMP_DIR}${sorted_new_vf} ${TMP_DIR}${new_var_feat}");
+  debug($config, "Sorting the new variation_feature file by seq regions");
+  system("sort -k2,2n -k3,3n -k4,4n -o ${TMP_DIR}${sorted_new_vf} ${TMP_DIR}${new_var_feat}");
+  debug($config, "Inserting HGMD data into variation_feature table");
   insert_variation_feature($dbh, $sorted_new_vf);
 
-  debug($config, "Inserting into variation_set_variation table");
+  debug($config, "Inserting HGMD data into variation_set_variation table");
   load_all_variation_sets($dbh, $new_var);
 
-  debug($config, "Inserting into phenotype_feature but first sort");
-  system("sort -k7,7 -k8,8 -k9,9 -o ${TMP_DIR}${sorted_new_pf} ${TMP_DIR}${new_pf_file}");
+  debug($config, "Sorting the new phenotype_feature file by seq regions"); 
+  system("sort -k7,7n -k8,8n -k9,9n -o ${TMP_DIR}${sorted_new_pf} ${TMP_DIR}${new_pf_file}");
+  debug($config, "Inserting into phenotype_feature table");
   insert_pheno_feature($dbh, $sorted_new_pf);
 
-  debug($config, "Inserting into phenotype_feature attrib");
+  debug($config, "Inserting into phenotype_feature_attrib table");
   insert_pheno_feature_attrib($dbh, $sorted_new_pfa);
 
   debug($config, "Removing files");
@@ -168,8 +210,29 @@ sub dump_vdata_into_file {
   close $var_fea;
 
   if ($config->{transcript}) {
-    my $dump_tv = $old_dbhvar->prepare(qq[ SELECT transcript_variation_id, variation_feature_id, feature_stable_id, allele_string, somatic, consequence_types, cds_start, cds ])
+    my $dump_tv = $old_dbhvar->prepare(qq[ SELECT transcript_variation_id, variation_feature_id, feature_stable_id, allele_string, somatic, consequence_types, cds_start, cds_end, cdna_start, cdna_end, translation_start, translation_end, distance_to_transcript, display from transcript_variation where variation_feature_id in  (SELECT variation_feature_id from variation_feature where source_id = 8) ]);
+    open(my $tv, ">>$TMP_DIR/$old_tv_file") or die ("Cannot open $TMP_DIR/$old_tv_file: $!");
+    $dump_tv->execute();
+
+    while ( my $tvref = $dump_tv->fetchrow_arrayref() ){
+      my @tv = map {defined($_) ? $_: '\N' } @$tvref;
+      print $tv join("\t", @tv), "\n";
+    }
+
+    $dump_tv->finish();
+
+    my $dump_mtmp = $old_dbhvar->prepare(qq[ SELECT transcript_variation_id, variation_feature_id, feature_stable_id, allele_string, somatic, consequence_types, cds_start, cds_end, cdna_start, cdna_end, translation_start, translation_end, distance_to_transcript from MTMP_transcript_variation where variation_feature_id in  (SELECT variation_feature_id from variation_feature where source_id = 8) ]);
+    open (my $mtmp, ">>$TMP_DIR/$old_mtmp_file") or die ("Cannot open $TMP_DIR/$old_mtmp_file: $!");
+    $dump_mtmp->execute();
+
+    while ( my $mtmpref = $dump_mtmp->fetchrow_arrayref() ){
+      my @mtmp = map {defined($_) ? $_: '\N'} @$mtmpref;
+      print $mtmp join("\t", @mtmp), "\n";
+    }
+
+    $dump_mtmp->finish();
   }
+ 
 }
 
 sub manipulate_var_ids {
@@ -186,7 +249,9 @@ sub manipulate_var_ids {
   my $vf = $select_max_feat->fetchall_arrayref();
   my $max_var_feat = $vf->[0]->[0];
   $max_var_feat = $max_var_feat + 1;
-
+  
+  $select_max_var->finish();
+  $select_max_feat->finish();
  
   open(my $fh, '<', "$TMP_DIR/$sorted_old_var") or die "Cannot open file: $!";
   open(my $new_var_file, '>', "$TMP_DIR/$new_var") or die "Cannot open file: $!";
@@ -216,10 +281,78 @@ sub manipulate_var_ids {
   }
 
   close $old_var;
-  close $new_var_file;
+  close $new_var_feat;
   
-  $select_max_var->finish();
-  $select_max_feat->finish();
+  if ($config->{transcript}) {
+
+    my $select_max_tv = $dbhvar->prepare(qq[SELECT MAX(transcript_variation_id) from transcript_variation]);
+    $select_max_tv->execute();
+    my $tv = $select_max_tv->fetchall_arrayref();
+    my $max_tv = $tv->[0]->[0];
+    $max_tv = $max_tv + 1;
+
+    my $select_max_mtmp = $dbhvar->prepare(qq[SELECT MAX(transcript_variation_id) from MTMP_transcript_variation]);
+    $select_max_mtmp->execute();
+    my $mtmp = $select_max_mtmp->fetchall_arrayref();
+    my $max_mtmp = $mtmp->[0]->[0];
+    $max_mtmp = $max_mtmp + 1;
+
+    open($old_var, '<', "$TMP_DIR/$sorted_old_var_feat") or die "Cannot open file: $!";
+    open($new_var_feat, '<', "$TMP_DIR/$new_var_feat") or die "Cannot open file: $!";
+    
+    my @old_vid;
+    my @new_vid;
+    while (<$old_var>) {
+      chomp;
+      my $key = (split)[0];
+      push @old_vid, $key;
+    }
+    while (<$new_var_feat>) {
+      chomp;
+      my $new_vid = (split)[0];
+      push @new_vid, $new_vid;
+    }
+    close $old_var;
+    close $new_var_feat;
+
+    my %v_feat = map { $old_vid[$_] => $new_vid[$_] } 0..$#old_vid;
+    
+    open (my $old_tv, "<", "$TMP_DIR/$sorted_old_tv_file") or die "Cannot open file: $!";
+    open (my $old_mtmp, "<", "$TMP_DIR/$sorted_old_mtmp_file") or die "Cannot open file: $!";
+    open (my $new_tv_feat, ">", "$TMP_DIR/$new_tv_file") or die "Cannot open file: $!";
+    open (my $new_mtmp_feat, ">", "$TMP_DIR/$new_mtmp_file") or die "Cannot open file: $!";
+    
+    while (my $tv_data = <$old_tv>){
+      chomp $tv_data;
+      my @columns = split ("\t", $tv_data);
+      
+      my $change_id = $columns[1];
+      if (exists $v_feat{$change_id}) {
+        $columns[0] = $max_tv++;
+        $columns[1] = $v_feat{$change_id};
+        print $new_tv_feat join ("\t", @columns), "\n";
+      } 
+    }
+    close $old_tv;
+    close $new_tv_feat;
+
+    while (my $mtmp_data = <$old_mtmp>) { 
+      chomp $mtmp_data;
+      my @columns = split ("\t", $mtmp_data);
+      
+      my $mtmp_id = $columns[1];
+      if (exists $v_feat{$mtmp_id}) {
+        $columns[0] = $max_mtmp++;
+        $columns[1] = $v_feat{$mtmp_id};
+        print $new_mtmp_feat join ("\t", @columns), "\n";
+      } 
+    }
+    close $old_mtmp;
+    close $new_tv_feat;
+   
+    
+  }
+
 }
 
 sub manipulate_pheno_ids {
@@ -261,7 +394,7 @@ sub manipulate_pheno_ids {
   close $old_pfa;  
 }
 
-sub insert_into_variation_table  {
+sub insert_into_variation_table {
   my $dbhvar = shift;
   my $load_file = shift;
 
