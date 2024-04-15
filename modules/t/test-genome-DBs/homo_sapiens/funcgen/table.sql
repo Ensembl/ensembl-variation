e0f4b79a04a1bdf1bb946906f70f083e71ce2866
Likhitha Surapaneni <10923198+likhitha-surapaneni@users.noreply.github.com>
2024-04-15 18:38:30 +0100

    Initial patch for test databases

--- modules/t/test-genome-DBs/homo_sapiens/funcgen/table.sql
+++ modules/t/test-genome-DBs/homo_sapiens/funcgen/table.sql
@@ -256,7 +256,7 @@ CREATE TABLE `coord_system_bu` (
 ) ENGINE=MyISAM AUTO_INCREMENT=2473 DEFAULT CHARSET=latin1;
 
 CREATE TABLE `data_file` (
-  `data_file_id` int(11) NOT NULL AUTO_INCREMENT,
+  `data_file_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `table_id` int(10) unsigned NOT NULL,
   `table_name` varchar(32) NOT NULL,
   `path` varchar(255) NOT NULL,
@@ -285,7 +285,7 @@ CREATE TABLE `epigenome_track` (
   `epigenome_track_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `epigenome_id` int(10) unsigned NOT NULL,
   `feature_type_id` int(10) unsigned NOT NULL,
-  `data_file_id` int(11) unsigned NOT NULL,
+  `data_file_id` int(10) unsigned NOT NULL,
   `track_type` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`epigenome_track_id`),
   KEY `et_index` (`epigenome_id`,`feature_type_id`)
@@ -466,7 +466,7 @@ CREATE TABLE `meta` (
   PRIMARY KEY (`meta_id`),
   UNIQUE KEY `species_key_value_idx` (`species_id`,`meta_key`,`meta_value`(50)),
   KEY `species_value_idx` (`species_id`,`meta_value`(50))
-) ENGINE=MyISAM AUTO_INCREMENT=778 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=780 DEFAULT CHARSET=latin1;
 
 CREATE TABLE `meta_coord` (
   `table_name` varchar(40) NOT NULL,
