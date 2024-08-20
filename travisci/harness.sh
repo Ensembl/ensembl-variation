2ab4ead818ccdb36fb0bbfdde14f66a2af66634f
Syed Nakib Hossain <snhossain@ebi.ac.uk>
2024-08-20 17:01:05 +0100

    Add ensembl-vep to the path

--- travisci/harness.sh
+++ travisci/harness.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export PERL5LIB=$PWD/bioperl-live:$PWD/ensembl-test/modules:$PWD/ensembl/modules:$PWD/ensembl-hive/modules:$PWD/modules:$PWD/scripts/import/:$PWD/ensembl-io/modules:$PWD/ensembl-funcgen/modules
+export PERL5LIB=$PWD/bioperl-live:$PWD/ensembl-test/modules:$PWD/ensembl/modules:$PWD/ensembl-hive/modules:$PWD/modules:$PWD/scripts/import/:$PWD/ensembl-io/modules:$PWD/ensembl-vep/modules:$PWD/ensembl-funcgen/modules
 
 export PATH=$PATH:$PWD/C_code:$PWD/htslib
 
